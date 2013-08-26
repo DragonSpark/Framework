c634951f8ba00ce1dcc81c79621fce3d27a6218e
Michael DeMond <michael@dragonspark.us>
2013-08-26 14:54:12 -0700

    - Build fix.

--- DragonSpark.Application.Client/Properties/AssemblyInfo.cs
+++ DragonSpark.Application.Client/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
+using DragonSpark.Client;
 using DragonSpark.Server.ClientHosting;
 using System.Reflection;
 using System.Runtime.InteropServices;
