5790cd6d4ba47faceaf4d1a98c6c5ab18c6fbc61
Michael DeMond <michael@dragonspark.us>
2013-11-11 05:47:18 -0800

    - Slight Configuration.

---
+++ DragonSpark.Application.Server/Client/initializer.js
@@ -0,0 +1,24 @@
+﻿requirejs.config({
+	baseUrl: "/Client",
+	paths: {
+		text: "DragonSpark.Client/Core/text",
+		durandal: "DragonSpark.Client/Durandal",
+		dragonspark: "DragonSpark.Client",
+		
+		plugins: "DragonSpark.Client/Plugins",
+		transitions: "DragonSpark.Client/Transitions",
+		widgets: "DragonSpark.Client/Widgets",
+		
+		application: "DragonSpark.Application.Client",
+		viewmodels: "DragonSpark.Application.Client/ViewModels",
+		views: "DragonSpark.Application.Client/Views"
+	}
+});
+
+define( "jquery", [], function () { return jQuery; } );
+define( "knockout", [], function () { return ko; } );
+define( "breeze", [], function () { return breeze; } );
+
+define( ["dragonspark/application"], function (dragonSpark) {
+	dragonSpark.initialize();
+});
