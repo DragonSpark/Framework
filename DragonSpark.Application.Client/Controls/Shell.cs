078cb9761c04305e377febc8df35adfb1e12d292
MichaelD! <michael@dragonspark.us>
2015-03-07 11:56:26 -0500

    Cleaning out shell, getting ready for navigation.

--- DragonSpark.Application.Client/Controls/Shell.cs
+++ DragonSpark.Application.Client/Controls/Shell.cs
@@ -1,7 +1,6 @@
-﻿using FirstFloor.ModernUI.Windows.Controls;
+﻿using DragonSpark.Extensions;
+using FirstFloor.ModernUI.Windows.Controls;
 using System.Windows;
-using System.Windows.Media;
-using DragonSpark.Extensions;
 
 namespace DragonSpark.Application.Client.Controls
 {
@@ -17,29 +16,6 @@ namespace DragonSpark.Application.Client.Controls
 			DefaultStyleKey = typeof(Shell);
 		}
 
-		/*public DataTemplate TitleLinkTemplate
-		{
-			get { return GetValue( TitleLinkTemplateProperty ).To<DataTemplate>(); }
-			set { SetValue( TitleLinkTemplateProperty, value ); }
-		}	public static readonly DependencyProperty TitleLinkTemplateProperty = DependencyProperty.Register( "TitleTitleLinkTemplate", typeof(DataTemplate), typeof(Shell), null );
-
-		public IEnumerable TitleLinksSource
-		{
-			get { return GetValue( TitleLinksSourceProperty ).To<IEnumerable>(); }
-			set { SetValue( TitleLinksSourceProperty, value ); }
-		}	public static readonly DependencyProperty TitleLinksSourceProperty = DependencyProperty.Register( "TitleLinksSource", typeof(IEnumerable), typeof(Shell), new PropertyMetadata( OnItemsChanged ) );
-
-		static void OnItemsChanged( DependencyObject d, DependencyPropertyChangedEventArgs e )
-		{
-			d.As<Shell>( shell =>
-			{
-				shell.TitleLinks.Clear();
-				var links = e.NewValue.To<IEnumerable>().Cast<object>().Select( item => shell.TitleLinkTemplate.LoadContent().AsTo<ContentControl, Link>( control => control.Content.To<Link>() ) );
-				shell.TitleLinks.AddRange( links );
-
-			} );
-		}*/
-
 		public DataTemplate LogoContentTemplate
 		{
 			get { return GetValue( LogoContentTemplateProperty ).To<DataTemplate>(); }
@@ -51,11 +27,5 @@ namespace DragonSpark.Application.Client.Controls
 			get { return GetValue( LogoContentProperty ).To<object>(); }
 			set { SetValue( LogoContentProperty, value ); }
 		}	public static readonly DependencyProperty LogoContentProperty = DependencyProperty.Register( "LogoContent", typeof(object), typeof(Shell), null );
-
-		/*public ImageSource LogoSource
-		{
-			get { return (ImageSource)GetValue(LogoSourceProperty); }
-			set { SetValue( LogoSourceProperty, value ); }
-		}	public static DependencyProperty LogoSourceProperty = DependencyProperty.Register("LogoSource", typeof(ImageSource), typeof(Shell));*/
 	}
 }
