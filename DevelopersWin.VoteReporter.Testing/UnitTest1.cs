95f20cda81b908b737a64f1c4b2a34833bd4a58e
MichaelD! <michael@dragonspark.us>
2015-11-29 10:02:29 -0500

    Testing setup and passing.

--- DevelopersWin.VoteReporter.Testing/UnitTest1.cs
+++ DevelopersWin.VoteReporter.Testing/UnitTest1.cs
@@ -1,19 +1,18 @@
-﻿using System;
+﻿using DragonSpark.Windows.Io;
+using System;
 using System.Globalization;
-using DragonSpark.Windows.Io;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace DevelopersWin.VoteReporter.Testing
 {
-	[TestClass]
 	public class UnitTest1
 	{
-		[TestMethod]
+		[Fact]
 		public void TestMethod1()
 		{
 			DateTimeOffset result;
 			var parsed = DateTimeOffset.TryParseExact( "2015-11-13--18-46-49", FileSystem.ValidPathTimeFormat, CultureInfo.CurrentCulture, DateTimeStyles.None, out result );
-			Assert.IsTrue( parsed );
+			Assert.True( parsed );
 		}
 	}
 }
