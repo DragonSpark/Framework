bafd226dcbb501cac6aab7d4b324848edc1673cd
MichaelD! <michael@dragonspark.us>
2015-12-27 09:03:37 -0500

    - Missing files - Removing unnecessary files.

---
+++ DragonSpark.Testing/Runtime/CollectionTests.cs
@@ -0,0 +1,18 @@
+﻿using DragonSpark.Extensions;
+using DragonSpark.Runtime;
+using DragonSpark.Testing.Framework.Setup;
+using DragonSpark.Testing.TestObjects;
+using System.Collections;
+using Xunit;
+
+namespace DragonSpark.Testing.Runtime
+{
+	public class CollectionTests
+	{
+		[Theory, MoqAutoData]
+		void Add( Collection<Class> sut )
+		{
+			Assert.Equal( -1, sut.To<IList>().Add( new object() ) );
+		} 
+	}
+}
---
+++ DragonSpark.Testing/Runtime/Specifications/EqualityContextAwareSpecificationTests.cs
@@ -0,0 +1,17 @@
+﻿using DragonSpark.Runtime.Specifications;
+using DragonSpark.Testing.Framework.Setup;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace DragonSpark.Testing.Runtime.Specifications
+{
+	public class EqualityContextAwareSpecificationTests
+	{
+		[Theory, MoqAutoData]
+		public void Equal( [Frozen]object item, EqualityContextAwareSpecification sut )
+		{
+			Assert.True( sut.IsSatisfiedBy( item ) );
+			Assert.False( sut.IsSatisfiedBy( new object() ) );
+		} 
+	}
+}
