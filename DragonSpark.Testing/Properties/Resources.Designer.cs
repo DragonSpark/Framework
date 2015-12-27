70dace8f20af0b14be5f0e56da128db6aa1c315e
MichaelD! <michael@dragonspark.us>
2015-12-27 09:24:29 -0500

    - Refreshing NuGet - Updating to .NET 4.6.1 - DragonSpark.Windows.Testing

--- DragonSpark.Testing/Properties/Resources.Designer.cs
+++ DragonSpark.Testing/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace DragonSpark.Testing.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DragonSpark.Testing.Items.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DragonSpark.Testing.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
