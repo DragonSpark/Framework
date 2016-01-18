6d3257b7ab934718dea69852976f35e2c8c87d96
MichaelD! <michael@dragonspark.us>
2016-01-18 17:20:39 -0500

    Missing file.

---
+++ DragonSpark.Testing.Framework/Setup/Location/CustomizationBase.cs
@@ -0,0 +1,11 @@
+﻿using Ploeh.AutoFixture;
+
+namespace DragonSpark.Testing.Framework.Setup.Location
+{
+	public abstract class CustomizationBase : ICustomization
+	{
+		void ICustomization.Customize( IFixture fixture ) => Customize( fixture );
+
+		protected abstract void Customize( IFixture fixture );
+	}
+}
