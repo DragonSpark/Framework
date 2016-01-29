c59b8d7c73f65fb419a652f4e26850a1acfce29a
MichaelD! <michael@dragonspark.us>
2016-01-29 15:16:25 -0500

    Nuget Updates.

--- DragonSpark/Extensions/TypeExtensions.cs
+++ DragonSpark/Extensions/TypeExtensions.cs
@@ -1,16 +1,27 @@
 ﻿using DragonSpark.Runtime.Values;
 using DragonSpark.TypeSystem;
-using PostSharp.Patterns.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using PostSharp.Patterns.Contracts;
 using Type = System.Type;
 
 namespace DragonSpark.Extensions
 {
 	public static class TypeExtensions
 	{
+		public static Type GetMemberType(this MemberInfo memberInfo)
+		{
+		  if (memberInfo is MethodInfo)
+			return ((MethodInfo) memberInfo).ReturnType;
+		  if (memberInfo is PropertyInfo)
+			return ((PropertyInfo) memberInfo).PropertyType;
+		  if (memberInfo is FieldInfo)
+			return ((FieldInfo) memberInfo).FieldType;
+		  return (Type) null;
+		}
+
 		public static Assembly[] Assemblies( [Required] this IEnumerable<Type> @this ) => @this.Select( x => x.Assembly() ).ToArray();
 
 		public static TypeAdapter Adapt( [Required]this Type @this ) => new AssociatedValue<TypeAdapter>( @this, () => new TypeAdapter( @this ) ).Item;
