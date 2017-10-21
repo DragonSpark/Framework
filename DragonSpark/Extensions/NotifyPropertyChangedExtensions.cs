01ba24df26b0f108b76917ed5ad2b21f7b4cd380
John Tasler <JohnTasler@users.noreply.github.com>
2017-10-21 06:37:00 -0700

    Fixed the namespace casing

--- DragonSpark/Extensions/NotifyPropertyChangedExtensions.cs
+++ DragonSpark/Extensions/NotifyPropertyChangedExtensions.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace Dragonspark.Extensions
+namespace DragonSpark.Extensions
 {
 	public static class NotifyPropertyChangedExtensions
 	{
