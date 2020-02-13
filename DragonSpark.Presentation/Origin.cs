714c39a78b978b5e51c7a569e60359cfe30b32ee
Mike-EEE <michael@dragonspark.network>
2020-02-13 11:48:48 -0500

    Added Origin url component.

---
+++ DragonSpark.Presentation/Origin.cs
@@ -0,0 +1,18 @@
+﻿using DragonSpark.Model.Results;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Net;
+
+namespace DragonSpark.Presentation
+{
+	public sealed class Origin : IResult<string>
+	{
+		public static implicit operator string(Origin instance) => instance.Get();
+
+		readonly NavigationManager _manager;
+
+		public Origin(NavigationManager manager) => _manager = manager;
+
+		public string Get() => WebUtility.UrlEncode(new Uri(_manager.Uri).PathAndQuery);
+	}
+}
