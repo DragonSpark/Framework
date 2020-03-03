f9b9af75a38576946d1ab20c5cc045021d96d32f
Mike-EEE <michael@dragonspark.network>
2020-03-03 05:21:30 -0500

    ContentView.

---
+++ DragonSpark.Presentation/Components/ContentView.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace DragonSpark.Presentation.Components
+{
+	public sealed class ContentView<TValue> : ComponentBase
+	{
+		protected override void BuildRenderTree(RenderTreeBuilder builder)
+		{
+			if (Content is null)
+			{
+				builder.AddContent(2, NotFound);
+			}
+			else
+			{
+				builder.AddContent(1, ChildContent(Content));
+			}
+		}
+
+		[Parameter, UsedImplicitly]
+		public TValue Content { get; set; }
+
+		[Parameter, UsedImplicitly]
+		public RenderFragment<TValue> ChildContent { get; set; }
+
+		[Parameter]
+		public RenderFragment NotFound { get; set; } = x => x.AddContent(2, "Not found.");
+	}
+}
