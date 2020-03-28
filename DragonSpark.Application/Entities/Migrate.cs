17bdd85ef21455916d3c6564ccd1854df875ef14
Mike-EEE <michael@dragonspark.network>
2020-03-28 04:58:09 -0400

    Entity initializer adjustments.

---
+++ DragonSpark.Application/Entities/Migrate.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace DragonSpark.Application.Entities
+{
+	public sealed class Migrate<T> : IInitializer<T> where T : DbContext
+	{
+		[UsedImplicitly]
+		public static Migrate<T> Default { get; } = new Migrate<T>();
+
+		Migrate() {}
+
+		public T Get(T parameter)
+		{
+			parameter.Database.Migrate();
+			return parameter;
+		}
+	}
+}
