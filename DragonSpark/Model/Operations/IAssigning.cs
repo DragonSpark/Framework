4da09e3c6d02842b5384b168f73e9f28cc66f509
Mike-E <michael@dragonspark.network>
2020-06-13 08:34:35 -0400

    Class organization.

---
+++ DragonSpark/Model/Operations/IAssigning.cs
@@ -0,0 +1,4 @@
+﻿namespace DragonSpark.Model.Operations
+{
+	public interface IAssigning<TKey, TValue> : IResulting<(TKey Key, TValue Value)> {}
+}
--- DragonSpark/Model/Operations/IDepending.cs
+++ DragonSpark/Model/Operations/IDepending.cs
@@ -1,6 +1,4 @@
 ﻿namespace DragonSpark.Model.Operations
 {
-	public interface IAssigning<TKey, TValue> : IResulting<(TKey Key, TValue Value)> {}
-
 	public interface IDepending<in T> : ISelecting<T, bool> {}
 }
