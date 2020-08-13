6409fb5c15726ff6a5f922b3048a0e8fc3e135a2
Mike-E <michael@dragonspark.network>
2020-08-13 05:52:34 -0400

    File-per-class.

---
+++ DragonSpark.Application/DefaultUniqueResourceName.cs
@@ -0,0 +1,9 @@
+﻿namespace DragonSpark.Application
+{
+	public sealed class DefaultUniqueResourceName : UniqueResourceName
+	{
+		public static DefaultUniqueResourceName Default { get; } = new DefaultUniqueResourceName();
+
+		DefaultUniqueResourceName() : base(Sluggy.Sluggy.DefaultSeparator, Sluggy.Sluggy.DefaultTranslationStrategy) {}
+	}
+}
--- DragonSpark.Application/UniqueResourceName.cs
+++ DragonSpark.Application/UniqueResourceName.cs
@@ -4,13 +4,6 @@ using Sluggy.Strategies;
 
 namespace DragonSpark.Application
 {
-	public sealed class DefaultUniqueResourceName : UniqueResourceName
-	{
-		public static DefaultUniqueResourceName Default { get; } = new DefaultUniqueResourceName();
-
-		DefaultUniqueResourceName() : base(Sluggy.Sluggy.DefaultSeparator, Sluggy.Sluggy.DefaultTranslationStrategy) {}
-	}
-
 	public class UniqueResourceName : IAlteration<string>
 	{
 		readonly string               _separator;
