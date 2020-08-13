191f9aca5234fa931e577e8f7305342d2f5ec868
Mike-E <michael@dragonspark.network>
2020-08-13 11:27:23 -0400

    Introduced AllocatedOperation.

---
+++ DragonSpark/Model/Operations/AllocatedOperation.cs
@@ -0,0 +1,14 @@
+﻿using DragonSpark.Model.Selection;
+using JetBrains.Annotations;
+using System;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Model.Operations
+{
+	public class AllocatedOperation<T> : Select<T, Task>
+	{
+		public AllocatedOperation([NotNull] ISelect<T, Task> @select) : base(@select) {}
+
+		public AllocatedOperation([NotNull] Func<T, Task> @select) : base(@select) {}
+	}
+}
