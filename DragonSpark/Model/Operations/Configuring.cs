a114802849c38de292552bdf13518034fd0123a0
Mike-E <michael@dragonspark.network>
2020-08-17 09:43:00 -0400

    Introduced Configuring.

---
+++ DragonSpark/Model/Operations/Configuring.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace DragonSpark.Model.Operations
+{
+	public class Configuring<TSource, TResult> : ISelecting<TSource, TResult>
+	{
+		readonly Await<TSource, TResult>     _select;
+		readonly Operate<(TSource, TResult)> _configure;
+
+		public Configuring(Await<TSource, TResult> select, Operate<(TSource, TResult)> configure)
+		{
+			_select    = select;
+			_configure = configure;
+		}
+
+		public async ValueTask<TResult> Get(TSource parameter)
+		{
+			var result = await _select(parameter);
+			await _configure((parameter, result));
+			return result;
+		}
+	}
+}
