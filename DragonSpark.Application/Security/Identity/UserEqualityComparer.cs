e5256f986a858121ac5d125b4482710856d2616b
Mike-E <michael@dragonspark.network>
2020-08-19 08:54:52 -0400

    Attended to messages.

--- DragonSpark.Application/Security/Identity/UserEqualityComparer.cs
+++ DragonSpark.Application/Security/Identity/UserEqualityComparer.cs
@@ -11,8 +11,9 @@ namespace DragonSpark.Application.Security.Identity
 
 		public bool Equals(IdentityUser? x, IdentityUser? y)
 			=> ReferenceEquals(x, y)
-			   || !ReferenceEquals(x, null)
-			   && !ReferenceEquals(y, null)
+			   ||
+			   x != null
+			   && y != null
 			   && string.Equals(x.UserName, y.UserName, StringComparison.OrdinalIgnoreCase);
 
 		public int GetHashCode(IdentityUser obj) => StringComparer.OrdinalIgnoreCase.GetHashCode(obj.UserName);
