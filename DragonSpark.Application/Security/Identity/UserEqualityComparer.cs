77c030ae607b475ef4af42b51fa3cd83e7f087c8
Mike-E <michael@dragonspark.network>
2020-08-19 05:34:50 -0400

    Basic User/Identity comparer.

---
+++ DragonSpark.Application/Security/Identity/UserEqualityComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DragonSpark.Application.Security.Identity
+{
+	public sealed class UserEqualityComparer : IEqualityComparer<IdentityUser>
+	{
+		public static UserEqualityComparer Default { get; } = new UserEqualityComparer();
+
+		UserEqualityComparer() {}
+
+		public bool Equals(IdentityUser? x, IdentityUser? y)
+			=> ReferenceEquals(x, y)
+			   || !ReferenceEquals(x, null)
+			   && !ReferenceEquals(y, null)
+			   && string.Equals(x.UserName, y.UserName, StringComparison.OrdinalIgnoreCase);
+
+		public int GetHashCode(IdentityUser obj) => StringComparer.OrdinalIgnoreCase.GetHashCode(obj.UserName);
+	}
+}
