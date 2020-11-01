60af571b9c8f9f2389bb1572956a343dbafe479b
Mike-E <michael@dragonspark.network>
2020-11-01 05:48:03 -0500

    Additional content views.

---
+++ DragonSpark.Presentation/Components/QueryableContentView.cs
@@ -0,0 +1,34 @@
+﻿using DragonSpark.Model;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DragonSpark.Presentation.Components
+{
+	public sealed class QueryableContentView<TItem> : ComponentBase
+	{
+		protected override void BuildRenderTree(RenderTreeBuilder builder)
+		{
+			var queryable = Content ?? Empty.Array<TItem>().AsQueryable();
+			if (!queryable.Any())
+			{
+				builder.AddContent(2, NotFoundTemplate);
+			}
+			else if (ChildContent != null)
+			{
+				builder.AddContent(1, ChildContent(queryable));
+			}
+		}
+
+		[Parameter, UsedImplicitly]
+		public IQueryable<TItem>? Content { get; set; } = default!;
+
+		[Parameter, UsedImplicitly]
+		public RenderFragment<IEnumerable<TItem>>? ChildContent { get; set; }
+
+		[Parameter]
+		public RenderFragment NotFoundTemplate { get; set; } = x => x.AddContent(2, "No elements found.");
+	}
+}
