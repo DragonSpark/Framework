8629c7a46e3de4a1a8e95273ef313d6e5a40c6d7
Mike-E <michael@dragonspark.network>
2020-11-03 07:29:03 -0500

    Saved file. :P

--- DragonSpark.Presentation/Components/ActiveView.cs
+++ DragonSpark.Presentation/Components/ActiveView.cs
@@ -1,32 +1,9 @@
-﻿using AsyncUtilities;
-using DragonSpark.Model.Operations;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
-using System;
 using System.Threading.Tasks;
 
 namespace DragonSpark.Presentation.Components
 {
-	public class ProtectedActiveView<TValue> : ActiveView<TValue>
-	{
-		readonly Func<object, AsyncLock> _lock;
-
-		public ProtectedActiveView() : this(Locks.Default.Get) {}
-
-		public ProtectedActiveView(Func<object, AsyncLock> @lock) => _lock = @lock;
-
-		protected override async Task OnParametersSetAsync()
-		{
-			using (await _lock(Receiver).LockAsync())
-			{
-				await base.OnParametersSetAsync();
-			}
-		}
-
-		[Parameter]
-		public object Receiver { get; set; } = default!;
-	}
-
 	public class ActiveView<TValue> : ComponentBase
 	{
 		protected override async Task OnParametersSetAsync()
