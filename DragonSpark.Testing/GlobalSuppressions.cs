266513cf1f262ca09eff8fd8db56b0dc6c9f2f05
Mike-E <michael@dragonspark.network>
2020-11-08 08:33:21 -0500

    Attended to messages.

--- DragonSpark.Testing/GlobalSuppressions.cs
+++ DragonSpark.Testing/GlobalSuppressions.cs
@@ -1,13 +1,10 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Performance", "EPS06:Hidden struct copy operation", Justification           = "<Pending>")]
+[assembly: SuppressMessage("Performance", "EPS06:Hidden struct copy operation", Justification = "<Pending>")]
 [assembly: SuppressMessage("Bug", "ERP001:Non-observed return value of the pure method.", Justification = "<Pending>")]
 
 [assembly:
 	SuppressMessage("Performance", "EPS06:Hidden struct copy operation", Justification = "<Pending>", Scope = "module")]
-[assembly:
-	SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member",
-	                Target =
-		                "~M:DragonSpark.Testing.Application.ValueIterationBenchmarks.Call(System.Int32)")]
 [assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:DragonSpark.Testing.IterationBenchmarks.Call(System.String)")]
 [assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:DragonSpark.Testing.ValueIterationBenchmarks.Call(System.Int32)")]
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "<Pending>", Scope = "member", Target = "~F:DragonSpark.Testing.Composition.CompositionTests.Service._dependency")]
