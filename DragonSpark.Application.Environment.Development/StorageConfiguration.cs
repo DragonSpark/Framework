293af47edcb358cee19e237d74515e687515967c
Mike-E <michael@dragonspark.network>
2020-11-11 01:48:32 -0500

    EnableDetailedErrors

--- DragonSpark.Application.Environment.Development/StorageConfiguration.cs
+++ DragonSpark.Application.Environment.Development/StorageConfiguration.cs
@@ -2,11 +2,11 @@
 
 namespace DragonSpark.Application.Environment
 {
-	public sealed class StorageConfiguration : DragonSpark.Application.Compose.Entities.StorageConfiguration
+	public sealed class StorageConfiguration : Compose.Entities.StorageConfiguration
 	{
 		[UsedImplicitly]
 		public static StorageConfiguration Default { get; } = new StorageConfiguration();
 
-		StorageConfiguration() : base(x => x.EnableSensitiveDataLogging()) {}
+		StorageConfiguration() : base(x => x.EnableSensitiveDataLogging().EnableDetailedErrors()) {}
 	}
 }
