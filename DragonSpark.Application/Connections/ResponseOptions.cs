d583d4eb202eae4ba3d7c8c22aba92be4dcd4fa1
Mike-E <michael@dragonspark.network>
2020-12-01 10:49:39 -0500

    ResponseOptions.cs

---
+++ DragonSpark.Application/Connections/ResponseOptions.cs
@@ -0,0 +1,25 @@
+﻿using DragonSpark.Compose;
+using DragonSpark.Model.Commands;
+using DragonSpark.Model.Sequences;
+using Microsoft.AspNetCore.ResponseCompression;
+using System.Linq;
+
+namespace DragonSpark.Application.Connections
+{
+	public sealed class ResponseOptions : ICommand<ResponseCompressionOptions>
+	{
+		public static ResponseOptions Default { get; } = new ResponseOptions();
+
+		ResponseOptions() : this(ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" })
+		                                                    .Result()) {}
+
+		readonly Array<string> _types;
+
+		public ResponseOptions(Array<string> types) => _types = types;
+
+		public void Execute(ResponseCompressionOptions parameter)
+		{
+			parameter.MimeTypes = _types.Open();
+		}
+	}
+}
