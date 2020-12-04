40cf4d60b2d6812970e31a6d92abf4fa9bf98377
Mike-E <michael@dragonspark.network>
2020-12-04 16:05:40 -0500

    Delegated inverse condition.

--- DragonSpark/Model/Selection/Conditions/InverseCondition.cs
+++ DragonSpark/Model/Selection/Conditions/InverseCondition.cs
@@ -1,13 +1,16 @@
 ﻿using DragonSpark.Runtime.Activation;
+using System;
 
 namespace DragonSpark.Model.Selection.Conditions
 {
 	public class InverseCondition<T> : ICondition<T>, IActivateUsing<ICondition<T>>
 	{
-		readonly ICondition<T> _condition;
+		readonly Func<T, bool> _condition;
 
-		public InverseCondition(ICondition<T> inner) => _condition = inner;
+		public InverseCondition(ICondition<T> condition) : this(condition.Get) {}
 
-		public bool Get(T parameter) => !_condition.Get(parameter);
+		public InverseCondition(Func<T, bool> inner) => _condition = inner;
+
+		public bool Get(T parameter) => !_condition(parameter);
 	}
 }
