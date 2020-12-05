6d0c943c2833ce53e88dd8d97402d9b9e89b7539
Mike-E <michael@dragonspark.network>
2020-12-05 05:23:04 -0500

    Added file.

---
+++ DragonSpark/Reflection/PrivateInstanceFlags.cs
@@ -0,0 +1,12 @@
+﻿using DragonSpark.Model.Results;
+using System.Reflection;
+
+namespace DragonSpark.Reflection
+{
+	public sealed class PrivateInstanceFlags : Instance<BindingFlags>
+	{
+		public static PrivateInstanceFlags Default { get; } = new PrivateInstanceFlags();
+
+		PrivateInstanceFlags() : base(BindingFlags.NonPublic | BindingFlags.Instance) {}
+	}
+}
