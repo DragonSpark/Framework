47de5fb742c668992a4577b7fe25e553df9c7edb
Mike-E <michael@dragonspark.network>
2020-12-07 14:16:26 -0500

    Query instance.

--- DragonSpark.Application/Entities/Query.cs
+++ DragonSpark.Application/Entities/Query.cs
@@ -1,7 +1,14 @@
-﻿using System;
+﻿using DragonSpark.Model.Results;
+using System;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace DragonSpark.Application.Entities
 {
+	public class Query<T> : Instance<IQueryable<T>>
+	{
+		public Query(IQueryable<T> instance) : base(instance) {}
+	}
+
 	public delegate Expression<Func<TEntity, bool>> Query<in TKey, TEntity>(TKey parameter);
 }
