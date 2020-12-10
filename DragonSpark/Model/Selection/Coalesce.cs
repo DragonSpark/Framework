0b554f495273f552e2c1b017108e209d575eb299
Mike-E <michael@dragonspark.network>
2020-12-10 01:19:44 -0500

    Added Coalesce

---
+++ DragonSpark/Model/Selection/Coalesce.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DragonSpark.Model.Selection
+{
+	public class Coalesce<TIn, TOut> : ISelect<TIn, TOut> where TOut : class
+	{
+		readonly Func<TIn, TOut?> _first;
+		readonly Func<TIn, TOut>  _second;
+
+		public Coalesce(ISelect<TIn, TOut?> first, ISelect<TIn, TOut> second) : this(first.Get, second.Get) {}
+
+		public Coalesce(Func<TIn, TOut?> first, Func<TIn, TOut> second)
+		{
+			_first  = first;
+			_second = second;
+		}
+
+		public TOut Get(TIn parameter) => _first(parameter) ?? _second(parameter);
+	}
+}
