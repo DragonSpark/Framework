a89338bc56f854900cd3c9781a2893773da939ef
Mike-E <michael@dragonspark.network>
2020-12-12 11:29:15 -0500

    Removed unused/replaced classes

--- DragonSpark.Application/Entities/Generation/ISome.cs
+++
@@ -1,8 +0,0 @@
-﻿using Bogus;
-using DragonSpark.Model.Selection;
-
-namespace DragonSpark.Application.Entities.Generation
-{
-	// TODO: remove
-	public interface ISome<T> : ISelect<Seeding<T>, Faker<T>> where T : class {}
-}
