ffa540377a6346b19afc4bfa912f4b680fa2d07f
Mike-E <michael@dragonspark.network>
2020-12-21 11:01:07 -0500

    Resulting

---
+++ DragonSpark/Model/Operations/Resulting.cs
@@ -0,0 +1,14 @@
+﻿using DragonSpark.Compose;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Model.Operations
+{
+	public class Resulting<T> : IResulting<T>
+	{
+		readonly T _instance;
+
+		public Resulting(T instance) => _instance = instance;
+
+		public ValueTask<T> Get() => _instance.ToOperation();
+	}
+}
