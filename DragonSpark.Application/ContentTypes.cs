8a27fa2320e2d7e5c432a8296465e7d34ae3eeb8
Mike-E <michael@dragonspark.network>
2020-12-23 02:10:07 -0500

    ContentTypes

---
+++ DragonSpark.Application/ContentTypes.cs
@@ -0,0 +1,18 @@
+﻿using DragonSpark.Model.Selection.Alterations;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace DragonSpark.Application
+{
+	public sealed class ContentTypes : IAlteration<string>
+	{
+		public static ContentTypes Default { get; } = new ContentTypes();
+
+		ContentTypes() : this(new FileExtensionContentTypeProvider()) {}
+
+		readonly IContentTypeProvider _provider;
+
+		public ContentTypes(IContentTypeProvider provider) => _provider = provider;
+
+		public string Get(string parameter)
+			=> _provider.TryGetContentType(parameter, out var contentType) ? contentType : "application/octet-stream";
+	}}
