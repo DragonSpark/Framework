7e99cdf0b1dc287bc560d1824f20b4f92f0d802a
Mike-E <michael@dragonspark.network>
2021-01-08 10:24:50 -0500

    SumSelected

---
+++ DragonSpark.Application/Entities/SumSelected.cs
@@ -0,0 +1,33 @@
+﻿using DragonSpark.Model.Operations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Application.Entities
+{
+	public class SumSelected<T> : SumSelected<T, T>
+	{
+		public SumSelected(IQueryable<T> queryable, Query<T, T> query, Expression<Func<T, decimal>> select)
+			: base(queryable, query, select) {}
+	}
+
+	public class SumSelected<TKey, TEntity> : ISelecting<TKey, decimal>
+	{
+		readonly IQueryable<TEntity>                _queryable;
+		readonly Query<TKey, TEntity>               _query;
+		readonly Expression<Func<TEntity, decimal>> _select;
+
+		public SumSelected(IQueryable<TEntity> queryable, Query<TKey, TEntity> query,
+		                   Expression<Func<TEntity, decimal>> select)
+		{
+			_queryable = queryable;
+			_query     = query;
+			_select    = select;
+		}
+
+		public async ValueTask<decimal> Get(TKey parameter)
+			=> await _queryable.Where(_query(parameter)).Select(_select).SumAsync().ConfigureAwait(false);
+	}
+}
