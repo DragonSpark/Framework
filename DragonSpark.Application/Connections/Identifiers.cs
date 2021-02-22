6579ae6a5612fc7f3b21cbf718a1b65b36d7c571
Mike-E <michael@dragonspark.network>
2021-02-22 13:56:55 -0500

    Hub adjustments

---
+++ DragonSpark.Application/Connections/Identifiers.cs
@@ -0,0 +1,19 @@
+﻿using DragonSpark.Model.Results;
+using DragonSpark.Model.Selection.Stores;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace DragonSpark.Application.Connections
+{
+	public sealed class Identifiers
+		: DelegatedSelection<ConcurrentDictionary<string, List<UserConnection>>, ITable<string, List<UserConnection>>>
+	{
+		public static Identifiers Default { get; } = new Identifiers();
+
+		Identifiers()
+			: base(new ConcurrentTables<string, List<UserConnection>>(_ => new List<UserConnection>()).Get,
+			       () => new ConcurrentDictionary<string, List<UserConnection>>(StringComparer
+				                                                                    .InvariantCultureIgnoreCase)) {}
+	}
+}
--- DragonSpark.Application/Connections/UserAwareHub.cs
+++ DragonSpark.Application/Connections/UserAwareHub.cs
@@ -1,35 +1,73 @@
 ﻿using DragonSpark.Compose;
+using DragonSpark.Model;
 using DragonSpark.Model.Selection.Stores;
 using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DragonSpark.Application.Connections
 {
+	public sealed class UserConnection
+	{
+		public UserConnection(string user, string connection)
+		{
+			User       = user;
+			Connection = connection;
+		}
+
+		public string User { get; }
+
+		public string Connection { get; }
+
+		public void Deconstruct(out string user, out string connection)
+		{
+			user       = User;
+			connection = Connection;
+		}
+	}
+
 	public class UserAwareHub : Hub
 	{
-		readonly ITable<string, string> _names;
+		readonly ITable<string, List<UserConnection>> _names;
 
-		public UserAwareHub(ITable<string, string> names) => _names = names;
+		protected UserAwareHub() : this(Connections.Identifiers.Default.Get()) {}
+
+		protected UserAwareHub(ITable<string, List<UserConnection>> names) => _names = names;
 
 		public override Task OnConnectedAsync()
 		{
 			var name = Context.User?.Identity?.Name;
 			if (name != null)
 			{
-				_names.Assign(name, Context.UserIdentifier.Verify());
+				var list = _names.Get(name);
+				lock (list)
+				{
+					list.Add(new UserConnection(Context.UserIdentifier.Verify(), Context.ConnectionId));
+				}
 			}
 
 			return base.OnConnectedAsync();
 		}
+
+		protected IClientProxy Identifiers(string user)
+			=> Clients.Users(_names.IsSatisfiedBy(user)
+				                 ? _names.Get(user).Select(x => x.User).Distinct()
+				                 : Empty.Enumerable<string>());
 
 		public override Task OnDisconnectedAsync(Exception? exception)
 		{
 			var name = Context.User?.Identity?.Name;
 			if (name != null)
 			{
-				_names.Remove(name);
+				var list = _names.Get(name);
+				lock (list)
+				{
+					list.RemoveAll(x => x.User == Context.UserIdentifier && x.Connection == Context.ConnectionId);
+				}
 			}
+
 			return base.OnDisconnectedAsync(exception);
 		}
 	}
