afed8004aabd04f9ffa5d81e9199bceb167f8890
Mike-E <michael@dragonspark.network>
2021-02-26 03:40:18 -0500

    Attribution/tests

---
+++ DragonSpark.Application.Testing/Security/Data/AddressMaskTests.cs
@@ -0,0 +1,17 @@
+﻿using DragonSpark.Application.Security.Data;
+using FluentAssertions;
+using Xunit;
+
+namespace DragonSpark.Application.Testing.Security.Data
+{
+	public sealed class AddressMaskTests
+	{
+		[Fact]
+		public void Verify()
+		{
+			AddressMask.Default.Get("some@email.com").Should().Be("s**e@e***l.com");
+			AddressMask.Default.Get("some@somedomain.com").Should().Be("s**e@s********n.com");
+
+		}
+	}
+}
--- DragonSpark.Application/Security/Data/AddressMask.cs
+++ DragonSpark.Application/Security/Data/AddressMask.cs
@@ -3,21 +3,26 @@ using System.Text.RegularExpressions;
 
 namespace DragonSpark.Application.Security.Data
 {
+	// ATTRIBUTION: https://stackoverflow.com/a/47637410/10340424
 	public sealed class AddressMask : IAlteration<string>
 	{
 		public static AddressMask Default { get; } = new AddressMask();
 
-		AddressMask() : this(@"(?<=[\w]{1})[\w-\._\+%\\]*(?=[\w]{1}@)|(?<=@[\w]{1})[\w-_\+%]*(?=\.)") {}
+		AddressMask() : this(@"(?:(?:^|(?<=@))([^.@])|\G(?!\A))[^.@](?:([^.@])(?=[.@]))?", @"$1*$2") {}
 
-		readonly string _pattern;
+		readonly string _pattern, _substitution;
 
-		public AddressMask(string pattern) => _pattern = pattern;
+		public AddressMask(string pattern, string substitution)
+		{
+			_pattern      = pattern;
+			_substitution = substitution;
+		}
 
 		public string Get(string parameter)
 			=> parameter.Contains("@")
 				   ? parameter.Split('@')[0].Length < 4
 					     ? @"*@*.*"
-					     : Regex.Replace(parameter, _pattern, m => new string('*', m.Length))
+					     : Regex.Replace(parameter, _pattern, _substitution)
 				   : new string('*', parameter.Length);
 	}
 }
