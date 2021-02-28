3f277da04810197d602bacd5694593ca59cdf5f7
Mike-E <michael@dragonspark.network>
2021-02-28 03:55:50 -0500

    Default message template

---
+++ DragonSpark.Application/Messaging/MessageTemplate.cs
@@ -0,0 +1,28 @@
+﻿using DragonSpark.Compose;
+using DragonSpark.Model.Selection;
+using System;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Application.Messaging
+{
+	public class MessageTemplate<T> : IMessageTemplate<T>
+	{
+		readonly Func<T, string> _to, _title, _template;
+
+		public MessageTemplate(string title, Func<T, string> to, ISelect<T, string> template)
+			: this(title, to, template.Get) {}
+
+		public MessageTemplate(string title, Func<T, string> to, Func<T, string> template)
+			: this(to, title.Accept, template) {}
+
+		public MessageTemplate(Func<T, string> to, Func<T, string> title, Func<T, string> template)
+		{
+			_to       = to;
+			_title    = title;
+			_template = template;
+		}
+
+		public ValueTask<Message> Get(T parameter)
+			=> new Message(_to(parameter), _title(parameter), _template(parameter)).ToOperation();
+	}
+}
