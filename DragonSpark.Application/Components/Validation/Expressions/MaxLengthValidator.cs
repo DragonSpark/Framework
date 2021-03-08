b72c0fafff753fb19c3f724dc528ad382b275d6a
Mike-E <michael@dragonspark.network>
2021-03-08 04:32:53 -0500

    Application additions

---
+++ DragonSpark.Application/Components/Validation/Expressions/MaxLengthValidator.cs
@@ -0,0 +1,12 @@
+﻿using DragonSpark.Compose;
+using System.ComponentModel.DataAnnotations;
+
+namespace DragonSpark.Application.Components.Validation.Expressions
+{
+	public sealed class MaxLengthValidator : MetadataValueValidator
+	{
+		public MaxLengthValidator(uint length) : this(new MaxLengthAttribute(length.Degrade())) {}
+
+		public MaxLengthValidator(ValidationAttribute metadata) : base(metadata) {}
+	}
+}
