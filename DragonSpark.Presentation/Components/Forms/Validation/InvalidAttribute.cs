80b065771f93d86e53ddc6e335b6b95499dc36da
Mike-E <michael@dragonspark.network>
2021-03-16 11:37:30 -0400

    Invalid context

---
+++ DragonSpark.Presentation/Components/Forms/Validation/InvalidAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DragonSpark.Presentation.Components.Forms.Validation
+{
+	public sealed class InvalidAttribute : ValidationAttribute
+	{
+		public override bool IsValid(object? value) => false;
+	}
+}
---
+++ DragonSpark.Presentation/Components/Forms/Validation/InvalidContext.cs
@@ -0,0 +1,12 @@
+﻿namespace DragonSpark.Presentation.Components.Forms.Validation
+{
+	public sealed class InvalidContext
+	{
+		public static InvalidContext Default { get; } = new InvalidContext();
+
+		InvalidContext() {}
+
+		[Invalid]
+		public string Invalid { get; set; } = default!;
+	}
+}
