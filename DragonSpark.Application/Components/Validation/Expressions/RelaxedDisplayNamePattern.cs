c0df47be8e5db0e10d49785671d925f23d236ecc
Mike-E <michael@dragonspark.network>
2021-03-21 04:38:40 -0400

    Pattern addition

--- DragonSpark.Application/Components/Validation/Expressions/RelaxedDisplayNamePattern.cs
+++ DragonSpark.Application/Components/Validation/Expressions/RelaxedDisplayNamePattern.cs
@@ -1,14 +1,5 @@
 ﻿namespace DragonSpark.Application.Components.Validation.Expressions
 {
-	public sealed class DisplayNamePattern : Expression
-	{
-		public const string Expression = "[a-zA-Z0-9- _]";
-
-		public static DisplayNamePattern Default { get; } = new DisplayNamePattern();
-
-		DisplayNamePattern() : base(Expression) {}
-	}
-
 	public sealed class RelaxedDisplayNamePattern : Expression
 	{
 		public const string Expression = "[a-zA-Z0-9- _.*@]";
