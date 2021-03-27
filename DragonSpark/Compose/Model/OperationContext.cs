18085cb2db6f6cad6f4aae18ea23c7805a00f13a
Mike-E <michael@dragonspark.network>
2021-03-27 12:22:30 -0400

    Appended

--- DragonSpark/Compose/Model/OperationContext.cs
+++ DragonSpark/Compose/Model/OperationContext.cs
@@ -23,6 +23,10 @@ namespace DragonSpark.Compose.Model
 		public OperationContext<T> Append(Await<T> command)
 			=> new OperationContext<T>(new Appended<T>(Get().Await, command));
 
+		public OperationContext<T> Append(IOperation command) => Append(command.Await);
+		public OperationContext<T> Append(Await command)
+			=> new OperationContext<T>(new AppendedTo<T>(Get().Await, command));
+
 		public LogOperationContext<T, TParameter> Bind<TParameter>(ILogMessage<TParameter> log)
 			=> new LogOperationContext<T, TParameter>(_subject, log);
 
--- DragonSpark/Model/Operations/Appended.cs
+++ DragonSpark/Model/Operations/Appended.cs
@@ -40,4 +40,21 @@ namespace DragonSpark.Model.Operations
 		}
 	}
 
+	public class AppendedTo<T> : IOperation<T>
+	{
+		readonly Await<T> _first;
+		readonly Await    _second;
+
+		public AppendedTo(Await<T> first, Await second)
+		{
+			_first  = first;
+			_second = second;
+		}
+
+		public async ValueTask Get(T parameter)
+		{
+			await _first(parameter);
+			await _second();
+		}
+	}
 }
