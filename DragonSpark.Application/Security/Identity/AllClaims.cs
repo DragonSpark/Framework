f81545083a34a7c7cb313568cd8b8b43b6633a70
Mike-E <michael@dragonspark.network>
2021-03-27 01:48:13 -0400

    Basic claims

---
+++ DragonSpark.Application/Security/Identity/AllClaims.cs
@@ -0,0 +1,9 @@
+﻿namespace DragonSpark.Application.Security.Identity
+{
+	public sealed class AllClaims : Claims
+	{
+		public static AllClaims Default { get; } = new AllClaims();
+
+		AllClaims() : base(_ => true) {}
+	}
+}
---
+++ DragonSpark.Application/Security/Identity/EmptyClaims.cs
@@ -0,0 +1,9 @@
+﻿namespace DragonSpark.Application.Security.Identity
+{
+	public sealed class EmptyClaims : Claims
+	{
+		public static EmptyClaims Default { get; } = new EmptyClaims();
+
+		EmptyClaims() : base(_ => false) {}
+	}
+}
