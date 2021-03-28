d2b945d441f05f7b9948b1c9bdf663e8794346d7
Mike-E <michael@dragonspark.network>
2021-03-28 11:49:57 -0400

    Class-per-file-per-class

---
+++ DragonSpark/Diagnostics/PolicyAwareOperation.cs
@@ -0,0 +1,26 @@
+﻿using DragonSpark.Compose;
+using DragonSpark.Compose.Model;
+using DragonSpark.Model.Operations;
+using Polly;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Diagnostics
+{
+	public class PolicyAwareOperation<T> : IOperation<T>
+	{
+		readonly OperationContext<T> _previous;
+		readonly IAsyncPolicy        _policy;
+
+		public PolicyAwareOperation(IOperation<T> previous, IAsyncPolicy policy)
+			: this(previous.Then(), policy) {}
+
+		public PolicyAwareOperation(OperationContext<T> previous, IAsyncPolicy policy)
+		{
+			_previous = previous;
+			_policy   = policy;
+		}
+
+		public ValueTask Get(T parameter)
+			=> _policy.ExecuteAsync(_previous.Bind(parameter).Select(x => x.AsTask()).Get().Get).ToOperation();
+	}
+}
--- DragonSpark/Diagnostics/PolicyAwareSelecting.cs
+++ DragonSpark/Diagnostics/PolicyAwareSelecting.cs
@@ -23,22 +23,4 @@ namespace DragonSpark.Diagnostics
 		public ValueTask<TResult> Get(TIn parameter)
 			=> _policy.ExecuteAsync(_previous.Bind(parameter).Select(x => x.AsTask()).Get().Get).ToOperation();
 	}
-
-	public class PolicyAwareOperation<T> : IOperation<T>
-	{
-		readonly OperationContext<T> _previous;
-		readonly IAsyncPolicy                          _policy;
-
-		public PolicyAwareOperation(IOperation<T> previous, IAsyncPolicy policy)
-			: this(previous.Then(), policy) {}
-
-		public PolicyAwareOperation(OperationContext<T> previous, IAsyncPolicy policy)
-		{
-			_previous = previous;
-			_policy   = policy;
-		}
-
-		public ValueTask Get(T parameter)
-			=> _policy.ExecuteAsync(_previous.Bind(parameter).Select(x => x.AsTask()).Get().Get).ToOperation();
-	}
 }
