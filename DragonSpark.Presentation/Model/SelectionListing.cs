46fc64e1931f3ef713830c3b960f284d5203c29a
Mike-E <michael@dragonspark.network>
2021-04-04 03:58:31 -0400

    Saved file :P

--- DragonSpark.Presentation/Model/SelectionListing.cs
+++ DragonSpark.Presentation/Model/SelectionListing.cs
@@ -1,7 +1,4 @@
-﻿using DragonSpark.Application.Runtime;
-using System.Collections.Generic;
-
-namespace DragonSpark.Presentation.Model
+﻿namespace DragonSpark.Presentation.Model
 {
 	public class SelectionListing : SelectionListing<string> {}
 
@@ -16,40 +13,5 @@ namespace DragonSpark.Presentation.Model
 		public string? Tag { get; set; }
 
 		public string Icon { get; set; } = default!;
-	}
-
-	public class SelectionListingCollection<T> : SelectedCollection<SelectionListing<T>>
-	{
-		readonly IEqualityComparer<T> _equality;
-
-		public SelectionListingCollection(IEnumerable<SelectionListing<T>> list)
-			: this(list, EqualityComparer<T>.Default) {}
-
-		public SelectionListingCollection(IEnumerable<SelectionListing<T>> list, IEqualityComparer<T> equality)
-			: base(list) => _equality = equality;
-
-		public T? Value
-		{
-			get => Selected != null ? Selected.Value : default;
-			set
-			{
-				if (value is not null)
-				{
-					foreach (var listing in this)
-					{
-						if (_equality.Equals(listing.Value, value))
-						{
-							Selected = listing;
-						}
-					}
-				}
-				else
-				{
-					Selected = default;
-				}
-
-
-			}
-		}
 	}
 }
