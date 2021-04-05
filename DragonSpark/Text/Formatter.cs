89c0b469ae6b17ee385a49f43633a8497245b5a7
Mike-E <michael@dragonspark.network>
2021-04-05 05:05:01 -0400

    Formatter base class

---
+++ DragonSpark/Text/Formatter.cs
@@ -0,0 +1,12 @@
+﻿using DragonSpark.Model.Selection;
+using System;
+
+namespace DragonSpark.Text
+{
+	public class Formatter<T> : Select<T, string>, IFormatter<T>
+	{
+		public Formatter(ISelect<T, string> @select) : base(@select) {}
+
+		public Formatter(Func<T, string> @select) : base(@select) {}
+	}
+}
