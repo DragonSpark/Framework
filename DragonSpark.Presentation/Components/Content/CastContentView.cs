a9bc2989e7d3540ff4ea5c0b84b0eeed73ef5e91
Mike-E <michael@dragonspark.network>
2021-04-11 12:43:42 -0400

    CastContentView

---
+++ DragonSpark.Presentation/Components/Content/CastContentView.cs
@@ -0,0 +1,41 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace DragonSpark.Presentation.Components.Content
+{
+	public sealed class CastContentView<TContent> : ComponentBase
+	{
+		protected override void BuildRenderTree(RenderTreeBuilder builder)
+		{
+			if (Content is null)
+			{
+				builder.AddContent(1, NotFoundTemplate);
+			}
+			else if (ChildContent != null)
+			{
+				if (Content is TContent content)
+				{
+					builder.AddContent(2, ChildContent(content));
+				}
+				else
+				{
+					builder.AddContent(3, NotCompatibleTemplate);
+
+				}
+			}
+		}
+
+		[Parameter, UsedImplicitly]
+		public object? Content { get; set; }
+
+		[Parameter, UsedImplicitly]
+		public RenderFragment<TContent>? ChildContent { get; set; }
+
+		[Parameter]
+		public RenderFragment NotFoundTemplate { get; set; } = x => x.AddContent(2, "Not found.");
+
+		[Parameter]
+		public RenderFragment NotCompatibleTemplate { get; set; } = x => x.AddContent(2, "The provided content is not compatible with this view.");
+	}
+}
