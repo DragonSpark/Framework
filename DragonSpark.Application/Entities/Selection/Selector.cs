d6c5589aa34c2f8e770c4494a26260583f15db4b
Mike-E <michael@dragonspark.network>
2021-04-18 04:44:49 -0400

    Selector/s

---
+++ DragonSpark.Application/Entities/Selection/Selector.cs
@@ -0,0 +1,14 @@
+﻿using DragonSpark.Model.Results;
+using System;
+using System.Linq.Expressions;
+
+namespace DragonSpark.Application.Entities.Selection
+{
+	public class Selector<TIn, TOut> : Instance<Expression<Func<TIn, TOut>>>
+	{
+		public static implicit operator Func<TIn, TOut>(Selector<TIn, TOut> value)
+			=> Selectors<TIn, TOut>.Default.Get(value.Get());
+
+		protected Selector(Expression<Func<TIn, TOut>> instance) : base(instance) {}
+	}
+}
