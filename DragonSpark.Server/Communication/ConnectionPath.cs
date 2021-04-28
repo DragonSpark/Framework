55685a1ad205dacb7a8a43f1072c212aa95e86fd
Mike-E <michael@dragonspark.network>
2021-04-28 10:37:58 -0400

    ConnectionPath

---
+++ DragonSpark.Server/Communication/ConnectionPath.cs
@@ -0,0 +1,19 @@
+﻿using DragonSpark.Model.Selection;
+using Flurl;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace DragonSpark.Server.Communication
+{
+	public class ConnectionPath : ISelect<string, Uri>
+	{
+		readonly Uri _root;
+
+		public ConnectionPath(IConfiguration configuration, string name)
+			: this(new Uri(configuration.GetConnectionString(name))) {}
+
+		public ConnectionPath(Uri root) => _root = root;
+
+		public Uri Get(string parameter) => _root.AppendPathSegment(parameter).ToUri();
+	}
+}
