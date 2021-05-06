2161dea490cfe877e867cf21e8d4460adc0eddb9
Mike-E <michael@dragonspark.network>
2021-05-06 12:30:40 -0400

    Authentication adjustments

--- DragonSpark.Identity.PayPal/ConfigureAuthentication.cs
+++ DragonSpark.Identity.PayPal/ConfigureAuthentication.cs
@@ -1,4 +1,5 @@
 ﻿using AspNet.Security.OAuth.Paypal;
+using DragonSpark.Application.Security;
 using DragonSpark.Model.Commands;
 using System;
 
@@ -7,8 +8,16 @@ namespace DragonSpark.Identity.PayPal
 	sealed class ConfigureAuthentication : ICommand<PaypalAuthenticationOptions>
 	{
 		readonly Func<PayPalApplicationSettings> _settings;
+		readonly IClaimAction                    _claim;
 
-		public ConfigureAuthentication(Func<PayPalApplicationSettings> settings) => _settings = settings;
+		public ConfigureAuthentication(Func<PayPalApplicationSettings> settings)
+			: this(settings, PayIdentifierClaimAction.Default) {}
+
+		public ConfigureAuthentication(Func<PayPalApplicationSettings> settings, IClaimAction claim)
+		{
+			_settings = settings;
+			_claim    = claim;
+		}
 
 		public void Execute(PaypalAuthenticationOptions parameter)
 		{
@@ -21,6 +30,8 @@ namespace DragonSpark.Identity.PayPal
 			parameter.TokenEndpoint           = authentication.TokenEndpoint;
 			parameter.UserInformationEndpoint = authentication.UserInformationEndpoint;
 
+			_claim.Execute(parameter.ClaimActions);
+
 			if (authentication.Scopes != null)
 			{
 				parameter.Scope.Clear();
@@ -30,5 +41,31 @@ namespace DragonSpark.Identity.PayPal
 				}
 			}
 		}
+	}
+
+	public sealed class PayPalClaimNamespace : Text.Text
+	{
+		public static PayPalClaimNamespace Default { get; } = new PayPalClaimNamespace();
+
+		PayPalClaimNamespace() : base("urn:paypal") {}
+	}
+
+	public sealed class PayIdentifierClaimAction : ClaimAction
+	{
+		public static PayIdentifierClaimAction Default { get; } = new PayIdentifierClaimAction();
+
+		PayIdentifierClaimAction() : base(PayIdentifier.Default, "payer_id") {}
+	}
+
+	public class PayPalClaim : Text.Text
+	{
+		protected PayPalClaim(string name) : base($"{PayPalClaimNamespace.Default}:{name}") {}
+	}
+
+	public sealed class PayIdentifier : PayPalClaim
+	{
+		public static PayIdentifier Default { get; } = new PayIdentifier();
+
+		PayIdentifier() : base(nameof(PayIdentifier).ToLower()) {}
 	}
 }
