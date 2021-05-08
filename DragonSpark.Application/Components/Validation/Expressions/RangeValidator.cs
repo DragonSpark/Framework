b0164bf35ace039c1a04125719cbda34c6073b8a
Mike-E <michael@dragonspark.network>
2021-05-08 04:54:13 -0400

    RangeValidator

---
+++ DragonSpark.Application/Components/Validation/Expressions/RangeValidator.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DragonSpark.Application.Components.Validation.Expressions
+{
+	public class RangeValidator : MetadataValueValidator
+	{
+		public RangeValidator(double minimum, double maximum) : this(new RangeAttribute(minimum, maximum)) {}
+
+		public RangeValidator(RangeAttribute metadata) : base(metadata) {}
+	}
+
+}
