5bc7b363921237a0ddceffb4f820b81d2d10515c
Mike-E <michael@dragonspark.network>
2021-06-18 08:37:00 -0400

    Identity edits

--- DragonSpark.Identity.Google/ProviderName.cs
+++ DragonSpark.Identity.Google/ProviderName.cs
@@ -1,9 +1,11 @@
-﻿namespace DragonSpark.Identity.Google
+﻿using Microsoft.AspNetCore.Authentication.Google;
+
+namespace DragonSpark.Identity.Google
 {
 	public sealed class ProviderName : Text.Text
 	{
 		public static ProviderName Default { get; } = new ProviderName();
 
-		ProviderName() : base("Google") {}
+		ProviderName() : base(GoogleDefaults.AuthenticationScheme) {}
 	}
 }
--- DragonSpark.Identity.Twitter/Claims/DefaultClaimActions.cs
+++ DragonSpark.Identity.Twitter/Claims/DefaultClaimActions.cs
@@ -1,11 +1,17 @@
 ﻿using DragonSpark.Application.Security;
 
-namespace DragonSpark.Identity.Twitter.Claims {
+namespace DragonSpark.Identity.Twitter.Claims
+{
 	public sealed class DefaultClaimActions : CompositeClaimAction
 	{
 		public static DefaultClaimActions Default { get; } = new DefaultClaimActions();
 
-		DefaultClaimActions()
-			: base(DisplayNameClaimAction.Default, ImagePathClaimAction.Default, DescriptionClaimAction.Default) {}
+		DefaultClaimActions() : base(DisplayNameClaimAction.Default, ImagePathClaimAction.Default,
+		                             DescriptionClaimAction.Default,
+		                             /**/
+		                             LocationClaimAction.Default,
+		                             WebsiteClaimAction.Default,
+		                             FollowersClaimAction.Default,
+		                             VerifiedClaimAction.Default) {}
 	}
 }
