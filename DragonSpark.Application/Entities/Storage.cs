d026b4610d8d88759bb9a2c115f529e9d1ca4122
Mike-E <michael@dragonspark.network>
2021-06-29 11:37:36 -0400

    Storage configuration command

--- DragonSpark.Application/Entities/Storage.cs
+++ DragonSpark.Application/Entities/Storage.cs
@@ -1,18 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DragonSpark.Model.Commands;
+using Microsoft.EntityFrameworkCore;
 using IdentityUser = DragonSpark.Application.Security.Identity.IdentityUser;
 
 namespace DragonSpark.Application.Entities
 {
 	public class Storage<T> : Security.Identity.IdentityDbContext<T> where T : IdentityUser
 	{
-		readonly IStorageInitializer _initializer;
+		readonly IStorageInitializer    _initializer;
+		readonly ICommand<ModelBuilder> _configure;
 
-		protected Storage(DbContextOptions options, IStorageInitializer initializer) : base(options)
-			=> _initializer = initializer;
+		protected Storage(DbContextOptions options, IStorageInitializer initializer)
+			: this(options, initializer, EmptyCommand<ModelBuilder>.Default) {}
+
+		protected Storage(DbContextOptions options, IStorageInitializer initializer, ICommand<ModelBuilder> configure)
+			: base(options)
+		{
+			_initializer = initializer;
+			_configure   = configure;
+		}
 
 		protected override void OnModelCreating(ModelBuilder builder)
 		{
 			var initialized = _initializer.Get(builder);
+			_configure.Execute(initialized);
 			base.OnModelCreating(initialized);
 		}
 	}
