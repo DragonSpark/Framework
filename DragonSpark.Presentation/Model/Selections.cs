4ac815b83f4e3a9b290d32fa6a956da923381c41
Mike-E <michael@dragonspark.network>
2021-07-05 10:43:53 -0400

    Added Selections model

---
+++ DragonSpark.Presentation/Model/Selections.cs
@@ -0,0 +1,10 @@
+﻿using DragonSpark.Model;
+using System.Collections.Generic;
+
+namespace DragonSpark.Presentation.Model
+{
+	public class Selections<T> : List<SelectionListing<T>>
+	{
+		public IEnumerable<T> Selected { get; set; } = Empty.Enumerable<T>();
+	}
+}
