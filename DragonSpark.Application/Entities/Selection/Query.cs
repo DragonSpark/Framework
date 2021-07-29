2190a65f7d14bb96f82f1f5b96648696fa911b67
Mike-E <michael@dragonspark.network>
2021-07-29 09:01:44 -0400

    Minor edit

--- DragonSpark.Application/Entities/Selection/Query.cs
+++ DragonSpark.Application/Entities/Selection/Query.cs
@@ -7,7 +7,7 @@ namespace DragonSpark.Application.Entities.Selection
 {
 	public class Query<T> : Instance<IQueryable<T>>
 	{
-		public Query(IQueryable<T> instance) : base(instance) {}
+		protected Query(IQueryable<T> instance) : base(instance) {}
 	}
 
 	public delegate Expression<Func<TEntity, bool>> Query<in TKey, TEntity>(TKey parameter);
