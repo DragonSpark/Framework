ad19f394b45691b3618e6a3e496c14884bba8d19
Mike-E <michael@dragonspark.network>
2021-07-30 08:16:28 -0400

    Schemes

---
+++ DragonSpark.Application/Security/Identity/Authentication/Schemes.cs
@@ -0,0 +1,22 @@
+﻿using DragonSpark.Model.Operations;
+using DragonSpark.Model.Sequences;
+using Microsoft.AspNetCore.Identity;
+using NetFabric.Hyperlinq;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Application.Security.Identity.Authentication
+{
+	public class Schemes<T> : IResulting<Array<string>> where T : class
+	{
+		readonly SignInManager<T> _authentication;
+
+		public Schemes(SignInManager<T> authentication) => _authentication = authentication;
+
+		public async ValueTask<Array<string>> Get()
+		{
+			var schemes = await _authentication.GetExternalAuthenticationSchemesAsync().ConfigureAwait(false);
+			var result  = schemes.AsValueEnumerable().Select(x => x.Name).ToArray();
+			return result;
+		}
+	}
+}
