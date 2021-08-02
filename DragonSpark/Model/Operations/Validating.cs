1d8afb64f6bedbfc4fcb3ed83f09d37955387f36
Mike-E <michael@dragonspark.network>
2021-08-02 05:36:14 -0400

    Cleaned up Validating

--- DragonSpark/Model/Operations/Validating.cs
+++ DragonSpark/Model/Operations/Validating.cs
@@ -6,55 +6,54 @@ namespace DragonSpark.Model.Operations
 {
 	public class Validating<TIn, TOut> : ISelecting<TIn, TOut>
 	{
-		readonly Await<TIn, TOut> _source, _fallback;
 		readonly Await<TIn, bool> _specification;
+		readonly Await<TIn, TOut> _true, _false;
 
-		public Validating(IDepending<TIn> condition, ISelecting<TIn, TOut> select)
-			: this(condition, select, Default<TIn, TOut>.Instance.Then().Operation().Out()) {}
+		public Validating(IDepending<TIn> condition, ISelecting<TIn, TOut> @true)
+			: this(condition, @true, Default<TIn, TOut>.Instance.Then().Operation().Out()) {}
 
-		public Validating(IDepending<TIn> condition, ISelecting<TIn, TOut> select, ISelecting<TIn, TOut> fallback)
-			: this(condition.Await, select.Await, fallback.Await) {}
+		public Validating(IDepending<TIn> condition, ISelecting<TIn, TOut> @true, ISelecting<TIn, TOut> @false)
+			: this(condition.Await, @true.Await, @false.Await) {}
 
-		public Validating(Await<TIn, bool> specification, Await<TIn, TOut> source)
-			: this(specification, source, Default<TIn, TOut>.Instance.Then().Operation()) {}
+		public Validating(Await<TIn, bool> specification, Await<TIn, TOut> @true)
+			: this(specification, @true, Default<TIn, TOut>.Instance.Then().Operation()) {}
 
-		public Validating(Await<TIn, bool> specification, Await<TIn, TOut> source, Await<TIn, TOut> fallback)
+		public Validating(Await<TIn, bool> specification, Await<TIn, TOut> @true, Await<TIn, TOut> @false)
 		{
 			_specification = specification;
-			_source        = source;
-			_fallback      = fallback;
+			_true          = @true;
+			_false         = @false;
 		}
 
 		public async ValueTask<TOut> Get(TIn parameter)
-			=> await _specification(parameter) ? await _source(parameter) : await _fallback(parameter);
+			=> await _specification(parameter) ? await _true(parameter) : await _false(parameter);
 	}
 
 	public class Validating<T> : IOperation<T>
 	{
-		readonly Await<T>       _source, _fallback;
 		readonly Await<T, bool> _specification;
+		readonly Await<T>       _true, _false;
 
-		public Validating(IDepending<T> condition, IOperation<T> select)
-			: this(condition.Await, select.Await, Default<T>.Instance.Then().Operation()) {}
+		public Validating(IDepending<T> condition, IOperation<T> @true)
+			: this(condition.Await, @true.Await, Default<T>.Instance.Then().Operation()) {}
 
-		public Validating(IDepending<T> condition, IOperation<T> select, IOperation<T> fallback)
-			: this(condition.Await, select.Await, fallback.Await) {}
+		public Validating(IDepending<T> condition, IOperation<T> @true, IOperation<T> @false)
+			: this(condition.Await, @true.Await, @false.Await) {}
 
-		public Validating(Await<T, bool> specification, Await<T> source)
-			: this(specification, source, Default<T>.Instance.Then().Operation()) {}
+		public Validating(Await<T, bool> specification, Await<T> @true)
+			: this(specification, @true, Default<T>.Instance.Then().Operation()) {}
 
-		public Validating(Await<T, bool> specification, Await<T> source, Await<T> fallback)
+		public Validating(Await<T, bool> specification, Await<T> @true, Await<T> @false)
 		{
 			_specification = specification;
-			_source        = source;
-			_fallback      = fallback;
+			_true          = @true;
+			_false         = @false;
 		}
 
 		public async ValueTask Get(T parameter)
 		{
-			var operation = await _specification(parameter) ? _source : _fallback;
+			var operation = await _specification(parameter) ? _true : _false;
 			await operation(parameter);
 		}
 	}
-
 }
