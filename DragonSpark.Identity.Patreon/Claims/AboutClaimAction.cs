666070e9592b1b9822a872b7c8d1883f3dff44a1
Mike-E <michael@dragonspark.network>
2021-08-07 16:22:24 -0400

    Claim edit

--- DragonSpark.Identity.Patreon/Claims/AboutClaimAction.cs
+++ DragonSpark.Identity.Patreon/Claims/AboutClaimAction.cs
@@ -2,10 +2,10 @@
 
 namespace DragonSpark.Identity.Patreon.Claims
 {
-	public sealed class AboutClaimAction : ClaimAction
+	public sealed class AboutClaimAction : SubKeyClaimAction
 	{
 		public static AboutClaimAction Default { get; } = new AboutClaimAction();
 
-		AboutClaimAction() : base(About.Default, "about") {}
+		AboutClaimAction() : base(About.Default, "attributes", "about") {}
 	}
 }
