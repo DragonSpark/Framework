131698f960e43f30df1f67b67f1ad5015458c30b
Mike-E <michael@dragonspark.network>
2021-08-09 09:05:44 -0400

    Removed authentication refresh from user synchronization

--- DragonSpark.Application/Security/Identity/Profile/UserSynchronization.cs
+++ DragonSpark.Application/Security/Identity/Profile/UserSynchronization.cs
@@ -1,6 +1,4 @@
-﻿
-using DragonSpark.Application.Security.Identity.Authentication;
-using DragonSpark.Compose;
+﻿using DragonSpark.Compose;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 
@@ -8,14 +6,11 @@ namespace DragonSpark.Application.Security.Identity.Profile
 {
 	sealed class UserSynchronization<T> : IUserSynchronization where T : IdentityUser
 	{
-		readonly IRefreshAuthentication<T> _refresh;
 		readonly ILocateUser<T>            _locate;
 		readonly IUserSynchronizer<T>      _synchronizer;
 
-		public UserSynchronization(IRefreshAuthentication<T> refresh, ILocateUser<T> locate,
-		                           IUserSynchronizer<T> synchronizer)
+		public UserSynchronization(ILocateUser<T> locate, IUserSynchronizer<T> synchronizer)
 		{
-			_refresh      = refresh;
 			_locate       = locate;
 			_synchronizer = synchronizer;
 		}
@@ -24,10 +19,7 @@ namespace DragonSpark.Application.Security.Identity.Profile
 		{
 			var locate = await _locate.Await(parameter);
 			var user   = locate.Verify();
-			if (await _synchronizer.Await(new(parameter, user)))
-			{
-				await _refresh.Await(user);
-			}
+			await _synchronizer.Await(new(parameter, user));
 		}
 	}
 }
