f6fb7874fffae1e359b9001a42ef07aaa6da4254
Mike-E <michael@dragonspark.network>
2021-09-04 12:07:37 -0400

    InParameterSelectionBenchmarks

--- DragonSpark.Testing/Model/Selection/SelectionTests.cs
+++ DragonSpark.Testing/Model/Selection/SelectionTests.cs
@@ -17,25 +17,25 @@ namespace DragonSpark.Testing.Model.Selection
 
 			public Benchmarks(ISelect<string, string> subject)
 				: this(subject, subject.Select(x => x),
-				       subject.Select(x => x)
-				              .Select(x => x)
-				              .Select(x => x)
-				              .Select(x => x)
-				              .Select(x => x),
-				       subject.Select(x => x)
-				              .Select(x => x)
-				              .Select(x => x)
-				              .Select(x => x)
-				              .Select(x => x)
-				              .Select(x => x)
-				              .Select(x => x)
-				              .Select(x => x)
-				              .Select(x => x)
-				              .Select(x => x)) {}
+					   subject.Select(x => x)
+							  .Select(x => x)
+							  .Select(x => x)
+							  .Select(x => x)
+							  .Select(x => x),
+					   subject.Select(x => x)
+							  .Select(x => x)
+							  .Select(x => x)
+							  .Select(x => x)
+							  .Select(x => x)
+							  .Select(x => x)
+							  .Select(x => x)
+							  .Select(x => x)
+							  .Select(x => x)
+							  .Select(x => x)) {}
 
 			// ReSharper disable once TooManyDependencies
 			public Benchmarks(ISelect<string, string> subject, ISelect<string, string> one,
-			                  ISelect<string, string> multiple, ISelect<string, string> extensive)
+							  ISelect<string, string> multiple, ISelect<string, string> extensive)
 			{
 				_subject   = subject;
 				_one       = one;
@@ -56,60 +56,90 @@ namespace DragonSpark.Testing.Model.Selection
 			public string Extensive() => _extensive.Get(string.Empty);
 		}
 
-		/*
-		public class StructureSelectionBenchmarks
+		public class InParameterSelectionBenchmarks
 		{
-			readonly ISelect<Reference, Reference>        _reference;
-			readonly IStructureSelect<Writable, Writable> _writable;
-			readonly IStructureSelect<Readable, Readable> _readable;
+			readonly ISelect<Reference, Reference>   _reference;
+			readonly ISelect<Writable, Writable>     _writable;
+			readonly ISelect<Readable, Readable>     _readable;
+			readonly IInSelect<Reference, Reference> _referenceIn;
+			readonly IInSelect<Writable, Writable>   _writableIn;
+			readonly IInSelect<Readable, Readable>   _readableIn;
 
-			public StructureSelectionBenchmarks()
+			public InParameterSelectionBenchmarks()
 				: this(A.Self<Reference>()
-				        .Select(x => x)
-				        .Select(x => x)
-				        .Select(x => x)
-				        .Select(x => x)
-				        .Select(x => x)
-				        .Select(x => x)
+						.Select(x => x)
+						.Select(x => x)
+						.Select(x => x)
+						.Select(x => x),
+				       A.Self<Writable>()
 				        .Select(x => x)
 				        .Select(x => x)
 				        .Select(x => x)
 				        .Select(x => x),
-				       new StructureSelect<Writable, Writable, Writable>(
-						   new StructureSelect<Writable, Writable, Writable>(
-							   new StructureSelect<Writable, Writable, Writable>(
-								   new StructureSelect<Writable, Writable, Writable>(
-									   x => x, writable => writable).Get,
-								   writable => writable).Get,
-							   writable => writable).Get,
-						   writable => writable),
-				       new StructureSelect<Readable, Readable, Readable>(
-						   new StructureSelect<Readable, Readable, Readable>(
-							   new StructureSelect<Readable, Readable, Readable>(
-								   new StructureSelect<Readable, Readable, Readable>(x => x, x => x).Get,
-								   x => x).Get,
-							   writable => writable).Get,
-						   writable => writable)) {}
+				       A.Self<Readable>()
+				        .Select(x => x)
+				        .Select(x => x)
+				        .Select(x => x)
+				        .Select(x => x),
+					  new InSelect<Reference, Reference, Reference>(
+							new InSelect<Reference, Reference, Reference>(
+							  new InSelect<Reference, Reference, Reference>(
+								new InSelect<Reference, Reference, Reference>(
+									(in Reference x) => x, (in Reference x) => x).Get,
+								(in Reference x) => x).Get,
+							  (in Reference x) => x).Get,
+							(in Reference x) => x),
+					   new InSelect<Writable, Writable, Writable>(
+						   new InSelect<Writable, Writable, Writable>(
+							   new InSelect<Writable, Writable, Writable>(
+								   new InSelect<Writable, Writable, Writable>(
+										(in Writable x) => x, (in Writable x) => x).Get,
+								   (in Writable x) => x).Get,
+							   (in Writable x) => x).Get,
+						   (in Writable x) => x),
+					   new InSelect<Readable, Readable, Readable>(
+						   new InSelect<Readable, Readable, Readable>(
+							   new InSelect<Readable, Readable, Readable>(
+								   new InSelect<Readable, Readable, Readable>((in Readable x) => x,
+									   (in Readable x) => x).Get,
+								   (in Readable x) => x).Get,
+							   (in Readable x) => x).Get,
+						   (in Readable x) => x)) {}
 
-			public StructureSelectionBenchmarks(ISelect<Reference, Reference> reference,
-			                                    IStructureSelect<Writable, Writable> writable,
-			                                    IStructureSelect<Readable, Readable> readable)
+			// ReSharper disable once TooManyDependencies
+			public InParameterSelectionBenchmarks(ISelect<Reference, Reference> reference,
+			                                    ISelect<Writable, Writable> writable,
+			                                    ISelect<Readable, Readable> readable,
+			                                    IInSelect<Reference, Reference> referenceIn,
+			                                    IInSelect<Writable, Writable> writableIn,
+			                                    IInSelect<Readable, Readable> readableIn)
 			{
-				_reference = reference;
-				_writable  = writable;
-				_readable  = readable;
+				_reference   = reference;
+				_writable    = writable;
+				_readable    = readable;
+				_referenceIn = referenceIn;
+				_writableIn  = writableIn;
+				_readableIn  = readableIn;
 			}
 
 			[Benchmark(Baseline = true)]
-			public string Class() => _reference.Get(new Reference("One", "Two", "Three")).First;
+			public string ReferenceSelect() => _reference.Get(new Reference("One", "Two", "Three")).First;
 
 			[Benchmark]
-			public string Write() => _writable.Get(new Writable("One", "Two", "Three")).Message;
+			public string WriteSelect() => _writable.Get(new Writable("One", "Two", "Three")).Message;
 
 			[Benchmark]
-			public string Read() => _readable.Get(new Readable("One", "Two", "Three")).Message;
+			public string ReadSelect() => _readable.Get(new Readable("One", "Two", "Three")).Message;
+
+			[Benchmark]
+			public string ClassIn() => _referenceIn.Get(new Reference("One", "Two", "Three")).First;
+
+			[Benchmark]
+			public string WriteIn() => _writableIn.Get(new Writable("One", "Two", "Three")).Message;
+
+			[Benchmark]
+			public string ReadIn() => _readableIn.Get(new Readable("One", "Two", "Three")).Message;
 		}
-		*/
 
 		public sealed class Reference
 		{
@@ -152,5 +182,45 @@ namespace DragonSpark.Testing.Model.Selection
 				Three   = three;
 			}
 		}
+	}
+
+	public interface IInSelect<TIn, out TOut>
+	{
+		TOut Get(in TIn parameter);
+	}
+
+	public class InSelect<TIn, TOut> : IInSelect<TIn, TOut>
+	{
+		readonly In<TIn, TOut> _source;
+
+		public InSelect(ISelect<TIn, TOut> select) : this(select.Get) {}
+
+		public InSelect(In<TIn, TOut> select) => _source = select;
+
+		public TOut Get(in TIn parameter) => _source(parameter);
+	}
+
+	public delegate TOut In<TIn, out TOut>(in TIn parameter);
+
+	public class InSelect<TIn, TFrom, TTo> : IInSelect<TIn, TTo>
+	{
+		readonly In<TFrom, TTo> _current;
+		readonly In<TIn, TFrom> _previous;
+
+		public InSelect(IInSelect<TIn, TFrom> previous, IInSelect<TFrom, TTo> current)
+			: this(previous.Get, current.Get) {}
+
+		public InSelect(In<TIn, TFrom> previous, In<TFrom, TTo> current)
+		{
+			_previous = previous;
+			_current  = current;
+		}
+
+		public TTo Get(in TIn parameter) => _current(_previous(in parameter));
+	}
+
+	public static class Extensions
+	{
+		public static TOut Get<TIn, TOut>(this ISelect<TIn, TOut> @this, in TIn parameter) => @this.Get(parameter);
 	}
 }
