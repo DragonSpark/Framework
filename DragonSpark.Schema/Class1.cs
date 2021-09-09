292640478f5cdd571d33c9a02b67e3c3c717131c
Mike-E <michael@dragonspark.network>
2021-09-09 11:26:09 -0400

    Basic reproduction of https://github.com/dotnet/efcore/issues/25878

---
+++ DragonSpark.Schema/Class1.cs
@@ -0,0 +1,24 @@
+﻿using DragonSpark.Application.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DragonSpark.Schema
+{
+	public class Class1 {}
+
+	public sealed class ApplicationState : Entities
+	{
+		public ApplicationState(DbContextOptions<ApplicationState> options) : base(options) {}
+
+		public DbSet<Subject> Subjects { get; set; } = default!;
+	}
+
+	public sealed class Subject
+	{
+		public Guid Id { get; set; }
+
+		public DateTimeOffset Created { get; set; }
+
+		public string Message { get; set; } = default!;
+	}
+}
---
+++ DragonSpark.Schema/Design/KnownSchemaModifications.cs
@@ -0,0 +1,11 @@
+﻿using DragonSpark.Application.Entities;
+
+namespace DragonSpark.Schema.Design
+{
+	public sealed class KnownSchemaModifications : SchemaModification
+	{
+		public static KnownSchemaModifications Default { get; } = new KnownSchemaModifications();
+
+		KnownSchemaModifications() : base() {}
+	}
+}
