de393b33d0187cf467ccb5a159b4e1ec504cdb42
Mike-E <michael@dragonspark.network>
2021-09-09 18:14:00 -0400

    Fix. :P

--- DragonSpark.Testing.Objects/Entities/PooledMemoryContexts.cs
+++ DragonSpark.Testing.Objects/Entities/PooledMemoryContexts.cs
@@ -1,23 +1,9 @@
-﻿using DragonSpark.Application.Entities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace DragonSpark.Testing.Objects.Entities
 {
 	public sealed class PooledMemoryContexts<T> : PooledContexts<T> where T : DbContext
 	{
 		public PooledMemoryContexts() : base(NewMemoryOptions<T>.Default.Get()) {}
-	}
-
-	public sealed class PooledSqlContexts<T> : PooledContexts<T> where T : DbContext
-	{
-		public PooledSqlContexts() : base(NewSqlOptions<T>.Default.Get()) {}
-	}
-
-	public class PooledContexts<T> : Contexts<T> where T : DbContext
-	{
-		public PooledContexts(DbContextOptions<T> options) : this(new PooledDbContextFactory<T>(options)) {}
-
-		public PooledContexts(IDbContextFactory<T> factory) : base(factory) {}
 	}
 }
