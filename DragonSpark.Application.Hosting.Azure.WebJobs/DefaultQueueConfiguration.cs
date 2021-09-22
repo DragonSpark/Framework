6d47a9b6cff040eaad59e0eb063424f81df6139b
Mike-E <michael@dragonspark.network>
2021-09-22 14:06:30 -0400

    Updated WebJobs to beta.5 courtesy of https://github.com/Azure/azure-webjobs-sdk/issues/2753

---
+++ DragonSpark.Application.Hosting.Azure.WebJobs/DefaultQueueConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Storage.Queues;
+using DragonSpark.Model.Commands;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace DragonSpark.Application.Hosting.Azure.WebJobs
+{
+	sealed class DefaultQueueConfiguration : ICommand<QueuesOptions>
+	{
+		public static DefaultQueueConfiguration Default { get; } = new DefaultQueueConfiguration();
+
+		DefaultQueueConfiguration() {}
+
+		public void Execute(QueuesOptions parameter)
+		{
+			parameter.MessageEncoding = QueueMessageEncoding.None;
+		}
+	}
+}
