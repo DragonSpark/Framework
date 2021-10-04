8d13d4583f09527a5f2e158c70d952d39e858a90
Mike-E <michael@dragonspark.network>
2021-10-04 09:20:10 -0400

    Removed unused code

--- DragonSpark.Application/Entities/Queries/Composition/IProjection.cs
+++
@@ -1,8 +0,0 @@
-﻿using DragonSpark.Model.Results;
-using System;
-using System.Linq.Expressions;
-
-namespace DragonSpark.Application.Entities.Queries.Composition
-{
-	public interface IProjection<TFrom, TTo> : IResult<Expression<Func<TFrom, TTo>>> {}
-}
