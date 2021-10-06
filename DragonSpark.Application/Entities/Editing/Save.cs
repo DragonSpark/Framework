8ef19e879a164f5c74909833750208b1a79ef1e7
Mike-E <michael@dragonspark.network>
2021-10-06 05:09:07 -0400

    Introduced editors

--- DragonSpark.Application/Entities/Editing/Save.cs
+++ DragonSpark.Application/Entities/Editing/Save.cs
@@ -2,11 +2,13 @@
 
 namespace DragonSpark.Application.Entities.Editing
 {
+	// TODO: Rename -> Update
 	public class Save<T> : Modify<T> where T : class
 	{
 		protected Save(IScopes scopes) : base(scopes, UpdateLocal<T>.Default.Then().Operation()) {}
 	}
 
+	// TODO: Rename -> Save
 	public sealed class StandardSave<T> : Save<T> where T : class
 	{
 		public StandardSave(IStandardScopes scopes) : base(scopes) {}
