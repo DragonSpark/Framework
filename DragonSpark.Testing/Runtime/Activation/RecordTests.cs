a569c9d3492a0301c80ce40b2b63287dfac66a58
Mike-E <michael@dragonspark.network>
2021-10-07 13:19:58 -0400

    Verified assumption

---
+++ DragonSpark.Testing/Runtime/Activation/RecordTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace DragonSpark.Testing.Runtime.Activation
+{
+	public sealed class RecordTests
+	{
+		[Fact]
+		public void Verify()
+		{
+			var instance = new Subject("Hello World", 123);
+			var first    = instance;
+			instance.Message = "Updated";
+
+			instance.Should().BeSameAs(first);
+			first.Message.Should().Be("Updated");
+		}
+
+		[Fact]
+		public void VerifyWith()
+		{
+			var instance = new Subject("Hello World", 123);
+
+			var next = instance with { Message = "Updated" };
+
+			next.Should().NotBeSameAs(instance);
+
+		}
+
+		sealed record Subject(string Message, int Number)
+		{
+			public string Message { get; set; } = Message;
+		}
+	}
+}
