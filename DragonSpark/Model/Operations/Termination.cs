9247cbaf40424633fa0af048541282c669500ee2
Mike-E <michael@dragonspark.network>
2021-10-08 15:51:41 -0400

    Termination

--- DragonSpark/Model/Operations/Termination.cs
+++ DragonSpark/Model/Operations/Termination.cs
@@ -1,11 +1,34 @@
-﻿using System.Threading.Tasks;
+﻿using DragonSpark.Compose;
+using System.Threading.Tasks;
 
 namespace DragonSpark.Model.Operations
 {
+	public class Termination<TIn, TNext> : IOperation<TIn>
+	{
+		readonly Await<TIn, TNext> _first;
+		readonly Await<TNext>      _second;
+
+		public Termination(ISelecting<TIn, TNext> first, IOperation<TNext> second) : this(first.Await, second.Await) {}
+
+		public Termination(Await<TIn, TNext> first, Await<TNext> second)
+		{
+			_first  = first;
+			_second = second;
+		}
+
+		public async ValueTask Get(TIn parameter)
+		{
+			var first = await _first(parameter);
+			await _second(first);
+		}
+	}
+
 	public class Termination<T> : IOperation<T>
 	{
 		readonly Await<T> _first;
 		readonly Await    _second;
+
+		public Termination(IOperation<T> first, IOperation second) : this(first.Await, second.Await) {}
 
 		public Termination(Await<T> first, Await second)
 		{
