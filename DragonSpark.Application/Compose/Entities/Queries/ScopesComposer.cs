91f83501a03f44ab5550906b38a2db8f39c6fc1e
Mike-E <michael@dragonspark.network>
2021-10-10 05:36:00 -0400

    Removed unused method

--- DragonSpark.Application/Compose/Entities/Queries/ScopesComposer.cs
+++ DragonSpark.Application/Compose/Entities/Queries/ScopesComposer.cs
@@ -3,7 +3,6 @@ using DragonSpark.Application.Entities.Queries.Compiled;
 using DragonSpark.Application.Entities.Queries.Composition;
 using DragonSpark.Application.Entities.Queries.Runtime;
 using DragonSpark.Model.Results;
-using System;
 
 namespace DragonSpark.Application.Compose.Entities.Queries
 {
@@ -22,10 +21,6 @@ namespace DragonSpark.Application.Compose.Entities.Queries
 
 		public EditInvocationComposer<TIn, T> Edit
 			=> new(new ScopesComposer<TIn, T>(_scopes.Then().WithAmbientLocation(), _query).Get());
-
-		public ScopesComposer<TIn, TTo> Select<TTo>(
-			Func<QueryComposer<TIn, T>, QueryComposer<TIn, TTo>> select)
-			=> new(_scopes, select(new QueryComposer<TIn, T>(_query)).Get());
 
 		public IRuntimeQuery<TIn, T> Compile() => new RuntimeQuery<TIn, T>(_scopes, _query);
 
