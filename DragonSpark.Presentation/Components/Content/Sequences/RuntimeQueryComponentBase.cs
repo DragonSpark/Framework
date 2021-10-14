c07d5f0a6df888f3bb1f11a79811bef16ce7f5e5
Mike-E <michael@dragonspark.network>
2021-10-14 07:29:15 -0400

    RuntimeQueryComponentBase

---
+++ DragonSpark.Presentation/Components/Content/Sequences/RuntimeQueryComponentBase.cs
@@ -0,0 +1,18 @@
+﻿using DragonSpark.Application.Entities.Queries.Runtime;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Presentation.Components.Content.Sequences
+{
+	public abstract class RuntimeQueryComponentBase<T> : ComponentBase
+	{
+		protected override async ValueTask Initialize()
+		{
+			await base.Initialize();
+			Content = GetContent();
+		}
+
+		protected abstract IQueries<T> GetContent();
+
+		protected IQueries<T> Content { get; private set; } = default!;
+	}
+}
