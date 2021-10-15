3ffb71ab50bae782ee5e8803b53e99799619adde
Mike-E <michael@dragonspark.network>
2021-10-15 14:56:46 -0400

    Additional constructors

--- DragonSpark.Application/Entities/Queries/Composition/StartWhereMany.cs
+++ DragonSpark.Application/Entities/Queries/Composition/StartWhereMany.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LinqKit;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace DragonSpark.Application.Entities.Queries.Composition;
@@ -10,17 +12,25 @@ public class StartWhereMany<TIn, T, TTo> : WhereMany<TIn, T, TTo> where T : clas
 	protected StartWhereMany(Expression<Func<T, bool>> where, Expression<Func<T, IEnumerable<TTo>>> select)
 		: base(Set<TIn, T>.Default, where, select) {}
 
-	public StartWhereMany(Expression<Func<T, bool>> where, Expression<Func<TIn, T, IEnumerable<TTo>>> select)
+	protected StartWhereMany(Expression<Func<T, bool>> where, Expression<Func<TIn, T, IEnumerable<TTo>>> select)
 		: base(Set<TIn, T>.Default, where, select) {}
 
-	public StartWhereMany(Expression<Func<TIn, T, bool>> where, Expression<Func<T, IEnumerable<TTo>>> select)
+	protected StartWhereMany(Expression<Func<IQueryable<T>, IQueryable<T>>> start,
+	                         Expression<Func<TIn, T, bool>> where, Expression<Func<T, IEnumerable<TTo>>> select)
+		: base((context, _) => start.Invoke(context.Set<T>()), where, select) {}
+
+	protected StartWhereMany(Expression<Func<TIn, T, bool>> where, Expression<Func<T, IEnumerable<TTo>>> select)
 		: base(Set<TIn, T>.Default, where, select) {}
 
-	public StartWhereMany(Expression<Func<TIn, T, bool>> where, Expression<Func<TIn, T, IEnumerable<TTo>>> select)
+	protected StartWhereMany(Expression<Func<IQueryable<T>, IQueryable<T>>> start,
+	                         Expression<Func<TIn, T, bool>> where, Expression<Func<TIn, T, IEnumerable<TTo>>> select)
+		: base((context, _) => start.Invoke(context.Set<T>()), where, select) {}
+
+	protected StartWhereMany(Expression<Func<TIn, T, bool>> where, Expression<Func<TIn, T, IEnumerable<TTo>>> select)
 		: base(Set<TIn, T>.Default, where, select) {}
 
-	public StartWhereMany(Expression<Func<TIn, T, bool>> where,
-	                      Expression<Func<DbContext, TIn, T, IEnumerable<TTo>>> select)
+	protected StartWhereMany(Expression<Func<TIn, T, bool>> where,
+	                         Expression<Func<DbContext, TIn, T, IEnumerable<TTo>>> select)
 		: base(Set<TIn, T>.Default, where, select) {}
 }
 
