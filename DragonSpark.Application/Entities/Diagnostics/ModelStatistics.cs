420a834184df5a836b6c5f02b1cfe2e19f9188e7
Mike-E <michael@dragonspark.network>
2021-10-15 11:28:15 -0400

    ModelStatistics

---
+++ DragonSpark.Application/Entities/Diagnostics/ModelStatistics.cs
@@ -0,0 +1,32 @@
+﻿using DragonSpark.Application.Diagnostics.Initialization;
+using DragonSpark.Model.Commands;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace DragonSpark.Application.Entities.Diagnostics
+{
+	sealed class ModelStatistics : ICommand<IModel>
+	{
+		public static ModelStatistics Default { get; } = new();
+
+		ModelStatistics() : this(DefaultInitializeLog<ModelStatistics>.Default.Get()) {}
+
+		readonly ILogger _logger;
+
+		public ModelStatistics(ILogger logger) => _logger = logger;
+
+		public void Execute(IModel parameter)
+		{
+
+			var properties    = parameter.GetEntityTypes().SelectMany(e => e.GetDeclaredProperties()).Count();
+			var relationships = parameter.GetEntityTypes().SelectMany(e => e.GetDeclaredForeignKeys()).Count();
+
+			_logger.LogInformation("Model has:");
+			_logger.LogInformation($"  {parameter.GetEntityTypes().Count()} entity types");
+			_logger.LogInformation($"  {properties} properties");
+			_logger.LogInformation($"  {relationships} relationships");
+			_logger.LogInformation($"  {properties + relationships} total compilation targets");
+		}
+	}
+}
