9dde5bfbe29fa2ddebc74ad716d9cc4d56a7fd20
Mike-E <michael@dragonspark.network>
2021-10-18 13:16:06 -0400

    InstanceComponentBase

---
+++ DragonSpark.Presentation/Components/Content/InstanceComponentBase.cs
@@ -0,0 +1,27 @@
+﻿using DragonSpark.Compose;
+using System;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Presentation.Components.Content
+{
+	public abstract class InstanceComponentBase<T> : ContentComponentBase<T>
+	{
+		protected abstract T GetInstance();
+
+		protected override ValueTask<T> GetContent() => GetInstance().ToOperation();
+
+		public T? Instance
+		{
+			get
+			{
+				var content = Content.Get();
+				var result = content.IsCompletedSuccessfully
+					             ? content.Result
+					             : throw new
+						               InvalidOperationException($"{GetType()} expected an in-memory instance to be available, but it was not.");
+				return result;
+			}
+		}
+		
+	}
+}
--- DragonSpark.Presentation/Components/Content/Sequences/RuntimeQueryComponentBase.cs
+++ DragonSpark.Presentation/Components/Content/Sequences/RuntimeQueryComponentBase.cs
@@ -1,17 +1,8 @@
 ﻿using DragonSpark.Application.Entities.Queries.Runtime;
-using System.Threading.Tasks;
 
 namespace DragonSpark.Presentation.Components.Content.Sequences;
 
-public abstract class RuntimeQueryComponentBase<T> : ComponentBase
+public abstract class RuntimeQueryComponentBase<T> : InstanceComponentBase<IQueries<T>>
 {
-	protected override async ValueTask Initialize()
-	{
-		await base.Initialize();
-		Content = GetContent();
-	}
 
-	protected abstract IQueries<T> GetContent();
-
-	protected IQueries<T> Content { get; private set; } = default!;
 }
