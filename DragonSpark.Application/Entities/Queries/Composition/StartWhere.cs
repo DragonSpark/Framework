2ae227d91e14120b4240dca5431d3e95cd61d718
Mike-E <michael@dragonspark.network>
2021-10-20 04:42:14 -0400

    Additional constructor

--- DragonSpark.Application/Entities/Queries/Composition/StartWhere.cs
+++ DragonSpark.Application/Entities/Queries/Composition/StartWhere.cs
@@ -13,6 +13,10 @@ public class StartWhere<TIn, T> : Where<TIn, T> where T : class
 	                     Expression<Func<TIn, T, bool>> where)
 		: base(context => previous.Invoke(context.Set<T>()), where) {}
 
+	protected StartWhere(Expression<Func<IQueryable<T>, IQueryable<T>>> previous,
+	                     Expression<Func<T, bool>> where)
+		: base(context => previous.Invoke(context.Set<T>()), where) {}
+
 	protected StartWhere(Expression<Func<TIn, T, bool>> where) : base(Set<TIn, T>.Default, where) {}
 }
 
