50fb7f5cc0b2d0669b7770bbe328a69dd118baf4
Mike-E <michael@dragonspark.network>
2021-10-26 17:52:17 -0400

    Added constructors

--- DragonSpark.Presentation/Model/Selections.cs
+++ DragonSpark.Presentation/Model/Selections.cs
@@ -5,5 +5,9 @@ namespace DragonSpark.Presentation.Model;
 
 public class Selections<T> : List<SelectionListing<T>>
 {
+	public Selections() {}
+
+	public Selections(IEnumerable<SelectionListing<T>> collection) : base(collection) {}
+
 	public IEnumerable<T> Selected { get; set; } = Empty.Enumerable<T>();
 }
