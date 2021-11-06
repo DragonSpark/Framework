5443edad7484d1c130cdf452312eb1608eb84754
Mike-E <michael@dragonspark.network>
2021-11-06 12:35:34 -0400

    Success interaction results

--- DragonSpark.Presentation/Interaction/SuccessResult.cs
+++ DragonSpark.Presentation/Interaction/SuccessResult.cs
@@ -1,8 +1,19 @@
-﻿namespace DragonSpark.Presentation.Interaction;
+﻿using DragonSpark.Model.Results;
+
+namespace DragonSpark.Presentation.Interaction;
 
 public sealed class SuccessResult : SuccessResultBase
 {
 	public static SuccessResult Default { get; } = new();
 
 	SuccessResult() {}
+}
+
+public class SuccessResult<T> : SuccessResultBase, IResult<T>
+{
+	public SuccessResult(T instance) => Instance = instance;
+
+	public T Instance { get; }
+
+	public T Get() => Instance;
 }
---
+++ DragonSpark.Presentation/Interaction/UniqueIdentityResult.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DragonSpark.Presentation.Interaction
+{
+	public class UniqueIdentityResult : SuccessResult<Guid>
+	{
+		public UniqueIdentityResult(Guid instance) : base(instance) {}
+	}
+}
