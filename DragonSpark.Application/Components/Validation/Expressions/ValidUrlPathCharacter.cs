da921624ee2546e6ea02ba91a8656c42397e1d4e
Mike-E <michael@dragonspark.network>
2021-11-06 07:24:47 -0400

    Added expression

---
+++ DragonSpark.Application/Components/Validation/Expressions/ValidUrlPathCharacter.cs
@@ -0,0 +1,9 @@
+﻿namespace DragonSpark.Application.Components.Validation.Expressions
+{
+	public sealed class ValidUrlPathCharacter : Expression
+	{
+		public static ValidUrlPathCharacter Default { get; } = new();
+
+		ValidUrlPathCharacter() : base("[A-Za-z0-9._-]") {}
+	}
+}
