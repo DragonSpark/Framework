ed71e2cbfa66339899ef5f2c0a8b8a043d526fb6
Mike-E <michael@dragonspark.network>
2021-11-07 05:50:54 -0500

    Selecting ValidatingValue

--- DragonSpark.Application/Components/Validation/Expressions/ValidatingValue.cs
+++ DragonSpark.Application/Components/Validation/Expressions/ValidatingValue.cs
@@ -11,3 +11,17 @@ public class ValidatingValue<T> : Depending<T>, IValidatingValue<T>
 
 	protected ValidatingValue(Func<T, ValueTask<bool>> @select) : base(@select) {}
 }
+
+public class ValidatingValue<TFrom, TTo> : IValidatingValue<TFrom>
+{
+	readonly IDepending<TTo>  _existing;
+	readonly Func<TFrom, TTo> _select;
+
+	public ValidatingValue(IDepending<TTo> existing, Func<TFrom, TTo> select)
+	{
+		_existing = existing;
+		_select   = @select;
+	}
+
+	public ValueTask<bool> Get(TFrom parameter) => _existing.Get(_select(parameter));
+}
