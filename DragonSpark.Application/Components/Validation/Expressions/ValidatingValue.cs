38f65e5480ee97dc8d9145d8f13724f86b159f5b
Mike-E <michael@dragonspark.network>
2021-11-07 03:59:02 -0500

    Added base class

---
+++ DragonSpark.Application/Components/Validation/Expressions/ValidatingValue.cs
@@ -0,0 +1,13 @@
+﻿using DragonSpark.Model.Operations;
+using DragonSpark.Model.Selection;
+using System;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Application.Components.Validation.Expressions;
+
+public class ValidatingValue<T> : Depending<T>, IValidatingValue<T>
+{
+	protected ValidatingValue(ISelect<T, ValueTask<bool>> @select) : base(@select) {}
+
+	protected ValidatingValue(Func<T, ValueTask<bool>> @select) : base(@select) {}
+}
