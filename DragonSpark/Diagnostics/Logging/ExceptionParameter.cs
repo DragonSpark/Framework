14aa1cb1814b95f4aa85a7401246c80dba23ff10
Mike-E <michael@dragonspark.network>
2021-11-19 16:04:07 -0500

    Converted to record

--- DragonSpark/Diagnostics/Logging/ExceptionParameter.cs
+++ DragonSpark/Diagnostics/Logging/ExceptionParameter.cs
@@ -1,20 +1,3 @@
 ﻿namespace DragonSpark.Diagnostics.Logging;
 
-public readonly struct ExceptionParameter<T>
-{
-	public ExceptionParameter(System.Exception exception, T argument)
-	{
-		Exception = exception;
-		Argument  = argument;
-	}
-
-	public System.Exception Exception { get; }
-
-	public T Argument { get; }
-
-	public void Deconstruct(out System.Exception exception, out T argument)
-	{
-		exception = Exception;
-		argument  = Argument;
-	}
-}
+public readonly record struct ExceptionParameter<T>(System.Exception Exception, T Argument);
