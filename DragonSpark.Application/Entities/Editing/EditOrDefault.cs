f31aa255bd0ad1f18c83c14cf02fb465b275c45a
Mike-E <michael@dragonspark.network>
2021-11-22 16:43:42 -0500

    EditOrDefault

---
+++ DragonSpark.Application/Entities/Editing/EditOrDefault.cs
@@ -0,0 +1,9 @@
+﻿using DragonSpark.Application.Entities.Queries.Composition;
+
+namespace DragonSpark.Application.Entities.Editing;
+
+public class EditOrDefault<TIn, T> : Edit<TIn, T?>
+{
+	protected EditOrDefault(IScopes context, IQuery<TIn, T> query)
+		: base(context.Then().Use(query).Edit.SingleOrDefault()) {}
+}
