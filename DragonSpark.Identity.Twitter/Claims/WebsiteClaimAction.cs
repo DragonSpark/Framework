165597d7759e5f8eb2f270acf658c3ce9d8f9a59
Mike-E <michael@dragonspark.network>
2021-11-23 16:25:25 -0500

    Safer Url mapping

--- DragonSpark.Identity.Twitter/Claims/WebsiteClaimAction.cs
+++ DragonSpark.Identity.Twitter/Claims/WebsiteClaimAction.cs
@@ -1,5 +1,7 @@
 ﻿using DragonSpark.Application.Security.Identity.Claims.Actions;
+using DragonSpark.Model.Selection;
 using Microsoft.AspNetCore.Authentication;
+using System.Text.Json;
 
 namespace DragonSpark.Identity.Twitter.Claims
 {
@@ -7,9 +9,24 @@ namespace DragonSpark.Identity.Twitter.Claims
 	{
 		public static WebsiteClaimAction Default { get; } = new WebsiteClaimAction();
 
-		WebsiteClaimAction() : base(Website.Default, "url", root => root.GetProperty("entities")
-		                                                                .GetProperty("url")
-		                                                                .GetProperty("urls")[0]
-		                                                                .GetString("expanded_url")!) {}
+		WebsiteClaimAction() : base(Website.Default, "url", GetUrl.Instance.Get!) {}
+
+		sealed class GetUrl : ISelect<JsonElement, string?>
+		{
+			public static GetUrl Instance { get; } = new();
+
+			GetUrl() {}
+
+			public string? Get(JsonElement parameter)
+			{
+				var entities = parameter.GetProperty("entities");
+				if (entities.TryGetProperty("url", out entities) && entities.TryGetProperty("urls", out var urls))
+				{
+					return urls[0].GetString("expanded_url");
+				}
+				return null;
+			}
+		}
+
 	}
 }
