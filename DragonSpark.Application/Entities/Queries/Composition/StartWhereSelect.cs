8254d884b882d14c3230559f6b7a180640595f2b
Mike-E <michael@dragonspark.network>
2021-11-30 14:38:31 -0500

    Added constructor

--- DragonSpark.Application/Entities/Queries/Composition/StartWhereSelect.cs
+++ DragonSpark.Application/Entities/Queries/Composition/StartWhereSelect.cs
@@ -25,6 +25,11 @@ public class StartWhereSelect<TIn, T, TTo> : WhereSelect<TIn, T, TTo> where T : 
 		: base(Set<TIn, T>.Default, where, select) {}
 
 	protected StartWhereSelect(Expression<Func<IQueryable<T>, IQueryable<T>>> query,
+	                           Expression<Func<TIn, T, bool>> where, Expression<Func<DbContext, T, TTo>> select)
+		: base((context, @in) => query.Invoke(Set<TIn, T>.Default.Get().Invoke(context, @in)), where,
+		       (context, _, x) => select.Invoke(context, x)) {}
+
+	protected StartWhereSelect(Expression<Func<IQueryable<T>, IQueryable<T>>> query,
 	                           Expression<Func<TIn, T, bool>> where, Expression<Func<T, TTo>> select)
 		: base((context, @in) => query.Invoke(Set<TIn, T>.Default.Get().Invoke(context, @in)), where, select) {}
 
