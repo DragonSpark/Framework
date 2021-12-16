d17c3edd65a9f51347ab7790b0c347e419ce4291
Mike-E <michael@dragonspark.network>
2021-12-16 03:24:24 -0500

    IdentityProviderValue

---
+++ DragonSpark.Application/Security/Identity/IdentityProviderValue.cs
@@ -0,0 +1,11 @@
+﻿using DragonSpark.Application.Security.Identity.Claims.Access;
+using System.Security.Claims;
+
+namespace DragonSpark.Application.Security.Identity;
+
+public sealed class IdentityProviderValue : RequiredClaim
+{
+	public static IdentityProviderValue Default { get; } = new();
+
+	IdentityProviderValue() : base(ClaimTypes.AuthenticationMethod) {}
+}
