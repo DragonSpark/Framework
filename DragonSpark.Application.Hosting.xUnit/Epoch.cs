719b7f636178d81562df587c6959c66cb18fd957
Mike-E <michael@dragonspark.network>
2022-01-13 07:53:36 -0500

    Fix

--- DragonSpark.Application.Hosting.xUnit/Epoch.cs
+++ DragonSpark.Application.Hosting.xUnit/Epoch.cs
@@ -9,6 +9,6 @@ public sealed class Epoch : Instance<DateTimeOffset>, ITime
 	public static Epoch Default { get; } = new Epoch();
 
 	Epoch() : base(new DateTimeOffset(1976, 6, 7, 23, 17, 24,
-	                                  TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time")
+	                                  TimeZoneInfo.FindSystemTimeZoneById("America/New_York")
 	                                              .BaseUtcOffset)) {}
 }
