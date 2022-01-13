48319ed94564d2abcb4f9cea4380d1a64fbf544c
Mike-E <michael@dragonspark.network>
2022-01-13 08:05:04 -0500

    Serialize time zone

--- DragonSpark.Application.Hosting.xUnit/Epoch.cs
+++ DragonSpark.Application.Hosting.xUnit/Epoch.cs
@@ -9,6 +9,6 @@ public sealed class Epoch : Instance<DateTimeOffset>, ITime
 	public static Epoch Default { get; } = new Epoch();
 
 	Epoch() : base(new DateTimeOffset(1976, 6, 7, 23, 17, 24,
-	                                  TimeZoneInfo.FindSystemTimeZoneById("America/New_York")
+	                                  TimeZoneInfo.FromSerializedString("Eastern Standard Time;-300;(UTC-05:00) Eastern Time (US & Canada);Eastern Standard Time;Eastern Daylight Time;[01:01:0001;12:31:2006;60;[0;02:00:00;4;1;0;];[0;02:00:00;10;5;0;];][01:01:2007;12:31:9999;60;[0;02:00:00;3;2;0;];[0;02:00:00;11;1;0;];];")
 	                                              .BaseUtcOffset)) {}
 }
