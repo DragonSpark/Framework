7054432de384d28cb3cd84681b445ce4b7dec21d
Mike-E <michael@dragonspark.network>
2022-01-16 14:05:29 -0500

    Updated MarkModified

---
+++ DragonSpark.Application/Security/Identity/Existing.cs
@@ -0,0 +1,9 @@
+﻿using DragonSpark.Application.Entities;
+using DragonSpark.Application.Entities.Editing;
+
+namespace DragonSpark.Application.Security.Identity;
+
+public class Existing<T> : EditExisting<T> where T : IdentityUser
+{
+	protected Existing(IEnlistedScopes scopes) : base(scopes) {}
+}
--- DragonSpark.Application/Security/Identity/MarkModified.cs
+++ DragonSpark.Application/Security/Identity/MarkModified.cs
@@ -1,26 +1,26 @@
-﻿using DragonSpark.Application.Entities.Editing;
-using DragonSpark.Compose;
-using DragonSpark.Runtime;
+﻿using DragonSpark.Runtime;
 using System.Threading.Tasks;
 
 namespace DragonSpark.Application.Security.Identity;
 
 sealed class MarkModified<T> : IMarkModified<T> where T : IdentityUser
 {
+	readonly IUsers<T> _users;
 	readonly ITime     _time;
-	readonly Update<T> _save;
 
-	public MarkModified(Save<T> save) : this(save, Time.Default) {}
+	public MarkModified(IUsers<T> edit) : this(edit, Time.Default) {}
 
-	public MarkModified(Save<T> save, ITime time)
+	public MarkModified(IUsers<T> users, ITime time)
 	{
-		_save = save;
-		_time = time;
+		_users = users;
+		_time  = time;
 	}
 
 	public async ValueTask Get(T parameter)
 	{
-		parameter.Modified = _time.Get();
-		await _save.Await(parameter);
+		using var users   = _users.Get();
+		var       subject = await users.Subject.FindByIdAsync(parameter.Id.ToString()).ConfigureAwait(false);
+		subject.Modified = _time.Get();
+		await users.Subject.UpdateAsync(subject).ConfigureAwait(false);
 	}
 }
