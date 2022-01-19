2f82519fd66b3a631ba83ca38047c67cb9a55bf1
Mike-E <michael@dragonspark.network>
2022-01-19 06:47:40 -0500

    Moved class to file

---
+++ DragonSpark.Application/Security/Identity/Claims/Compile/CopyKnownClaims.cs
@@ -0,0 +1,6 @@
+﻿namespace DragonSpark.Application.Security.Identity.Claims.Compile;
+
+public sealed class CopyKnownClaims : CopyClaims
+{
+	public CopyKnownClaims(IKnownClaims known) : base(new ClaimNames(known)) {}
+}
--- DragonSpark.Application/Security/Identity/Claims/Compile/DeferredCopyClaims.cs
+++ DragonSpark.Application/Security/Identity/Claims/Compile/DeferredCopyClaims.cs
@@ -6,10 +6,3 @@ sealed class DeferredCopyClaims : CopyClaims
 {
 	public DeferredCopyClaims(Func<IKnownClaims> claims) : base(new DeferredClaimNames(claims)) {}
 }
-// TODO:
-
-public sealed class CopyKnownClaims : CopyClaims
-{
-	public CopyKnownClaims(IKnownClaims known) : base(new ClaimNames(known)) {}
-}
-
