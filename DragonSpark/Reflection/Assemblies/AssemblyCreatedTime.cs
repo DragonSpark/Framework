1a189dbf5d2eb1368a167c5cf8f48b5279c8bba1
Mike-E <michael@dragonspark.network>
2022-01-24 07:12:02 -0500

    Made building more Hot Reload-friendly

--- DragonSpark/Reflection/Assemblies/AssemblyCreatedTime.cs
+++ DragonSpark/Reflection/Assemblies/AssemblyCreatedTime.cs
@@ -1,13 +1,21 @@
 ﻿using DragonSpark.Compose;
 using DragonSpark.Model.Selection;
+using DragonSpark.Runtime;
 using System;
 using System.Reflection;
 
 namespace DragonSpark.Reflection.Assemblies;
 
-sealed class AssemblyCreatedTime : Select<Assembly, DateTimeOffset>
+sealed class AssemblyCreatedTime : ISelect<Assembly, DateTimeOffset>
 {
 	public static AssemblyCreatedTime Default { get; } = new();
 
-	AssemblyCreatedTime() : base(x => x.Attribute<AssemblyBuildDateAttribute>().Verify().On) {}
+	AssemblyCreatedTime() : this(Time.Default) {}
+
+	readonly ITime _time;
+
+	public AssemblyCreatedTime(ITime time) => _time = time;
+
+	public DateTimeOffset Get(Assembly parameter)
+		=> parameter.Attribute<AssemblyBuildDateAttribute>()?.On ?? _time.Get();
 }
