20fc09e98d92f4345423ea78d7f4a2de9c1eb3df
Mike-E <michael@dragonspark.network>
2022-02-23 07:03:29 -0500

    Debug

--- DragonSpark.Presentation/Connections/Initialization/Initialized.cs
+++ DragonSpark.Presentation/Connections/Initialization/Initialized.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DragonSpark.Presentation.Environment.Browser;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace DragonSpark.Presentation.Connections.Initialization;
 
@@ -6,15 +8,19 @@ sealed class Initialized : IInitialized
 {
 	readonly IIsInitialized        _initialized;
 	readonly IInitializeConnection _initialize;
+	readonly ILogger<Initialized>  _logger;
 
-	public Initialized(IIsInitialized initialized, IInitializeConnection initialize)
+	public Initialized(IIsInitialized initialized, IInitializeConnection initialize, ILogger<Initialized> logger)
 	{
 		_initialized = initialized;
 		_initialize  = initialize;
+		_logger      = logger;
 	}
 
 	public bool Get(HttpContext parameter)
 	{
+		_logger.LogInformation("User-Agent: {UserAgent} - {IsBot}",
+		                       UserAgent.Default.Get(parameter.Request), IsBot.Default.Get(parameter.Request));
 		var result = _initialized.Get(parameter);
 		if (!result)
 		{
