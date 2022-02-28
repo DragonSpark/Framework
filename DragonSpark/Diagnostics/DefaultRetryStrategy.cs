aabbe6f5e5a120250e7f720533a61b17b7b901da
Mike-E <michael@dragonspark.network>
2022-02-28 07:29:03 -0500

    ~20 second retry

--- DragonSpark/Diagnostics/DefaultRetryStrategy.cs
+++ DragonSpark/Diagnostics/DefaultRetryStrategy.cs
@@ -4,5 +4,5 @@ public sealed class DefaultRetryStrategy : RetryStrategy
 {
 	public static DefaultRetryStrategy Default { get; } = new();
 
-	DefaultRetryStrategy() : base(1.6f) {}
+	DefaultRetryStrategy() : base(1.5f) {}
 }
