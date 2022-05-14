2dca60c48dd6d4a3299d31dd53736a818d53cfc6
Mike-E <michael@dragonspark.network>
2022-05-14 15:34:05 -0400

    Save

--- DragonSpark.Application.Hosting.Azure.WebJobs/Hosting.cs
+++ DragonSpark.Application.Hosting.Azure.WebJobs/Hosting.cs
@@ -1,13 +1,7 @@
-﻿using DragonSpark.Compose;
-using DragonSpark.Model.Commands;
+﻿using DragonSpark.Model.Commands;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using NetFabric.Hyperlinq;
 using System;
-using System.Buffers;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace DragonSpark.Application.Hosting.Azure.WebJobs;
 
@@ -22,41 +16,5 @@ sealed class Hosting : ICommand<IHostBuilder>
 		parameter.ConfigureWebJobs(x => x.AddAzureStorageCoreServices()
 		                                 .AddAzureStorageBlobs()
 		                                 .AddAzureStorageQueues(_queues));
-	}
-}
-
-sealed class WithWebJobConfigurationAdjustments : ICommand<IHostBuilder>
-{
-	public static WithWebJobConfigurationAdjustments Default { get; } = new();
-
-	WithWebJobConfigurationAdjustments() : this(AdjustWebJobConfigurations.Default.Execute) {}
-
-	readonly Action<HostBuilderContext, IConfigurationBuilder> _adjust;
-
-	public WithWebJobConfigurationAdjustments(Action<HostBuilderContext, IConfigurationBuilder> adjust)
-		=> _adjust = adjust;
-
-	public void Execute(IHostBuilder parameter)
-	{
-		parameter.ConfigureAppConfiguration(_adjust);
-	}
-}
-
-sealed class AdjustWebJobConfigurations : ICommand<(HostBuilderContext, IConfigurationBuilder)>
-{
-	public static AdjustWebJobConfigurations Default { get; } = new();
-
-	AdjustWebJobConfigurations() : this(ArrayPool<IConfigurationSource>.Shared) {}
-
-	readonly ArrayPool<IConfigurationSource> _pool;
-
-	public AdjustWebJobConfigurations(ArrayPool<IConfigurationSource> pool) => _pool = pool;
-
-	public void Execute((HostBuilderContext, IConfigurationBuilder) parameter)
-	{
-		var (_, builder) = parameter;
-		using var items = builder.Sources.ToArray()[..^2].AsValueEnumerable().ToArray(_pool);
-		builder.Sources.Clear();
-		builder.Sources.To<List<IConfigurationSource>>().AddRange(items);
 	}
 }
