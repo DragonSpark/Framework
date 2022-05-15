605d0c053ebaea630680718e465f5b139d1b9e7b
Mike-E <michael@dragonspark.network>
2022-05-15 10:13:45 -0400

    EntityInitializer

---
+++ DragonSpark.Application/Entities/Initialization/EntityInitializer.cs
@@ -0,0 +1,18 @@
+﻿using DragonSpark.Model.Sequences;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Application.Entities.Initialization;
+
+public class EntityInitializer<T> : IInitializer where T : class
+{
+	readonly Array<T> _entities;
+
+	protected EntityInitializer(params T[] entities) => _entities = entities;
+
+	public async ValueTask Get(DbContext parameter)
+	{
+		parameter.Set<T>().AddRange(_entities);
+		await parameter.SaveChangesAsync().ConfigureAwait(false);
+	}
+}
