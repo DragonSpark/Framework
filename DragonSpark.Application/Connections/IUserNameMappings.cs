3cbaf5c2f7469a40efb817bb80c7b19255e1b549
Mike-E <michael@dragonspark.network>
2022-05-22 11:39:06 -0400

    Simplified user hub

--- DragonSpark.Application/Connections/IUserNameMappings.cs
+++
@@ -1,7 +0,0 @@
-﻿using DragonSpark.Model.Selection;
-using DragonSpark.Model.Selection.Stores;
-using Microsoft.AspNetCore.SignalR;
-
-namespace DragonSpark.Application.Connections;
-
-public interface IUserNameMappings : ISelect<HubCallerContext, ITable<string, string>> {}
--- DragonSpark.Application/Connections/UserAwareHub.cs
+++ DragonSpark.Application/Connections/UserAwareHub.cs
@@ -1,67 +1,24 @@
-﻿using DragonSpark.Compose;
-using DragonSpark.Model;
-using DragonSpark.Model.Selection.Stores;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DragonSpark.Application.Connections;
 
 public class UserAwareHub : Hub
 {
-	readonly ConcurrentDictionary<string, List<UserConnection>> _store;
-	readonly ITable<string, List<UserConnection>>               _names;
-
-	protected UserAwareHub() : this(new MappedConnections()) {}
-
-	protected UserAwareHub(ConcurrentDictionary<string, List<UserConnection>> store)
-		: this(store, Tables.Default.Get(store)) {}
-
-	protected UserAwareHub(ConcurrentDictionary<string, List<UserConnection>> store,
-	                       ITable<string, List<UserConnection>> names)
-	{
-		_store = store;
-		_names = names;
-	}
-
-	public uint Count => _store.Count.Grade();
-
 	public override Task OnConnectedAsync()
 	{
 		var name = Context.User?.Identity?.Name;
-		if (name != null)
-		{
-			var user = Context.UserIdentifier.Verify();
-			var list = _names.Get(name);
-			lock (list)
-			{
-				list.Add(new UserConnection(user, Context.ConnectionId));
-			}
-		}
-
-		return base.OnConnectedAsync();
+		return name != null ? Groups.AddToGroupAsync(Context.ConnectionId, name) : base.OnConnectedAsync();
 	}
 
-	protected IClientProxy Identifiers(string user)
-		=> Clients.Users(_names.IsSatisfiedBy(user)
-			                 ? _names.Get(user).Select(x => x.User).Distinct()
-			                 : Empty.Enumerable<string>());
+	protected IClientProxy Identifiers(string user) => Clients.Group(user);
 
 	public override Task OnDisconnectedAsync(Exception? exception)
 	{
 		var name = Context.User?.Identity?.Name;
-		if (name != null)
-		{
-			var list = _names.Get(name);
-			lock (list)
-			{
-				list.RemoveAll(x => x.User == Context.UserIdentifier && x.Connection == Context.ConnectionId);
-			}
-		}
-
-		return base.OnDisconnectedAsync(exception);
+		return name != null
+			       ? Groups.RemoveFromGroupAsync(Context.ConnectionId, name)
+			       : base.OnDisconnectedAsync(exception);
 	}
 }
