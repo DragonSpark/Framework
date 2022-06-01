69940823dcc119228b0563859eeeb69e9b5b2e52
Mike-E <michael@dragonspark.network>
2022-06-01 08:11:57 -0400

    EditingOrDefault

---
+++ DragonSpark.Application/Entities/Editing/EditingOrDefault.cs
@@ -0,0 +1,9 @@
+﻿using DragonSpark.Application.Entities.Queries.Composition;
+
+namespace DragonSpark.Application.Entities.Editing;
+
+public class EditingOrDefault<TIn, T> : Edit<TIn, T?>
+{
+	protected EditingOrDefault(IScopes context, IQuery<TIn, T> query)
+		: base(context.Then().Use(query).Edit.SingleOrDefault()) {}
+}
