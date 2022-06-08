e0b7dbd50da0885d98202e20ccd40374cd914804
Mike-E <michael@dragonspark.network>
2022-06-08 15:57:09 -0400

    Blob Storage Result

--- DragonSpark.Server/Requests/View.cs
+++ DragonSpark.Server/Requests/View.cs
@@ -3,43 +3,9 @@ using System;
 
 namespace DragonSpark.Server.Requests;
 
-public readonly struct View
+public readonly record struct View(Controller Owner, Guid Subject)
 {
-	public static implicit operator Query(View instance) => new Query(instance.Owner, instance.Subject);
-
-	public View(Controller owner, Guid subject)
-	{
-		Owner   = owner;
-		Subject = subject;
-	}
-
-	public Controller Owner { get; }
-
-	public Guid Subject { get; }
-
-	public void Deconstruct(out Controller owner, out Guid subject)
-	{
-		owner   = Owner;
-		subject = Subject;
-	}
-		
+	public static implicit operator Query(View instance) => new (instance.Owner, instance.Subject);
 }
 
-public readonly struct View<T>
-{
-	public View(Controller owner, T subject)
-	{
-		Owner   = owner;
-		Subject = subject;
-	}
-
-	public Controller Owner { get; }
-
-	public T Subject { get; }
-
-	public void Deconstruct(out Controller owner, out T subject)
-	{
-		owner   = Owner;
-		subject = Subject;
-	}
-}
+public readonly record struct View<T>(Controller Owner, T Subject);
