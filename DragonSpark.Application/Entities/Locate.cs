ef1db3b05f7d6474b36e5b7d16f4edff99158099
Mike-E <michael@dragonspark.network>
2022-06-12 07:05:43 -0400

    Local/Locate

---
+++ DragonSpark.Application/Entities/Locate.cs
@@ -0,0 +1,14 @@
+﻿using DragonSpark.Application.Entities.Queries.Composition;
+using DragonSpark.Model.Operations;
+using System;
+
+namespace DragonSpark.Application.Entities;
+
+public class Locate<TKey, T> : Coalesce<T, T> where T : class
+{
+	protected Locate(IScopes scopes, Func<T, TKey> key, IQuery<T, T> query)
+		: this(scopes, key, scopes.Then().Use(query).To.Single()) {}
+
+	protected Locate(IScopes scopes, Func<T, TKey> key, ISelecting<T, T> second)
+		: base(new Local<TKey, T>(scopes, key), second) {}
+}
