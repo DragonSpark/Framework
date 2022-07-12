061136418f081578b8bc3df9dd657a1a039b22b6
Mike-E <michael@dragonspark.network>
2022-07-12 08:47:59 -0400

    AdaptiveGrid ProtectedSessionStorage

---
+++ DragonSpark.Syncfusion/Components/GridStateVariable.cs
@@ -0,0 +1,11 @@
+﻿using DragonSpark.Compose;
+using DragonSpark.Presentation.Environment.Browser;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace DragonSpark.SyncfusionRendering.Components;
+
+public sealed class GridStateVariable : ClientVariable<string>
+{
+	public GridStateVariable(string identity, ProtectedSessionStorage storage)
+		: base($"{A.Type<GridStateVariable>().AssemblyQualifiedName}+{identity}", storage) {}
+}
