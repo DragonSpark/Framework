09ced9d8948f1d36fb58fce693fa99d3a5bf8846
Mike-E <michael@dragonspark.network>
2022-07-17 07:42:42 -0400

    Constructor visibility

--- DragonSpark.Server/Communication/ConnectionPath.cs
+++ DragonSpark.Server/Communication/ConnectionPath.cs
@@ -8,10 +8,10 @@ namespace DragonSpark.Server.Communication;
 
 public class ConnectionPath : Instance<Uri>, ISelect<string, Uri>
 {
-	public ConnectionPath(IConfiguration configuration, string name)
+	protected ConnectionPath(IConfiguration configuration, string name)
 		: this(new Uri(configuration.GetConnectionString(name))) {}
 
-	public ConnectionPath(Uri root) : base(root) {}
+	protected ConnectionPath(Uri root) : base(root) {}
 
 	public Uri Get(string parameter) => Get().AppendPathSegment(parameter).ToUri();
 }
