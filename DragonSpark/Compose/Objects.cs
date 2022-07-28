4793da1035b5142840e686cd2c497784cbaf9ade
Mike-E <michael@dragonspark.network>
2022-07-28 14:48:18 -0400

    Currency extension method

--- DragonSpark/Compose/Objects.cs
+++ DragonSpark/Compose/Objects.cs
@@ -75,9 +75,7 @@ public static partial class ExtensionMethods
 
 	public static (T1, T2) Tuple<T1, T2>(this T1 @this, T2 other) => (@this, other);
 
-
 	public static Pair<T1, T2> Pair<T1, T2>(this T1 @this, T2 other) => Pairs.Create(@this, other);
-
 
 	public static string? NullIfEmpty(this string? target) => string.IsNullOrEmpty(target) ? null : target;
 
@@ -103,12 +101,10 @@ public static partial class ExtensionMethods
 	public static T Verify<T>(this T? @this, string message = "Provided instance is not assigned.")
 		=> @this ?? throw new InvalidOperationException(message);
 
-
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static T Value<T>(this T? @this, string message = "Provided instance is not assigned.")
 		where T : struct
 		=> @this ?? throw new InvalidOperationException(message);
-
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static T? Account<T>(this T @this) => (T?)@this;
@@ -137,6 +133,7 @@ public static partial class ExtensionMethods
 	public static bool Inverse(this in bool @this) => !@this;
 
 	public static short Degrade(this in ushort @this) => (short)@this;
+
 	public static int Degrade(this in ulong @this) => (int)@this;
 
 	public static int Degrade(this in uint @this) => (int)@this;
@@ -146,37 +143,53 @@ public static partial class ExtensionMethods
 	public static ulong Grade(this in long @this) => (ulong)@this;
 
 	public static int Contract(this in long @this) => (int)@this;
+
 	public static short Contract(this in int @this) => (short)@this;
+
 	public static byte Contract(this in short @this) => (byte)@this;
 
 	public static uint Contract(this in ulong @this) => (uint)@this;
+
 	public static ushort Contract(this in uint @this) => (ushort)@this;
+
 	public static byte Contract(this in ushort @this) => (byte)@this;
 
 	public static ushort Expand(this in byte @this) => @this;
+
 	public static uint Expand(this in ushort @this) => @this;
+
 	public static ulong Expand(this in uint @this) => @this;
 
 	public static short Expand(this in sbyte @this) => @this;
+
 	public static int Expand(this in short @this) => @this;
+
 	public static long Expand(this in int @this) => @this;
 
 	public static float Real(this in byte @this) => @this;
+
 	public static float Real(this in ushort @this) => @this;
+
 	public static float Real(this in uint @this) => @this;
 
 	public static float Real(this in sbyte @this) => @this;
+
 	public static float Real(this in short @this) => @this;
+
 	public static float Real(this in int @this) => @this;
 
+	public static decimal Currency(this in decimal @this) => Math.Round(@this * 100) / 100;
 
 	public static double Expand(this in float @this) => @this;
 
 	public static decimal Expand(this in double @this) => (decimal)@this;
 
 	public static double Contract(this in decimal @this) => (double)@this;
+
 	public static float Contract(this in double @this) => (float)@this;
+
 	public static long Clip(this in float @this) => (long)@this;
+
 	public static long Clip(this in double @this) => (long)@this;
 
 	public static long Clip(this in decimal @this) => (long)@this;
@@ -184,12 +197,15 @@ public static partial class ExtensionMethods
 	public static uint Next(this in uint @this) => @this + 1;
 
 	public static byte Plus(this in byte @this, in byte other) => (byte)(@this + other);
+
 	public static byte Minus(this in byte @this, in byte other) => (byte)(@this - other);
 
 	public static byte Next(this in byte @this) => @this.Plus(1);
 
 	public static byte Previous(this in byte @this) => @this.Minus(1);
+
 	public static uint Previous(this in uint @this) => @this - 1;
+
 	public static int Previous(this in int @this) => @this - 1;
 
 	public static int Next(this in int @this) => @this + 1;
