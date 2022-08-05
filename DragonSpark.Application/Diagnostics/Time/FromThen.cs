5c9ee7f468c14916ef0dfb84b8b4d769d564ec43
Mike-E <michael@dragonspark.network>
2022-08-05 16:28:58 -0400

    Build fixer

--- DragonSpark.Application/Diagnostics/Time/FromThen.cs
+++ DragonSpark.Application/Diagnostics/Time/FromThen.cs
@@ -3,22 +3,6 @@ using System;
 
 namespace DragonSpark.Application.Diagnostics.Time;
 
-sealed class FromNow : IWindow
-{
-	readonly ITime    _time;
-	readonly TimeSpan _window;
-
-	public FromNow(TimeSpan window) : this(DragonSpark.Runtime.Time.Default, window) {}
-
-	public FromNow(ITime time, TimeSpan window)
-	{
-		_time   = time;
-		_window = window;
-	}
-
-	public bool Get(DateTimeOffset parameter) => parameter <= _time.Get() + _window;
-}
-
 sealed class FromThen : IWindow
 {
 	readonly ITime    _time;
