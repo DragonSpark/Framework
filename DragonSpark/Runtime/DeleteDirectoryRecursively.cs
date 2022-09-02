9170093a76dc3a55f73f623d2ceacf1dc7397e2a
Mike-E <michael@dragonspark.network>
2022-09-02 05:40:13 -0400

    DeleteDirectoryRecursively

---
+++ DragonSpark/Runtime/DeleteDirectoryRecursively.cs
@@ -0,0 +1,28 @@
+﻿using DragonSpark.Model.Commands;
+using System.IO;
+
+namespace DragonSpark.Runtime;
+
+public sealed class DeleteDirectoryRecursively : ICommand<DirectoryInfo>
+{
+	public static DeleteDirectoryRecursively Default { get; } = new();
+
+	DeleteDirectoryRecursively() {}
+
+	public void Execute(DirectoryInfo parameter)
+	{
+		foreach (var directory in parameter.EnumerateDirectories())
+		{
+			Execute(directory);
+		}
+
+		foreach (var file in parameter.EnumerateFiles())
+		{
+			file.Attributes = FileAttributes.Normal;
+			file.Delete();
+		}
+
+		parameter.Delete(true);
+	}
+}
+
