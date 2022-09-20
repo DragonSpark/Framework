72e988eefd29e8203382bc1463cc05f657743d02
Mike-E <michael@dragonspark.network>
2022-09-20 03:09:42 -0400

    Additional constructor

--- DragonSpark.Application/Entities/Editing/Updating.cs
+++ DragonSpark.Application/Entities/Editing/Updating.cs
@@ -9,8 +9,13 @@ public class Updating<TIn, TOut> : Modifying<TIn, TOut> where TOut : class
 	protected Updating(IScopes scopes, IQuery<TIn, TOut> query) : this(scopes.Then().Use(query).Edit.Single()) {}
 
 	protected Updating(IScopes scopes, ISelecting<TIn, TOut> selecting)
-		: this(new SelectForEdit<TIn, TOut>(scopes, selecting)) {}
+		: this(scopes, selecting, UpdateLocal<TOut>.Default) {}
 
-	protected Updating(IEdit<TIn, TOut> select)
-		: base(@select, UpdateLocal<TOut>.Default.Then().Operation().Out()) {}
+	protected Updating(IScopes scopes, ISelecting<TIn, TOut> selecting, IModify<TOut> modify)
+		: this(new SelectForEdit<TIn, TOut>(scopes, selecting), modify) {}
+
+	protected Updating(IEdit<TIn, TOut> select) : this(select, UpdateLocal<TOut>.Default) {}
+
+	protected Updating(IEdit<TIn, TOut> select, IModify<TOut> modify)
+		: base(@select, modify.Then().Operation().Out()) {}
 }
