2c4e8ca63b07dad1ce0e8ecc76b840f097db152d
Mike-E <michael@dragonspark.network>
2022-12-06 06:18:58 -0500

    FixedScopes

---
+++ DragonSpark.Application/Entities/FixedScopes.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DragonSpark.Application.Entities;
+
+public sealed class FixedScopes : InstanceScopes
+{
+	public FixedScopes(DbContext context) : base(context, EmptyBoundary.Default) {}
+}
