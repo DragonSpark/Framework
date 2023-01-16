3ef5e0ca7f4b09bb41b6bd08360b3b7b5498fc0a
Mike-E <michael@dragonspark.network>
2023-01-16 07:42:28 -0500

    Striping

---
+++ DragonSpark/Runtime/Invocation/Striping.cs
@@ -0,0 +1,27 @@
+﻿using AsyncUtilities;
+using DragonSpark.Compose;
+using DragonSpark.Model.Operations;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Runtime.Invocation;
+
+public class Striping<TIn, TOut> : ISelecting<TIn, TOut> where TIn : notnull
+{
+	readonly ISelecting<TIn, TOut> _previous;
+	readonly StripedAsyncLock<TIn> _lock;
+
+	public Striping(ISelecting<TIn, TOut> previous) : this(previous, new (16)) {}
+
+	public Striping(ISelecting<TIn, TOut> previous, StripedAsyncLock<TIn> @lock)
+	{
+		_previous = previous;
+		_lock     = @lock;
+	}
+
+	public async ValueTask<TOut> Get(TIn parameter)
+	{
+		using var @lock  = await _lock.LockAsync(parameter).ConfigureAwait(false);
+		var       result = await _previous.Await(parameter);
+		return result;
+	}
+}
