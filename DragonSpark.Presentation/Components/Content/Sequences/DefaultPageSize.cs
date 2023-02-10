87b33b7c66c4665f280fa2937faf12b94ace8c28
Mike-E <michael@dragonspark.network>
2023-02-10 15:47:51 -0500

    DefaultPageSize

---
+++ DragonSpark.Presentation/Components/Content/Sequences/DefaultPageSize.cs
@@ -0,0 +1,8 @@
+﻿namespace DragonSpark.Presentation.Components.Content.Sequences;
+
+public sealed class DefaultPageSize : DragonSpark.Model.Results.Instance<byte>
+{
+	public static DefaultPageSize Default { get; } = new();
+
+	DefaultPageSize() : base(12) {}
+}
