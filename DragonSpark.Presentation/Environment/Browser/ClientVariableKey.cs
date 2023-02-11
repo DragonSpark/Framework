bbd9c8679e9910e24055b214067cea31c12f91e3
Mike-E <michael@dragonspark.network>
2023-02-11 08:30:15 -0500

    Key adjustment

--- DragonSpark.Presentation/Environment/Browser/ClientVariableKey.cs
+++ DragonSpark.Presentation/Environment/Browser/ClientVariableKey.cs
@@ -3,9 +3,11 @@ using System;
 
 namespace DragonSpark.Presentation.Environment.Browser;
 
-public readonly record struct ClientVariableKey(Type Owner, Guid Identifier)
+public readonly record struct ClientVariableKey(Type Owner, string Identifier)
 {
-	public ClientVariableKey(object Owner, Guid Identifier) : this(Owner.GetType(), Identifier) {}
+	public ClientVariableKey(object Owner, Guid Identifier) : this(Owner, Identifier.ToString()) {}
+
+	public ClientVariableKey(object Owner, string Identifier) : this(Owner.GetType(), Identifier) {}
 
 	public override string ToString() => $"{Owner.AssemblyQualifiedName.Verify()}+{Identifier}";
 }
