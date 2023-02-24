fbfedf649af7cb6f5afbed9c94971b839981ea7a
Mike-E <michael@dragonspark.network>
2023-02-24 15:35:34 -0500

    SelectInstance

---
+++ DragonSpark.Application/Entities/Queries/Composition/SelectInstance.cs
@@ -0,0 +1,15 @@
+﻿using LinqKit;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DragonSpark.Application.Entities.Queries.Composition;
+
+public class SelectInstance<T, TTo> : Start<T, TTo> where T : class
+{
+	protected SelectInstance(IInstance<TTo> instance) : this(instance.Get().Then().Elide().Get()) {}
+
+	protected SelectInstance(Expression<Func<DbContext, TTo>> general)
+		: base((d, q) => q.AsSplitQuery().Select(_ => general.Invoke(d))) {}
+}
