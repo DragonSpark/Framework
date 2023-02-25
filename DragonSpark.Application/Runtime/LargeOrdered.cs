645ca3c9bdf037d6be1f308ced178b00df6fdf50
Mike-E <michael@dragonspark.network>
2023-02-25 16:25:22 -0500

    Revert

---
+++ DragonSpark.Application/Runtime/LargeOrdered.cs
@@ -0,0 +1,21 @@
+﻿using DragonSpark.Model.Commands;
+using NetFabric.Hyperlinq;
+using System.Collections.Generic;
+
+namespace DragonSpark.Application.Runtime;
+
+sealed class LargeOrdered<T> : ICommand<IEnumerable<T>> where T : class, ILargeOrderAware
+{
+	public static LargeOrdered<T> Default { get; } = new();
+
+	LargeOrdered() {}
+
+	public void Execute(IEnumerable<T> parameter)
+	{
+		var order = 0u;
+		foreach (var aware in parameter.AsValueEnumerable())
+		{
+			aware.Order ??= order++;
+		}
+	}
+}
