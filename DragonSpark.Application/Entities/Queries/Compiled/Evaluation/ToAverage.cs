204776e1f0afd5cef248ef28366e8cab2d3c2a82
Mike-E <michael@dragonspark.network>
2023-02-27 14:49:09 -0500

    Averages

---
+++ DragonSpark.Application/Entities/Queries/Compiled/Evaluation/ToAverage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Application.Entities.Queries.Compiled.Evaluation;
+
+sealed class ToAverage : IEvaluate<decimal, decimal>
+{
+	public static ToAverage Default { get; } = new();
+
+	ToAverage() {}
+
+	public ValueTask<decimal> Get(IAsyncEnumerable<decimal> parameter) => parameter.AverageAsync();
+}
