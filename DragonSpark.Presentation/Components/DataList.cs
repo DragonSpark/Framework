b34fdc591fa38432fb5350658362bc59b6fc9941
Mike-E <michael@dragonspark.network>
2023-02-28 09:34:39 -0500

    DataList page state

--- DragonSpark.Presentation/Components/DataList.cs
+++ DragonSpark.Presentation/Components/DataList.cs
@@ -7,6 +7,28 @@ namespace DragonSpark.Presentation.Components;
 
 public class DataList<T> : RadzenDataList<T>, IRefreshAware
 {
+	bool _update, _assigned;
+
+	[Parameter]
+	public int? PageIndex
+	{
+		get => CurrentPage;
+		set
+		{
+			_assigned  = true;
+			if (_pageIndex != value)
+			{
+				_update    = true;
+				_pageIndex = value;
+			}
+			
+			
+		}
+	} int? _pageIndex;
+
+	[Parameter]
+	public EventCallback<int?> PageIndexChanged { get; set; }
+
 	[CascadingParameter]
 	IRefreshContainer? Container
 	{
@@ -24,19 +46,40 @@ public class DataList<T> : RadzenDataList<T>, IRefreshAware
 		}
 	}	IRefreshContainer? _container;
 
-	protected override void OnParametersSet()
+	public override async Task Reload()
 	{
-		if (Visible && LoadData.HasDelegate && Data == null)
+		if (_assigned)
+		{
+			if (_pageIndex is not null)
+			{
+				await PageIndexChanged.InvokeAsync((int)(_pageIndex = CurrentPage));	
+			}
+			else
+			{
+				return;
+			}
+		}
+		await base.Reload().ConfigureAwait(false);
+	}
+
+	protected override async Task OnParametersSetAsync()
+	{
+		if (Visible && !_assigned && LoadData.HasDelegate && Data == null)
 		{
 			Visible = false;
-			InvokeAsync(Reload);
+			await base.Reload().ConfigureAwait(false);
 		}
 		else
 		{
+			if (_update && !(_update = false) && _pageIndex is not null)
+			{
+				await GoToPage(_pageIndex.Value, true).ConfigureAwait(false);
+			}
 			Visible |= Data != null;
 		}
-		base.OnParametersSet();
 	}
+
+
 
 	public Task Get() => Reload();
 
