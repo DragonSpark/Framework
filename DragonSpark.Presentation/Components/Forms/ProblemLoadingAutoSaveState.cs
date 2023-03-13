c6ee8f734b1f3c0f96fe376fc45acb0caa20d5a5
Mike-E <michael@dragonspark.network>
2023-03-13 17:15:26 -0400

    AutoSave adjustments and improvements

---
+++ DragonSpark.Presentation/Components/Forms/ProblemLoadingAutoSaveState.cs
@@ -0,0 +1,10 @@
+﻿using DragonSpark.Diagnostics.Logging;
+using Microsoft.Extensions.Logging;
+
+namespace DragonSpark.Presentation.Components.Forms;
+
+public sealed class ProblemLoadingAutoSaveState : LogWarningException<string>
+{
+	public ProblemLoadingAutoSaveState(ILogger<ProblemLoadingAutoSaveState> logger)
+		: base(logger, "There was a problem loading auto save state {Content}") {}
+}
