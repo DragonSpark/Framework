ec2ac581d69f489f7185c395715a4cc66b9afc5e
Mike-E <michael@dragonspark.network>
2023-03-30 07:43:37 -0400

    Handled TODOs

--- DragonSpark/Model/Results/Switch.cs
+++ DragonSpark/Model/Results/Switch.cs
@@ -1,23 +1,8 @@
-﻿using DragonSpark.Compose;
-using System;
-
-namespace DragonSpark.Model.Results;
+﻿namespace DragonSpark.Model.Results;
 
 public class Switch : Variable<bool>, ISwitch
 {
 	public static implicit operator bool(Switch instance) => instance.Get();
 
 	public Switch(bool instance = default) : base(instance) {}
-}
-// TODO
-public readonly struct Switching : IDisposable
-{
-	readonly ISwitch _subject;
-
-	public Switching(ISwitch subject) => _subject = subject;
-
-	public void Dispose()
-	{
-		_subject.Down();
-	}
 }
---
+++ DragonSpark/Model/Results/Switching.cs
@@ -0,0 +1,16 @@
+﻿using DragonSpark.Compose;
+using System;
+
+namespace DragonSpark.Model.Results;
+
+public readonly struct Switching : IDisposable
+{
+	readonly ISwitch _subject;
+
+	public Switching(ISwitch subject) => _subject = subject;
+
+	public void Dispose()
+	{
+		_subject.Down();
+	}
+}
