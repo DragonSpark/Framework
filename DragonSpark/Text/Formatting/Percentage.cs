05522ae5e72fa1ff43920e242b7feaf0c34a9199
Mike-E <michael@dragonspark.network>
2023-04-29 11:58:26 -0400

    Display Sold vs Unsold %'s

---
+++ DragonSpark/Text/Formatting/Percentage.cs
@@ -0,0 +1,21 @@
+﻿namespace DragonSpark.Text.Formatting;
+
+public sealed class Percentage : IFormatter<float>
+{
+	public static Percentage Default { get; } = new();
+
+	Percentage() : this("P2") {}
+
+	readonly string _template;
+	readonly string _default;
+
+	public Percentage(string template) : this(template, 0.ToString(template)) {}
+
+	public Percentage(string template, string @default)
+	{
+		_template = template;
+		_default  = @default;
+	}
+
+	public string Get(float parameter) => float.IsNaN(parameter) ? _default : parameter.ToString(_template);
+}
