9e682ba2182c6fc987256ba910af7834529d43ab
Mike-E <michael@dragonspark.network>
2023-05-17 07:59:28 -0400

    Added inventory list actions

--- DragonSpark.Application/Runtime/ThrottleOperation.cs
+++ DragonSpark.Application/Runtime/ThrottleOperation.cs
@@ -16,7 +16,8 @@ public class ThrottleOperation<T> : IOperation<T> where T : notnull
 		: this(subject, interval, new ConcurrentDictionary<T, Timer>()) {}
 
 	public ThrottleOperation(Operate<T> subject, TimeSpan interval, ConcurrentDictionary<T, Timer> store)
-		: this(new ConcurrentTable<T, Timer>(store, new CreateTimer<T>(store, subject, interval.TotalMilliseconds).Get)) {}
+		: this(new ConcurrentTable<T, Timer>(store,
+		                                     new CreateTimer<T>(store, subject, interval.TotalMilliseconds).Get)) {}
 
 	public ThrottleOperation(ISelect<T, Timer> timers) => _timers = timers;
 
@@ -28,25 +29,3 @@ public class ThrottleOperation<T> : IOperation<T> where T : notnull
 		return ValueTask.CompletedTask;
 	}
 }
-/*public class Throttling : ICommand
-{
-	readonly Timer _timer;
-
-	//public Throttling(Operate operate) : this(operate, TimeSpan.FromMilliseconds(750)) {}
-
-	public Throttling(Operate operate, TimeSpan window)
-		: this(operate, new Timer { Enabled = false, AutoReset = false, Interval = window.TotalMilliseconds }) {}
-
-	public Throttling(Operate operate, Timer timer)
-	{
-		_timer = timer;
-		// Who am I to argue: https://stackoverflow.com/questions/38917818/pass-async-callback-to-timer-constructor#comment91001639_38918443
-		timer.Elapsed += (_, _) => operate();
-	}
-
-	public void Execute(None parameter)
-	{
-		_timer.Stop();
-		_timer.Start();
-	}
-}*/
