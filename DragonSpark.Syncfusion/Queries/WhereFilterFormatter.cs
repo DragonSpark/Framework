2b3d0d4a72655fa43838ca5eff7224deb343b6a1
Mike-E <michael@dragonspark.network>
2023-06-12 13:19:02 -0400

    Fix

--- DragonSpark.Syncfusion/Queries/WhereFilterFormatter.cs
+++ DragonSpark.Syncfusion/Queries/WhereFilterFormatter.cs
@@ -1,4 +1,6 @@
-﻿using DragonSpark.Text;
+﻿using DragonSpark.Compose;
+using DragonSpark.Model;
+using DragonSpark.Text;
 using Syncfusion.Blazor.Data;
 using System;
 using System.Linq;
@@ -15,5 +17,6 @@ sealed class WhereFilterFormatter : IFormatter<WhereFilter>
 
 	public WhereFilterFormatter(Func<WhereFilter, string> predicate) => _predicate = predicate;
 
-	public string Get(WhereFilter parameter) => $"{_predicate(parameter)}+{string.Join(' ' , parameter.predicates.Select(_predicate))}";
+	public string Get(WhereFilter parameter)
+		=> $"{_predicate(parameter)}+{string.Join(' ', parameter.predicates.Account()?.Select(_predicate) ?? Empty.Enumerable<string>())}";
 }
