03b42aa195d888563786dcacb788e3c498b819d2
Mike-E <michael@dragonspark.network>
2023-06-14 09:42:13 -0400

    v2 Tweetz

---
+++ DragonSpark.Identity.Twitter/TweetV2PostRequest.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace DragonSpark.Identity.Twitter;
+
+/// <summary>
+/// Attribution: https://github.com/linvi/tweetinvi/issues/1147#issuecomment-1173174302
+/// </summary>
+public class TweetV2PostRequest
+{
+	/// <summary>
+	/// The text of the tweet to post.
+	/// </summary>
+	[JsonProperty("text")]
+	public string Text { get; set; } = string.Empty;
+}
---
+++ DragonSpark.Identity.Twitter/TweetsV2Poster.cs
@@ -0,0 +1,29 @@
+﻿using DragonSpark.Model.Operations.Allocated;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Core.Web;
+
+namespace DragonSpark.Identity.Twitter;
+
+/// <summary>
+/// Attribution: https://github.com/linvi/tweetinvi/issues/1147#issuecomment-1173174302
+/// </summary>
+public sealed class TweetsV2Poster : IAllocating<string, ITwitterResult>
+{
+	readonly ITwitterClient client;
+
+	public TweetsV2Poster(ITwitterClient client) => this.client = client;
+
+	public Task<ITwitterResult> Get(string parameter)
+		=> client.Execute.AdvanceRequestAsync(x =>
+		                                      {
+			                                      var request = new TweetV2PostRequest { Text = parameter };
+			                                      var body    = client.Json.Serialize(request);
+			                                      x.Query.Url        = "https://api.twitter.com/2/tweets";
+			                                      x.Query.HttpMethod = Tweetinvi.Models.HttpMethod.POST;
+			                                      x.Query.HttpContent =
+				                                      new StringContent(body, Encoding.UTF8, "application/json");
+		                                      });
+}
