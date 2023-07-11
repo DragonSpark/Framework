6487c125f52e63cc13434098fe9be4ceb58eb457
Mike-E <michael@dragonspark.network>
2023-07-11 16:05:42 -0400

    Announcer design WIP

--- DragonSpark.Identity.Twitter/TweetsV2Poster.cs
+++ DragonSpark.Identity.Twitter/TweetsV2Poster.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Tweetinvi;
+using Tweetinvi.Client;
 using Tweetinvi.Core.Web;
 
 namespace DragonSpark.Identity.Twitter;
@@ -12,18 +13,25 @@ namespace DragonSpark.Identity.Twitter;
 /// </summary>
 public sealed class TweetsV2Poster : IAllocating<string, ITwitterResult>
 {
-	readonly ITwitterClient client;
+	readonly IExecuteClient _execute;
+	readonly IJsonClient    _json;
 
-	public TweetsV2Poster(ITwitterClient client) => this.client = client;
+	public TweetsV2Poster(ITwitterClient client) : this(client.Execute, client.Json) {}
+
+	public TweetsV2Poster(IExecuteClient execute, IJsonClient json)
+	{
+		_execute = execute;
+		_json    = json;
+	}
 
 	public Task<ITwitterResult> Get(string parameter)
-		=> client.Execute.AdvanceRequestAsync(x =>
-		                                      {
-			                                      var request = new TweetV2PostRequest { Text = parameter };
-			                                      var body    = client.Json.Serialize(request);
-			                                      x.Query.Url        = "https://api.twitter.com/2/tweets";
-			                                      x.Query.HttpMethod = Tweetinvi.Models.HttpMethod.POST;
-			                                      x.Query.HttpContent =
-				                                      new StringContent(body, Encoding.UTF8, "application/json");
-		                                      });
+		=> _execute.AdvanceRequestAsync(x =>
+		                                {
+			                                var request = new TweetV2PostRequest { Text = parameter };
+			                                var body    = _json.Serialize(request);
+			                                x.Query.Url        = "https://api.twitter.com/2/tweets";
+			                                x.Query.HttpMethod = Tweetinvi.Models.HttpMethod.POST;
+			                                x.Query.HttpContent =
+				                                new StringContent(body, Encoding.UTF8, "application/json");
+		                                });
 }
