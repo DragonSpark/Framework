a751bbb76f92fee5cd422d7ff5e8efeaee2d2c3c
Mike-E <michael@dragonspark.network>
2023-09-12 09:57:48 -0400

    Confirm new email

---
+++ DragonSpark.Application/Components/Validation/Class1.cs
@@ -0,0 +1,21 @@
+﻿using DragonSpark.Application.Components.Validation.Expressions;
+using System;
+
+namespace DragonSpark.Application.Components.Validation;
+
+class Class1 {}
+
+public sealed class DifferentValueValidator : IValidateValue<string>
+{
+	readonly string           _other;
+	readonly StringComparison _comparison;
+
+	public DifferentValueValidator(string other,
+	                               StringComparison comparison = StringComparison.InvariantCultureIgnoreCase)
+	{
+		_other      = other;
+		_comparison = comparison;
+	}
+
+	public bool Get(string parameter) => !_other.Equals(parameter, _comparison);
+}
