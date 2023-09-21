5ad5709d184e3413df0e7374dd63012c256fa9c2
Mike-E <michael@dragonspark.network>
2023-09-21 18:31:59 -0400

    Adjust load

--- DragonSpark.Application.Hosting.Azure.WebJobs/DefaultQueueConfiguration.cs
+++ DragonSpark.Application.Hosting.Azure.WebJobs/DefaultQueueConfiguration.cs
@@ -17,7 +17,7 @@ sealed class DefaultQueueConfiguration : ICommand<QueuesOptions>
 
 	public void Execute(QueuesOptions parameter)
 	{
-		parameter.NewBatchThreshold  = 16;
+		parameter.NewBatchThreshold  = 8;
 		parameter.MaxPollingInterval = _poll;
 		parameter.MessageEncoding    = QueueMessageEncoding.None;
 	}
