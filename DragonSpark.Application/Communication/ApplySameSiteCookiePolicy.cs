be181c2832f03e1610f17589514e1c95c004a00d
Mike-E <michael@dragonspark.network>
2023-09-23 12:41:22 -0400

    Same-site cookie policy

---
+++ DragonSpark.Application/Communication/ApplySameSiteCookiePolicy.cs
@@ -0,0 +1,28 @@
+﻿using DragonSpark.Model.Commands;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.CookiePolicy;
+using System;
+
+namespace DragonSpark.Application.Communication;
+
+public sealed class ApplySameSiteCookiePolicy : ICommand<CookiePolicyOptions>
+{
+	public static ApplySameSiteCookiePolicy Default { get; } = new();
+
+	ApplySameSiteCookiePolicy() : this(AssignSameSite.Default.Execute, AssignSameSite.Default.Execute) {}
+
+	readonly Action<AppendCookieContext> _configure;
+	readonly Action<DeleteCookieContext> _delete;
+
+	public ApplySameSiteCookiePolicy(Action<AppendCookieContext> configure, Action<DeleteCookieContext> delete)
+	{
+		_configure = configure;
+		_delete    = delete;
+	}
+
+	public void Execute(CookiePolicyOptions parameter)
+	{
+		parameter.OnAppendCookie = _configure;
+		parameter.OnDeleteCookie = _delete;
+	}
+}
---
+++ DragonSpark.Application/Communication/AssignSameSite.cs
@@ -0,0 +1,41 @@
+﻿using DragonSpark.Model.Commands;
+using DragonSpark.Model.Selection.Conditions;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Http;
+
+namespace DragonSpark.Application.Communication;
+
+sealed class AssignSameSite : ICommand<AppendCookieContext>
+{
+	public static AssignSameSite Default { get; } = new();
+
+	AssignSameSite() : this(AgentShouldOmit.Default) {}
+
+	readonly ICondition<string> _agent;
+
+	public AssignSameSite(ICondition<string> agent) => _agent = agent;
+
+	public void Execute(AppendCookieContext parameter)
+	{
+		Execute(parameter.Context, parameter.CookieOptions);
+	}
+
+	public void Execute(DeleteCookieContext parameter)
+	{
+		Execute(parameter.Context, parameter.CookieOptions);
+	}
+
+	void Execute(HttpContext context, CookieOptions options)
+	{
+		switch (options.SameSite)
+		{
+			case SameSiteMode.None:
+				if (_agent.Get(context.Request.Headers.UserAgent.ToString()))
+				{
+					options.SameSite = SameSiteMode.Unspecified;
+				}
+
+				break;
+		}
+	}
+}
