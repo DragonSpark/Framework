410a577eb953ec8c09b8e40a701f610fb012311f
Mike-E <michael@dragonspark.network>
2023-10-01 09:23:30 -0400

    Fix exception state for grid queries

--- DragonSpark.Syncfusion/Queries/StateAwareDataRequest.cs
+++ DragonSpark.Syncfusion/Queries/StateAwareDataRequest.cs
@@ -1,5 +1,6 @@
 ﻿using DragonSpark.Compose;
 using DragonSpark.Model.Operations;
+using DragonSpark.Model.Results;
 using DragonSpark.Presentation.Environment.Browser;
 using Syncfusion.Blazor;
 using System.Threading.Tasks;
@@ -10,23 +11,28 @@ sealed class StateAwareDataRequest : IDataRequest
 {
 	readonly IDataRequest _previous;
 	readonly IOperation   _remove;
+	readonly Switch       _active;
 
-	public StateAwareDataRequest(IDataRequest previous, IClientVariable<string> state) : this(previous, state.Remove) {}
+	public StateAwareDataRequest(IDataRequest previous, IClientVariable<string> state, Switch active)
+		: this(previous, state.Remove, active) {}
 
-	public StateAwareDataRequest(IDataRequest previous, IOperation remove)
+	public StateAwareDataRequest(IDataRequest previous, IOperation remove, Switch active)
 	{
-		_previous = previous;
-		_remove   = remove;
+		_previous    = previous;
+		_remove      = remove;
+		_active = active;
 	}
 
 	public async ValueTask<object> Get(DataManagerRequest parameter)
 	{
 		try
 		{
+			_active.Up();
 			return await _previous.Get(parameter);
 		}
 		catch
 		{
+			_active.Down();
 			await _remove.Await();
 			throw;
 		}
