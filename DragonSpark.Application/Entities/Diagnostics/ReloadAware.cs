075b17aa7c9e89fbd8af8da8fd9c61da470132ed
Mike-E <michael@dragonspark.network>
2023-11-06 07:21:22 -0500

    PolicyAwareNewSalesProfile

--- DragonSpark.Application/Entities/Diagnostics/ReloadAware.cs
+++ DragonSpark.Application/Entities/Diagnostics/ReloadAware.cs
@@ -1,9 +1,15 @@
 ﻿using DragonSpark.Diagnostics;
 using DragonSpark.Model.Operations;
+using DragonSpark.Model.Operations.Selection;
 
 namespace DragonSpark.Application.Entities.Diagnostics;
 
 public class ReloadAware<T> : PolicyAwareOperation<T>
 {
 	public ReloadAware(IOperation<T> previous) : base(previous, ConcurrencyAwarePolicy.Default.Get()) {}
+}
+
+public class ReloadAware<TIn, TOut> : PolicyAwareSelecting<TIn, TOut>
+{
+	protected ReloadAware(ISelecting<TIn, TOut> previous) : base(previous, ConcurrencyAwarePolicy.Default.Get()) {}
 }
