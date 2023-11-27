aa487d3b07a12bb182e2f8103ee99e48f48037b2
Mike-E <michael@dragonspark.network>
2023-11-27 10:51:03 -0500

    Pending + applied migrations

---
+++ DragonSpark.Application/Entities/Configuration/TargetMigrationName.cs
@@ -0,0 +1,6 @@
+﻿namespace DragonSpark.Application.Entities.Configuration;
+
+public sealed class TargetMigrationName : SettingVariable
+{
+	public TargetMigrationName(ISettingAccessor accessor) : base(nameof(TargetMigrationName), accessor) {}
+}
