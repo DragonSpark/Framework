0c468a6b5d2c2ba528fc1198a67d3d0283e1adc1
Mike-E <michael@dragonspark.network>
2024-02-12 15:40:46 -0500

    Revert logging

--- DragonSpark.Azure/Events/Receive/HandleEvent.cs
+++ DragonSpark.Azure/Events/Receive/HandleEvent.cs
@@ -3,7 +3,6 @@ using Azure.Messaging.EventHubs.Processor;
 using DragonSpark.Compose;
 using DragonSpark.Model.Operations;
 using DragonSpark.Model.Selection;
-using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace DragonSpark.Azure.Events.Receive;
@@ -12,15 +11,13 @@ public sealed class HandleEvent : IOperation<ProcessEventArgs>
 {
 	readonly ISelect<EventData, RegistryEntry?> _entry;
 	readonly ProcessEntry                       _process;
-	readonly ILogger<HandleEvent>               _logger;
 
-	public HandleEvent(ProcessEntry process, ILogger<HandleEvent> logger) : this(GetEntry.Default, process, logger) {} // TODO
+	public HandleEvent(ProcessEntry process) : this(GetEntry.Default, process) {}
 
-	public HandleEvent(ISelect<EventData, RegistryEntry?> entry, ProcessEntry process, ILogger<HandleEvent> logger)
+	public HandleEvent(ISelect<EventData, RegistryEntry?> entry, ProcessEntry process)
 	{
-		_entry       = entry;
-		_process     = process;
-		_logger = logger;
+		_entry   = entry;
+		_process = process;
 	}
 
 	public async ValueTask Get(ProcessEventArgs parameter)
@@ -29,7 +26,6 @@ public sealed class HandleEvent : IOperation<ProcessEventArgs>
 		if (entry is not null)
 		{
 			var (key, handlers) = entry;
-			_logger.LogInformation("Entry Received! {Type}", key);
 			var message = await parameter.Data.Data.Verify().ToObjectAsync(key).ConfigureAwait(false);
 			if (message is not null)
 			{
