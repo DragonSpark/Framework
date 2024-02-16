f12addfafa2813e894d5b8ce5ce5adfe548f1cf5
Mike-E <michael@dragonspark.network>
2024-02-16 05:50:17 -0500

    Logging (for now)

--- DragonSpark.Azure/Messaging/Events/Receive/ProcessEvent.cs
+++ DragonSpark.Azure/Messaging/Events/Receive/ProcessEvent.cs
@@ -4,6 +4,7 @@ using DragonSpark.Compose;
 using DragonSpark.Model.Operations;
 using DragonSpark.Model.Operations.Allocated;
 using DragonSpark.Model.Selection;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace DragonSpark.Azure.Messaging.Events.Receive;
@@ -13,15 +14,18 @@ public sealed class ProcessEvent : IAllocated<ProcessEventArgs>
 	readonly ISelect<EventData, RegistryEntry?> _entry;
 	readonly ProcessEntry                       _process;
 	readonly IOperation<ProcessEventArgs>       _tracker;
+	readonly ILogger<ProcessEvent>              _logger;
 
-	public ProcessEvent(ProcessEntry process) : this(GetEntry.Default, process, CheckpointTracker.Default) {}
+	public ProcessEvent(ProcessEntry process, ILogger<ProcessEvent> logger)
+		: this(GetEntry.Default, process, CheckpointTracker.Default, logger) {}
 
 	public ProcessEvent(ISelect<EventData, RegistryEntry?> entry, ProcessEntry process,
-	                    IOperation<ProcessEventArgs> tracker)
+	                    IOperation<ProcessEventArgs> tracker, ILogger<ProcessEvent> logger)
 	{
 		_entry   = entry;
 		_process = process;
 		_tracker = tracker;
+		_logger  = logger;
 	}
 
 	public async Task Get(ProcessEventArgs parameter)
@@ -30,6 +34,7 @@ public sealed class ProcessEvent : IAllocated<ProcessEventArgs>
 		if (entry is not null)
 		{
 			var (key, handlers) = entry;
+			_logger.LogInformation("Message Received: {Type} {Handlers}", key, handlers.Count);
 			var message = await parameter.Data.Data.Verify().ToObjectAsync(key).ConfigureAwait(false);
 			if (message is not null)
 			{
