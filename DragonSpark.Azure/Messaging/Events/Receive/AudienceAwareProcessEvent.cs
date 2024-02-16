d608a9deedc6327174af52e25729b178cb937782
Mike-E <michael@dragonspark.network>
2024-02-16 09:06:30 -0500

    Revert

--- DragonSpark.Azure/Messaging/Events/Receive/AudienceAwareProcessEvent.cs
+++ DragonSpark.Azure/Messaging/Events/Receive/AudienceAwareProcessEvent.cs
@@ -1,39 +1,27 @@
 ﻿using Azure.Messaging.EventHubs.Processor;
 using DragonSpark.Model.Operations.Allocated;
 using DragonSpark.Model.Selection.Conditions;
-using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace DragonSpark.Azure.Messaging.Events.Receive;
 
 public sealed class AudienceAwareProcessEvent : IAllocated<ProcessEventArgs>
 {
-	readonly ProcessEvent                       _previous;
-	readonly ICondition<ProcessEventArgs>       _condition;
-	readonly ILogger<AudienceAwareProcessEvent> _logger;
+	readonly ProcessEvent                 _previous;
+	readonly ICondition<ProcessEventArgs> _condition;
 
-	public AudienceAwareProcessEvent(ProcessEvent previous, EventHubConfiguration configuration,
-	                                 ILogger<AudienceAwareProcessEvent> logger)
-		: this(previous, configuration.Audience, logger) {}
+	public AudienceAwareProcessEvent(ProcessEvent previous, EventHubConfiguration configuration)
+		: this(previous, configuration.Audience) {}
 
-	public AudienceAwareProcessEvent(ProcessEvent previous, string? audience, ILogger<AudienceAwareProcessEvent> logger)
-		: this(previous, new ContainsIntendedAudience(audience), logger) {}
+	public AudienceAwareProcessEvent(ProcessEvent previous, string? audience)
+		: this(previous, new ContainsIntendedAudience(audience)) {}
 
-	public AudienceAwareProcessEvent(ProcessEvent previous, ICondition<ProcessEventArgs> condition,
-	                                 ILogger<AudienceAwareProcessEvent> logger)
+	public AudienceAwareProcessEvent(ProcessEvent previous, ICondition<ProcessEventArgs> condition)
 	{
 		_previous  = previous;
 		_condition = condition;
-		_logger    = logger;
 	}
 
 	public Task Get(ProcessEventArgs parameter)
-	{
-		var b    = _condition.Get(parameter);
-		var type = parameter.Data.Properties.TryGetValue(EventType.Default, out var t) ? t : null;
-		_logger.LogInformation("{Hub}/{Partition}/{Offset} - {Type} - {Allowed}", parameter.Partition.EventHubName,
-		                       parameter.Partition.PartitionId, parameter.Data.Offset,
-							   type, b);
-		return b ? _previous.Get(parameter) : Task.CompletedTask;
-	}
+		=> _condition.Get(parameter) ? _previous.Get(parameter) : Task.CompletedTask;
 }
--- DragonSpark.Azure/Messaging/Events/Receive/ProcessEvent.cs
+++ DragonSpark.Azure/Messaging/Events/Receive/ProcessEvent.cs
@@ -4,7 +4,6 @@ using DragonSpark.Compose;
 using DragonSpark.Model.Operations;
 using DragonSpark.Model.Operations.Allocated;
 using DragonSpark.Model.Selection;
-using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace DragonSpark.Azure.Messaging.Events.Receive;
@@ -14,18 +13,15 @@ public sealed class ProcessEvent : IAllocated<ProcessEventArgs>
 	readonly ISelect<EventData, RegistryEntry?> _entry;
 	readonly ProcessEntry                       _process;
 	readonly IOperation<ProcessEventArgs>       _tracker;
-	readonly ILogger<ProcessEvent>              _logger;
 
-	public ProcessEvent(ProcessEntry process, ILogger<ProcessEvent> logger)
-		: this(GetEntry.Default, process, CheckpointTracker.Default, logger) {}
+	public ProcessEvent(ProcessEntry process) : this(GetEntry.Default, process, CheckpointTracker.Default) {}
 
 	public ProcessEvent(ISelect<EventData, RegistryEntry?> entry, ProcessEntry process,
-	                    IOperation<ProcessEventArgs> tracker, ILogger<ProcessEvent> logger)
+	                    IOperation<ProcessEventArgs> tracker)
 	{
 		_entry   = entry;
 		_process = process;
 		_tracker = tracker;
-		_logger  = logger;
 	}
 
 	public async Task Get(ProcessEventArgs parameter)
@@ -34,7 +30,6 @@ public sealed class ProcessEvent : IAllocated<ProcessEventArgs>
 		if (entry is not null)
 		{
 			var (key, handlers) = entry;
-			_logger.LogInformation("Message Received: {Type} {Handlers}", key, handlers.Count);
 			var message = await parameter.Data.Data.Verify().ToObjectAsync(key).ConfigureAwait(false);
 			if (message is not null)
 			{
