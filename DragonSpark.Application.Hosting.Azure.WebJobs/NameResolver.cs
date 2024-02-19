82820f0cf1574660ecdead3fbefc4e8c55424e3c
Mike-E <michael@dragonspark.network>
2024-02-19 14:26:37 -0500

    Revert

--- DragonSpark.Application.Hosting.Azure.WebJobs/NameResolver.cs
+++ DragonSpark.Application.Hosting.Azure.WebJobs/NameResolver.cs
@@ -1,35 +1,26 @@
 ﻿using DragonSpark.Azure.Messaging.Messages;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Logging;
 
 namespace DragonSpark.Application.Hosting.Azure.WebJobs;
 
 sealed class NameResolver : INameResolver
 {
-	readonly string                _audience;
-	readonly bool                  _apply;
-	readonly string                _name;
-	readonly ILogger<NameResolver> _logger;
+	readonly string _audience;
+	readonly bool   _apply;
+	readonly string _name;
 
-	public NameResolver(ServiceBusConfiguration configuration, ILogger<NameResolver> logger)
-		: this(configuration.Audience, logger) {}
+	public NameResolver(ServiceBusConfiguration configuration) : this(configuration.Audience) {}
 
-	public NameResolver(string? audience, ILogger<NameResolver> logger)
+	public NameResolver(string? audience)
 		: this(audience ?? string.Empty, !string.IsNullOrEmpty(audience),
-		       nameof(ServiceBusConfiguration.Audience).ToLowerInvariant(), logger) {}
+		       nameof(ServiceBusConfiguration.Audience).ToLowerInvariant()) {}
 
-	public NameResolver(string audience, bool apply, string name, ILogger<NameResolver> logger)
+	public NameResolver(string audience, bool apply, string name)
 	{
-		_audience    = audience;
-		_apply       = apply;
-		_name        = name;
-		_logger = logger;
+		_audience = audience;
+		_apply    = apply;
+		_name     = name;
 	}
 
-	public string Resolve(string name)
-	{
-		var audience = _apply && _name == name ? _audience : string.Empty;
-		_logger.LogInformation("Resolve: {Name} {Audience} {Result}", name, _name, audience);
-		return audience;
-	}
+	public string Resolve(string name) => _apply && _name == name ? _audience : string.Empty;
 }
