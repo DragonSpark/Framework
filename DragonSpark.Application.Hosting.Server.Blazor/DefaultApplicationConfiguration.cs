6d36f3b931d1faa5fad1edbe756e930b4682e105
Mike-E <michael@dragonspark.network>
2024-03-24 15:36:10 -0400

    Gateway Blazor Web Application WIP

--- DragonSpark.Application.Hosting.Server.Blazor/DefaultApplicationConfiguration.cs
+++ DragonSpark.Application.Hosting.Server.Blazor/DefaultApplicationConfiguration.cs
@@ -1,23 +1,12 @@
 ﻿using DragonSpark.Model.Commands;
 using DragonSpark.Server.Requests.Warmup;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
-using System;
 
 namespace DragonSpark.Application.Hosting.Server.Blazor;
 
 sealed class DefaultApplicationConfiguration : ICommand<IApplicationBuilder>
 {
 	public static DefaultApplicationConfiguration Default { get; } = new();
-
-	DefaultApplicationConfiguration() : this(EndpointConfiguration.Default.Execute) {}
-
-	readonly Action<IEndpointRouteBuilder> _endpoints;
-
-	public DefaultApplicationConfiguration(Action<IEndpointRouteBuilder> endpoints)
-	{
-		_endpoints = endpoints;
-	}
 
 	public void Execute(IApplicationBuilder parameter)
 	{
@@ -26,6 +15,6 @@ sealed class DefaultApplicationConfiguration : ICommand<IApplicationBuilder>
 		         .UseRouting()
 		         .UseAuthentication()
 		         .UseAuthorization()
-		         .UseEndpoints(_endpoints);
+		         .UseAntiforgery();
 	}
 }
