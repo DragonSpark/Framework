d58f501375b912357f18cc6594c52e62398adf4e
Mike-E <michael@dragonspark.network>
2024-03-25 11:28:42 -0400

    Remove older router

--- DragonSpark.Presentation/Components/Routing/RouteContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DragonSpark.Presentation.Components.Routing;
-
-/// <summary>
-/// ATTRIBUTION: https://github.com/ShaunCurtis/CEC.Routing/tree/master/CEC.Routing/Routing
-/// </summary>
-readonly record struct RouteContext(string Path, string[] Segments)
-{
-	public RouteContext(string path) : this(path, Routing.Segments.Default.Get(path)) {}
-}
