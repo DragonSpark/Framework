dee4981788efde7c7a3a3523970170eb0168cfd2
Mike-E <michael@dragonspark.network>
2024-03-25 11:11:10 -0400

    Router adjustment

---
+++ DragonSpark.Presentation/Components/Routing/NavigationAwareComponent.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Presentation.Components.Routing;
+
+public abstract class NavigationAwareComponent : ComponentBase, IAsyncDisposable
+{
+	IDisposable _registration = null!;
+
+	protected override void OnInitialized()
+	{
+		_registration = Navigation.RegisterLocationChangingHandler(OnLocationChanging);
+		base.OnInitialized();
+	}
+
+	async ValueTask OnLocationChanging(LocationChangingContext parameter)
+	{
+		if (await Allow(parameter))
+		{
+			await Exit().ConfigureAwait(false);
+		}
+		else
+		{
+			parameter.PreventNavigation();
+			await OnNavigationCanceled().ConfigureAwait(false);
+		}
+	}
+
+	protected abstract ValueTask<bool> Allow(LocationChangingContext parameter);
+
+	[Inject]
+	protected NavigationManager Navigation { get; set; } = default!;
+
+	[Parameter]
+	public EventCallback Exited { get; set; }
+
+	protected virtual Task OnNavigationCanceled() => Task.CompletedTask;
+
+	protected virtual Task Exit() => Exited.InvokeAsync(this);
+
+	protected virtual void OnDispose(bool disposing) {}
+
+	protected virtual ValueTask OnDisposing()
+	{
+		OnDispose(true);
+		_registration.Dispose();
+		return ValueTask.CompletedTask;
+	}
+
+	public async ValueTask DisposeAsync()
+	{
+		await OnDisposing().ConfigureAwait(false);
+		GC.SuppressFinalize(this);
+	}
+
+	~NavigationAwareComponent()
+	{
+		OnDispose(false);
+	}
+}
