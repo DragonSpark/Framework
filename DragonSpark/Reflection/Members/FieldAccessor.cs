5f99f28ac61096f94c150b507d545042ea9d922e
Mike-E <michael@dragonspark.network>
2024-04-12 12:55:32 -0400

    Addressed TODOs

--- DragonSpark/Reflection/Members/FieldAccessor.cs
+++ DragonSpark/Reflection/Members/FieldAccessor.cs
@@ -1,5 +1,4 @@
-﻿using DragonSpark.Model.Commands;
-using DragonSpark.Model.Selection;
+﻿using DragonSpark.Model.Selection;
 using System.Reflection;
 
 namespace DragonSpark.Reflection.Members;
@@ -9,20 +8,4 @@ public class FieldAccessor<T, TValue> : Select<T, TValue>
 	protected FieldAccessor(string name) : this(new FieldDefinition<T>(name)) {}
 
 	protected FieldAccessor(FieldInfo metadata) : base(FieldValueDelegates<T, TValue>.Default.Get(metadata)) {}
-}
-
-// TODO
-
-public class PropertyAccessor<T, TValue> : Select<T, TValue>
-{
-	protected PropertyAccessor(string name) : this(new PropertyDefinition<T>(name)) {}
-
-	protected PropertyAccessor(PropertyInfo metadata) : base(PropertyValueDelegates<T, TValue>.Default.Get(metadata)) {}
-}
-public class PropertyAssignment<T, TValue> : Assign<T, TValue>
-{
-	protected PropertyAssignment(string name) : this(new PropertyDefinition<T>(name)) {}
-
-	protected PropertyAssignment(PropertyInfo metadata)
-		: base(PropertyAssignmentDelegates<T, TValue>.Default.Get(metadata)) {}
 }
