00eb0c4c6d8a6185b00f650d19d9cd167ffd85f8
Mike-E <michael@dragonspark.network>
2024-04-19 06:52:27 -0400

    Limit amount of rows requested

--- DragonSpark.Application/Entities/Queries/Compiled/Evaluation/EvaluateToAny.cs
+++ DragonSpark.Application/Entities/Queries/Compiled/Evaluation/EvaluateToAny.cs
@@ -1,4 +1,5 @@
-﻿using DragonSpark.Model.Operations.Selection.Conditions;
+﻿using DragonSpark.Model;
+using DragonSpark.Model.Operations.Selection.Conditions;
 using LinqKit;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -7,10 +8,21 @@ using System.Linq.Expressions;
 
 namespace DragonSpark.Application.Entities.Queries.Compiled.Evaluation;
 
+public class EvaluateToAny<T> : EvaluateToAny<None, T>
+{
+	public EvaluateToAny(IScopes scopes, Expression<Func<DbContext, IQueryable<T>>> expression)
+		: base(scopes, expression.Then()) {}
+
+	public EvaluateToAny(IScopes scopes, Expression<Func<DbContext, None, IQueryable<T>>> expression)
+		: base(scopes, expression) {}
+
+	public EvaluateToAny(IReading<None, T> reading) : base(reading) {}
+}
+
 public class EvaluateToAny<TIn, T> : Evaluate<TIn, T, bool>, IDepending<TIn>
 {
 	public EvaluateToAny(IScopes scopes, Expression<Func<DbContext, TIn, IQueryable<T>>> expression)
-		: this(new Reading<TIn, T>(scopes, (d, @in) => expression.Invoke(d, @in))) {}
+		: this(new Reading<TIn, T>(scopes, (d, @in) => expression.Invoke(d, @in).Take(1))) {}
 
 	protected EvaluateToAny(IReading<TIn, T> reading) : base(reading, ToAny<T>.Default) {}
 }
--- DragonSpark.Application/Entities/Queries/Compiled/Evaluation/EvaluateToSingleOrDefault.cs
+++ DragonSpark.Application/Entities/Queries/Compiled/Evaluation/EvaluateToSingleOrDefault.cs
@@ -1,4 +1,5 @@
-﻿using LinqKit;
+﻿using DragonSpark.Model;
+using LinqKit;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
@@ -6,10 +7,21 @@ using System.Linq.Expressions;
 
 namespace DragonSpark.Application.Entities.Queries.Compiled.Evaluation;
 
+public class EvaluateToSingleOrDefault<T> : EvaluateToSingleOrDefault<None, T>
+{
+	public EvaluateToSingleOrDefault(IScopes scopes, Expression<Func<DbContext, IQueryable<T>>> expression)
+		: base(scopes, expression.Then()) {}
+
+	public EvaluateToSingleOrDefault(IScopes scopes, Expression<Func<DbContext, None, IQueryable<T>>> expression)
+		: base(scopes, expression) {}
+
+	public EvaluateToSingleOrDefault(IReading<None, T> reading) : base(reading) {}
+}
+
 public class EvaluateToSingleOrDefault<TIn, T> : Evaluate<TIn, T, T?>
 {
 	public EvaluateToSingleOrDefault(IScopes scopes, Expression<Func<DbContext, TIn, IQueryable<T>>> expression)
-		: this(new Reading<TIn, T>(scopes, (d, @in) => expression.Invoke(d, @in))) {}
+		: this(new Reading<TIn, T>(scopes, (d, @in) => expression.Invoke(d, @in).Take(2))) {}
 
 	protected EvaluateToSingleOrDefault(IReading<TIn, T> reading) : base(reading, ToSingleOrDefault<T>.Default) {}
 }
