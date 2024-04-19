85b0c73e2b3a50beadcc2ececbcbe0327c31ee67
Mike-E <michael@dragonspark.network>
2024-04-19 13:48:46 -0400

    Basic delete

---
+++ DragonSpark.Presentation/Components/Icons/WarningIcon.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace DragonSpark.Presentation.Components.Icons;
+
+public sealed class WarningIcon : DragonSpark.Model.Results.Instance<Icon>
+{
+	public static WarningIcon Default { get; } = new();
+
+	WarningIcon() : base(new Microsoft.FluentUI.AspNetCore.Components.Icons.Regular.Size28.Warning()) {}
+}
