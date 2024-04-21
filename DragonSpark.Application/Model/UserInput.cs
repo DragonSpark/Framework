a951a3708909f381a5840593c6bbc586c82310f2
Mike-E <michael@dragonspark.network>
2024-04-21 06:51:24 -0400

    UserInput reorganization

---
+++ DragonSpark.Application/Model/UserInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DragonSpark.Application.Model;
+
+public readonly record struct UserInput<T>(ulong User, T Input) : IUserIdentity
+{
+	public uint Get() => (uint)User;
+}
+
+public readonly record struct UserInput(ulong User, Guid Input) : IUserIdentity
+{
+	public uint Get() => (uint)User;
+}
