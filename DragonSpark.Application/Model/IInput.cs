dd61ca5edb0089b54a4dca6c8545bcdb570fdc67
Mike-E <michael@dragonspark.network>
2024-04-23 16:14:08 -0400

    Move input

---
+++ DragonSpark.Application/Model/IInput.cs
@@ -0,0 +1,5 @@
+﻿using DragonSpark.Model.Operations.Selection;
+
+namespace DragonSpark.Application.Model;
+
+public interface IInput<TSubject, TOut> : ISelecting<UserInput<TSubject>, TOut>;
---
+++ DragonSpark.Application/Model/IInputted.cs
@@ -0,0 +1,5 @@
+﻿using DragonSpark.Model.Operations;
+
+namespace DragonSpark.Application.Model;
+
+public interface IInputted<T> : IOperation<UserInput<T>>;
