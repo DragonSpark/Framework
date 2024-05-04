0da1f7dc7430156c3099dae7b2424cd7cd4dd064
Mike-E <michael@dragonspark.network>
2024-05-04 12:39:43 -0400

    Empty event

---
+++ DragonSpark.Presentation/Components/Icons/AddIcon.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace DragonSpark.Presentation.Components.Icons;
+
+public sealed class AddIcon : DragonSpark.Model.Results.Instance<Icon>
+{
+	public static AddIcon Default { get; } = new();
+
+	AddIcon() : base(new Microsoft.FluentUI.AspNetCore.Components.Icons.Regular.Size24.AddCircle()) {}
+}
