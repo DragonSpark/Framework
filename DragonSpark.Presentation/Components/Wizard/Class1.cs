c65358b877aafbac69fd7c3769eb7065259eb612
Mike-E <michael@dragonspark.network>
2024-05-12 13:33:34 -0400

    Basic Wizard + AutoSave WIP

---
+++ DragonSpark.Presentation/Components/Wizard/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace DragonSpark.Presentation.Components.Wizard;
+
+class Class1;
+
+public sealed class WizardStepArgs
+{
+	public WizardStepArgs(int index, int active)
+	{
+		Index  = index;
+		Active = index == active;
+	}
+
+	public int Index { get; }
+
+	public bool Active { get; }
+}
+
+public sealed class WizardStepChangeEventArgs
+{
+	/// <summary />
+	public WizardStepChangeEventArgs(int targetIndex, string targetLabel)
+	{
+		TargetIndex = targetIndex;
+		TargetLabel = targetLabel;
+	}
+
+	/// <summary />
+	public int TargetIndex { get; }
+
+	/// <summary />
+	public string TargetLabel { get; }
+
+	/// <summary />
+	public bool IsCancelled { get; set; }
+}
