14a29cf1cdaa6017d0508b7268d3c08434277e54
Mike-E <michael@dragonspark.network>
2024-05-14 13:08:10 -0400

    TODOs

--- DragonSpark.Presentation/Components/Forms/ProblemSavingState.cs
+++ DragonSpark.Presentation/Components/Forms/ProblemSavingState.cs
@@ -8,10 +8,3 @@ public sealed class ProblemSavingState : LogWarningException
 	public ProblemSavingState(ILogger<ProblemSavingState> logger)
 		: base(logger, "There was a problem saving model state") {}
 }
-
-// TODO
-
-public sealed class SavedContentMessage : LogMessage<string>
-{
-	public SavedContentMessage(ILogger<SavedContentMessage> logger) : base(logger, "Saved model content at {Size}") {}
-}
