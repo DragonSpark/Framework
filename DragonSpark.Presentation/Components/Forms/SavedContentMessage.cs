02101ca4cc4dec05ca5bdbb4675852d36f730c70
Mike-E <michael@dragonspark.network>
2024-05-14 13:08:10 -0400

    TODOs

---
+++ DragonSpark.Presentation/Components/Forms/SavedContentMessage.cs
@@ -0,0 +1,9 @@
+﻿using DragonSpark.Diagnostics.Logging;
+using Microsoft.Extensions.Logging;
+
+namespace DragonSpark.Presentation.Components.Forms;
+
+public sealed class SavedContentMessage : LogMessage<string>
+{
+	public SavedContentMessage(ILogger<SavedContentMessage> logger) : base(logger, "Saved model content at {Size}") {}
+}
