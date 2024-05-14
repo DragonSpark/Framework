153dfb7ece83779870e52cfd1cbee5f89535eed5
Mike-E <michael@dragonspark.network>
2024-05-14 09:40:48 -0400

    AutoSave adjustments

--- DragonSpark.Presentation/Components/Forms/ModelPersistenceComponentBase.cs
+++ DragonSpark.Presentation/Components/Forms/ModelPersistenceComponentBase.cs
@@ -1,5 +1,4 @@
-﻿using DragonSpark.Compose;
-using DragonSpark.Model.Results;
+﻿using DragonSpark.Model.Results;
 using DragonSpark.Presentation.Environment.Browser;
 using DragonSpark.Text;
 using Microsoft.AspNetCore.Components;
@@ -8,20 +7,6 @@ namespace DragonSpark.Presentation.Components.Forms;
 
 public class ModelPersistenceComponentBase<T> : ComponentBase where T : class
 {
-	[Parameter]
-	public T Model
-	{
-		get => _model;
-		set
-		{
-			if (_model != value)
-			{
-				_model = value;
-				Save.Down();
-			}
-		}
-	}   T _model = default!;
-
 	[Parameter]
 	public Switch Load { get; set; } = new();
 
