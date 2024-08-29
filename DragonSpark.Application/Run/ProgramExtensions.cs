573094dcd698613c0a492f3515abc26149ba2788
Mike-E <michael@dragonspark.network>
2024-08-29 04:08:44 -0400

    Removed unused feature

--- DragonSpark.Application/Run/ProgramExtensions.cs
+++ DragonSpark.Application/Run/ProgramExtensions.cs
@@ -1,7 +1,5 @@
 ﻿using DragonSpark.Model.Commands;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace DragonSpark.Application.Run;
 
@@ -9,7 +7,4 @@ public static class ProgramExtensions
 {
 	public static ICommand<IHostedApplicationBuilder> Adapt(this ICommand<IServiceCollection> @this)
 		=> new ConfigureServicesAdapter(@this);
-
-	public static ICommand<IApplicationBuilder> Adapt(this ICommand<IHostApplicationBuilder> @this)
-		=> new ApplicationConfigurationAdapter(@this);
 }
