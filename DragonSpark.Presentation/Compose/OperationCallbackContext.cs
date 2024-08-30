79ae791a65daa43f8840d64f6a1e2a29de650f8d
Mike-E <michael@dragonspark.network>
2024-08-30 15:30:54 -0400

    Receiver fix

--- DragonSpark.Presentation/Compose/OperationCallbackContext.cs
+++ DragonSpark.Presentation/Compose/OperationCallbackContext.cs
@@ -38,7 +38,7 @@ public sealed class OperationCallbackContext : IResult<EventCallback>
 	public OperationCallbackContext UpdateActivity() => UpdateActivity(ActivityReceiverInput.Default);
 
 	public OperationCallbackContext UpdateActivity(ActivityReceiverInput input)
-		=> new(_receiver, new ActivityAwareOperation(_operation, _receiver, input), _activity);
+		=> new(_receiver, new ActivityAwareOperation(_operation, _activity ?? _receiver, input), _activity);
 
 	public OperationCallbackContext Watching(IRenderState parameter)
 		=> new(_receiver, new ActiveRenderAwareOperation(_operation, parameter), _activity);
@@ -53,39 +53,36 @@ public sealed class OperationCallbackContext<T> : IResult<EventCallback<T>>
 
 	readonly object             _receiver;
 	readonly IOperation<T>      _operation;
-	readonly IActivityReceiver? _activity;
 
-	public OperationCallbackContext(object receiver, IOperation<T> operation, IActivityReceiver? activity = null)
+	public OperationCallbackContext(object receiver, IOperation<T> operation)
 	{
 		_receiver  = receiver;
 		_operation = operation;
-		_activity  = activity;
 	}
 
 	public OperationCallbackContext<T> Block() => Block(TimeSpan.FromSeconds(1.5));
 
 	public OperationCallbackContext<T> Block(TimeSpan duration)
-		=> new(_receiver, new BlockingEntryOperation<T>(_operation, duration), _activity);
+		=> new(_receiver, new BlockingEntryOperation<T>(_operation, duration));
 
 	// TODO:
 	public OperationCallbackContext<T> UpdateActivityWithRefresh() => UpdateActivity(ActivityReceiverInput.WithRefresh);
 
 	// TODO: Move to UpdateActivity
-	public OperationCallbackContext<T> Using(IActivityReceiver receiver) => new(_receiver, _operation, receiver);
+	public OperationCallbackContext<T> Using(IActivityReceiver receiver) => new(receiver, _operation);
 
 	public OperationCallbackContext<T> UpdateActivity() => UpdateActivity(ActivityReceiverInput.Default);
 
 	public OperationCallbackContext<T> UpdateActivity(ActivityReceiverInput input)
-		=> new(_receiver, new ActivityAwareOperation<T>(_operation, _activity ?? _receiver, input), _activity);
+		=> new(_receiver, new ActivityAwareOperation<T>(_operation, _receiver, input));
 
 	public EventCallback<T> Get()
-		=> EventCallback.Factory.Create(_activity ?? _receiver, new Func<T, Task>(_operation.Allocate));
+		=> EventCallback.Factory.Create(_receiver, new Func<T, Task>(_operation.Allocate));
 
 	public EventCallback Adapt()
-		=> EventCallback.Factory.Create(_activity ?? _receiver,
-		                                Start.A.Selection<object>()
-		                                     .By.CastDown<T>()
-		                                     .Select(_operation)
-		                                     .Then()
-		                                     .Allocate());
+		=> EventCallback.Factory.Create(_receiver, Start.A.Selection<object>()
+		                                                .By.CastDown<T>()
+		                                                .Select(_operation)
+		                                                .Then()
+		                                                .Allocate());
 }
