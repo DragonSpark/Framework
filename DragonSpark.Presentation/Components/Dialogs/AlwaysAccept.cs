b4586ab1cb3ae368cad6c0442af3393cdd26266d
Mike-E <michael@dragonspark.network>
2024-09-28 11:33:01 -0400

    AlwaysAccept

---
+++ DragonSpark.Presentation/Components/Dialogs/AlwaysAccept.cs
@@ -0,0 +1,11 @@
+﻿using DragonSpark.Compose;
+using DragonSpark.Model.Operations.Selection;
+
+namespace DragonSpark.Presentation.Components.Dialogs;
+
+public sealed class AlwaysAccept<T> : Selecting<T, DialogResult>, IConfirming<T>
+{
+	public static AlwaysAccept<T> Default { get; } = new();
+
+	AlwaysAccept() : base(_ => DialogResult.Ok.ToOperation()) {}
+}
