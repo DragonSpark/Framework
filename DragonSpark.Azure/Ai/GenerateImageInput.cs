2130c77ec325eded3397562b62a5d53bd0f91fa9
Mike-E <michael@dragonspark.network>
2024-10-16 07:07:49 -0400

    Updated NuGetz

--- DragonSpark.Azure/Ai/GenerateImageInput.cs
+++ DragonSpark.Azure/Ai/GenerateImageInput.cs
@@ -2,11 +2,10 @@
 
 namespace DragonSpark.Azure.Ai;
 
-public readonly record struct GenerateImageInput(
-	string Prompt,
-	GeneratedImageSize Size,
-	GeneratedImageFormat Format = GeneratedImageFormat.Bytes)
+public readonly record struct GenerateImageInput(string Prompt, GeneratedImageSize Size, GeneratedImageFormat Format)
 {
-	public GenerateImageInput(string prompt, GeneratedImageFormat format = GeneratedImageFormat.Bytes)
+	public GenerateImageInput(string prompt) : this(prompt, GeneratedImageFormat.Bytes) {}
+
+	public GenerateImageInput(string prompt, GeneratedImageFormat format)
 		: this(prompt, GeneratedImageSize.W1024xH1024, format) {}
 }
