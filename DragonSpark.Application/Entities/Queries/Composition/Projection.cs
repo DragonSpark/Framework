c34ec7769dfdeaf4a6e7f8ecbe385ca6d3ed29d2
Mike-E <michael@dragonspark.network>
2024-11-04 14:17:52 -0500

    Storefront files adjustments WIP

--- DragonSpark.Application/Entities/Queries/Composition/Projection.cs
+++ DragonSpark.Application/Entities/Queries/Composition/Projection.cs
@@ -6,10 +6,15 @@ namespace DragonSpark.Application.Entities.Queries.Composition;
 public class Projection<TFrom, TTo> : DragonSpark.Model.Results.Instance<Expression<Func<TFrom, TTo>>>,
 									  IProjection<TFrom, TTo>
 {
+	public static implicit operator Func<TFrom, TTo>(Projection<TFrom, TTo> instance) => instance.Get().Compile();
+
 	protected Projection(Expression<Func<TFrom, TTo>> instance) : base(instance) {}
 }
 
 public class Projection<TIn, TFrom, TTo> : DragonSpark.Model.Results.Instance<Expression<Func<TIn, TFrom, TTo>>>
 {
+	public static implicit operator Func<TIn, TFrom, TTo>(Projection<TIn, TFrom, TTo> instance)
+		=> instance.Get().Compile();
+
 	protected Projection(Expression<Func<TIn, TFrom, TTo>> instance) : base(instance) {}
 }
