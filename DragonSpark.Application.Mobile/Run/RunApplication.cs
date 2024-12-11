020e88368dca0014c4cda7e71fa7e1975a81d2b9
Michael DeMond <michael@dragonspark.network>
2024-12-11 15:33:28 -0500

    Various adjustments WIP

--- DragonSpark.Application.Mobile/Run/RunApplication.cs
+++ DragonSpark.Application.Mobile/Run/RunApplication.cs
@@ -1,16 +1,14 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using DragonSpark.Compose;
 using DragonSpark.Model.Operations.Allocated;
-using JetBrains.Annotations;
 using Microsoft.Extensions.Hosting;
 using Microsoft.UI.Xaml;
 using Uno.Extensions.Hosting;
 
 namespace DragonSpark.Application.Mobile.Run;
 
-public abstract class RunApplication : IAllocating<InitializeInput, IApplication>
+public abstract class RunApplication : IAllocating<InitializeInput, Application>
 {
 	readonly Func<InitializeInput, IApplicationBuilder> _builder;
 	readonly Func<IApplicationBuilder, Task<IHost>>     _host;
@@ -22,38 +20,14 @@ public abstract class RunApplication : IAllocating<InitializeInput, IApplication
 		_builder = builder;
 	}
 
-	public async Task<IApplication> Get(InitializeInput parameter)
+	public async Task<Application> Get(InitializeInput parameter)
 	{
 		var builder = _builder(parameter);
-		return new Application(builder, await _host(builder).Await());
+		return new (builder, await _host(builder).Await());
 	}
 }
 
-sealed class Application : IApplication
-{
-	readonly IApplicationBuilder _previous;
-
-	public Application(IApplicationBuilder previous, IHost host)
-	{
-		Host      = host;
-		_previous = previous;
-	}
-
-	public IApplicationBuilder Configure(Action<IHostBuilder> configureHost) => _previous.Configure(configureHost);
-
-	[MustDisposeResource]
-	public IHost Build() => _previous.Build();
-
-	public Microsoft.UI.Xaml.Application App => _previous.App;
-
-	public LaunchActivatedEventArgs Arguments => _previous.Arguments;
-
-	public Window Window => _previous.Window;
-
-	public IDictionary<object, object> Properties => _previous.Properties;
-
-	public IHost Host { get; }
-}
+public sealed record Application(IApplicationBuilder Builder, IHost Host);
 
 // TODO
 public readonly record struct InitializeInput(Microsoft.UI.Xaml.Application Owner, LaunchActivatedEventArgs Arguments);
