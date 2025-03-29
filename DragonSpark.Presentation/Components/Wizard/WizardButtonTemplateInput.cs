0fef8a6604e607caa5b1c257daed375149432142
Mike-E <michael@dragonspark.network>
2025-03-29 04:41:07 -0400

    Age prompt on filter

--- DragonSpark.Presentation/Components/Wizard/WizardButtonTemplateInput.cs
+++ DragonSpark.Presentation/Components/Wizard/WizardButtonTemplateInput.cs
@@ -1,3 +1,3 @@
 ﻿namespace DragonSpark.Presentation.Components.Wizard;
 
-public sealed record WizardButtonTemplateInput(byte Value, bool Active, WizardButtonInput? Previous, WizardButtonInput? Next);
+public sealed record WizardButtonTemplateInput(byte Value, bool Active, WizardButtonInput? Previous, WizardButtonInput Next);
