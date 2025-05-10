c885ee2e6ae16f9f86adaaf185a7c509edce38a5
Mike-E <michael@dragonspark.network>
2025-05-10 11:52:34 -0400

    Relay value component

---
+++ DragonSpark.Presentation/Components/Runtime/RelayComponentBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DragonSpark.Presentation.Components.Runtime;
+
+public class RelayComponentBase<T> : ComponentBase
+{
+	[Parameter]
+	public required bool Enabled { get; set; } = true;
+
+	[Parameter]
+	public required T Input { get; set; }
+
+	[Parameter]
+	public EventCallback<T> Changed { get; set; }
+}
