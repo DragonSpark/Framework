a50eaf3720519e9dde133c9ffd19698e68a79c03
Michael DeMond <michael@dragonspark.network>
2025-05-13 09:53:52 -0400

    Basic API calls

--- DragonSpark.Application/Communication/ConnectionPath.cs
+++ DragonSpark.Application/Communication/ConnectionPath.cs
@@ -1,17 +1,18 @@
-﻿using DragonSpark.Compose;
+using System;
+using DragonSpark.Compose;
 using DragonSpark.Model.Results;
 using DragonSpark.Model.Selection;
 using Microsoft.Extensions.Configuration;
-using System;
 
 namespace DragonSpark.Application.Communication;
 
 public class ConnectionPath : Instance<Uri>, ISelect<string, Uri>
 {
-	protected ConnectionPath(IConfiguration configuration, string name)
-		: this(new Uri(configuration.GetConnectionString(name).Verify())) {}
+    protected ConnectionPath(IConfiguration configuration, string name)
+        : this(new(configuration.GetConnectionString(name)
+                                .Verify($"The configuration section {name} was not found"))) {}
 
-	protected ConnectionPath(Uri root) : base(root) {}
+    protected ConnectionPath(Uri root) : base(root) {}
 
-	public Uri Get(string parameter) => new (Get(), parameter);
+    public Uri Get(string parameter) => new(Get(), parameter);
 }
