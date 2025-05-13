a0880dff211d9a75133f0b1c8da45299a321c18b
Mike-E <michael@dragonspark.network>
2025-05-13 07:21:56 -0400

    Inventory files rough draft

--- DragonSpark.Application/Communication/Http/UserApis.cs
+++ DragonSpark.Application/Communication/Http/UserApis.cs
@@ -1,8 +1,5 @@
 ﻿using DragonSpark.Application.Security.Identity.Bearer;
-using DragonSpark.Composition;
-using DragonSpark.Model.Commands;
 using DragonSpark.Model.Selection.Stores;
-using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Security.Claims;
 
@@ -12,19 +9,4 @@ public sealed class UserApis<T> : ReferenceValueStore<ClaimsPrincipal, T> where 
 {
 	public UserApis(IBearer bearer, Func<T> client)
 		: base(new AuthenticatedApi<T>(new ApplicationUserAwareBearer(bearer), client)) {}
-}
-
-// TODO
-sealed class Registrations : ICommand<IServiceCollection>
-{
-	public static Registrations Default { get; } = new();
-
-	Registrations() {}
-
-	public void Execute(IServiceCollection parameter)
-	{
-		parameter.Start<UserApis<object>>()
-		         .Generic()
-		         .Singleton();
-	}
 }
