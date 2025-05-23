bcea60d518ccdab5fdc5dacb50b36d63e6dc99f0
Mike-E <michael@dragonspark.network>
2025-05-23 08:22:03 -0400

    Validation WIP

--- DragonSpark.Presentation/Components/Forms/Validation/StoreAwareResourceQuery.cs
+++ DragonSpark.Presentation/Components/Forms/Validation/StoreAwareResourceQuery.cs
@@ -1,12 +1,18 @@
-﻿using DragonSpark.Compose;
+﻿using DragonSpark.Application;
+using DragonSpark.Compose;
 using DragonSpark.Model.Operations;
-using DragonSpark.Model.Operations.Selection;
+using DragonSpark.Model.Operations.Selection.Stop;
+using Microsoft.Extensions.Caching.Memory;
+using System;
 
 namespace DragonSpark.Presentation.Components.Forms.Validation;
 
-sealed class StoreAwareResourceQuery : ReferenceStoring<Stop<string>, ResourceQueryRecord?>, IResourceQuery
+sealed class StoreAwareResourceQuery : StopAware<string, ResourceQueryRecord?>, IResourceQuery
 {
-	public StoreAwareResourceQuery(IResourceQuery select)
-		: base(Start.A.Selection<Stop<string>>().By.Calling(x => x.Subject).Select(string.Intern).Select(select)) {}
+	public StoreAwareResourceQuery(IResourceQuery select, IMemoryCache memory)
+		: base(select.Then()
+		             .Store()
+		             .In(memory)
+		             .For(TimeSpan.FromDays(1).Slide())
+		             .Using(Start.A.Selection<Stop<string>>().By.Calling(x => x.Subject))) {}
 }
-
