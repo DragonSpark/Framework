51f88da97f108e2e2507a806a3eeb00b7a1a180b
Mike-E <michael@dragonspark.network>
2025-05-26 07:04:43 -0400

    Nudge

--- DragonSpark/Model/Operations/Stop.cs
+++ DragonSpark/Model/Operations/Stop.cs
@@ -4,6 +4,6 @@ namespace DragonSpark.Model.Operations;
 
 public readonly record struct Stop<T>(T Subject, CancellationToken Token)
 {
-	public static implicit operator T(Stop<T> instance) => instance.Subject; // TODO: 227
+	public static implicit operator T(Stop<T> instance) => instance.Subject;
 	public static implicit operator CancellationToken(Stop<T> instance) => instance.Token;
 }
