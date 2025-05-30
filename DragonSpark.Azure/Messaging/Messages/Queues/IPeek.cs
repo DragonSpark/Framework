709db6eebe27dca448e4eb88eddf229a421fcb58
Mike-E <michael@dragonspark.network>
2025-05-30 05:26:26 -0400

    Basic Peek functionality

---
+++ DragonSpark.Azure/Messaging/Messages/Queues/IPeek.cs
@@ -0,0 +1,6 @@
+﻿using Azure.Messaging.ServiceBus;
+using DragonSpark.Model.Operations.Results;
+
+namespace DragonSpark.Azure.Messaging.Messages.Queues;
+
+public interface IPeek : IResulting<ServiceBusReceivedMessage?>;
---
+++ DragonSpark.Azure/Messaging/Messages/Queues/Peek.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Messaging.ServiceBus;
+using DragonSpark.Compose;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Azure.Messaging.Messages.Queues;
+
+public class Peek : IPeek
+{
+	readonly ServiceBusReceiver _receiver;
+
+	protected Peek(ServiceBusClient client, string name, ServiceBusConfiguration configuration)
+		: this(client, name, configuration.Audience) {}
+
+	protected Peek(ServiceBusClient client, string name, string? audience)
+		: this(client.CreateReceiver($"{name}{audience}")) {}
+
+
+	public Peek(ServiceBusReceiver receiver) => _receiver = receiver;
+
+	public ValueTask<ServiceBusReceivedMessage?> Get() => _receiver.PeekMessageAsync().ToOperation();
+}
