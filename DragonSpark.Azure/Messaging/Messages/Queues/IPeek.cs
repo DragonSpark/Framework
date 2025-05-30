361fa65d4f6de0760bdd41b681f6df74d20c5560
Mike-E <michael@dragonspark.network>
2025-05-30 06:31:02 -0400

    Revert "Basic Peek functionality"
    This reverts commit 709db6eebe27dca448e4eb88eddf229a421fcb58.

--- DragonSpark.Azure/Messaging/Messages/Queues/IPeek.cs
+++
@@ -1,6 +0,0 @@
-﻿using Azure.Messaging.ServiceBus;
-using DragonSpark.Model.Operations.Results;
-
-namespace DragonSpark.Azure.Messaging.Messages.Queues;
-
-public interface IPeek : IResulting<ServiceBusReceivedMessage?>;
