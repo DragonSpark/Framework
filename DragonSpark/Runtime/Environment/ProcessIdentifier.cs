73418ce4f3177413697da885f0ac36d85f379cc6
Mike-E <michael@dragonspark.network>
2025-05-31 04:32:21 -0400

    ProcessIdentifier

---
+++ DragonSpark/Runtime/Environment/ProcessIdentifier.cs
@@ -0,0 +1,12 @@
+﻿using DragonSpark.Model.Results;
+using System;
+
+namespace DragonSpark.Runtime.Environment;
+
+public sealed class ProcessIdentifier : Result<Guid>
+{
+	public static ProcessIdentifier Default { get; } = new();
+
+	ProcessIdentifier()
+		: base(() => new(System.Environment.ProcessId.ToString("00000000-0000-0000-0000-000000000000"))) {}
+}
