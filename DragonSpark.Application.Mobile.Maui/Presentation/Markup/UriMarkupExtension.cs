1735fef9bcd721e66b479260827cb352b63c581f
Michael DeMond <michael@dragonspark.network>
2025-06-05 13:58:35 -0400

    Basic settings screen

---
+++ DragonSpark.Application.Mobile.Maui/Presentation/Markup/UriMarkupExtension.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DragonSpark.Application.Mobile.Maui.Presentation.Markup;
+
+public sealed class UriMarkupExtension : MarkupExtension<Uri>
+{
+    public required string Address { get; set; }
+
+    public override Uri ProvideValue(IServiceProvider serviceProvider) => new(Address);
+}
