1087b10904be8f36b7ef70fc0277966e7c0b394f
Michael DeMond <michael@dragonspark.network>
2025-06-05 13:58:35 -0400

    Basic settings screen

--- DragonSpark.Application.Mobile.Maui/Navigation/Go.cs
+++ DragonSpark.Application.Mobile.Maui/Navigation/Go.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Threading.Tasks;
+using DragonSpark.Application.Mobile.Maui.Model.Navigation;
 using DragonSpark.Compose;
 using DragonSpark.Model.Operations;
+using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Controls;
 
 namespace DragonSpark.Application.Mobile.Maui.Navigation;
@@ -19,4 +22,13 @@ public class Go : IOperation
     }
 
     public ValueTask Get() => _shell.GoToAsync(_state).ToOperation();
+}
+
+// TODO
+
+public sealed class Launch : AsynchronousCommand<Uri>
+{
+    public static Launch Default { get; } = new();
+
+    Launch() : base(Launcher.OpenAsync!) {}
 }
