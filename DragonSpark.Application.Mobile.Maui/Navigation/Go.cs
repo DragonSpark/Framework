f2dd4a7fea8aa352859a130e4037fa4f201470c3
Michael DeMond <michael@dragonspark.network>
2025-06-06 12:06:51 -0400

    TODOs

--- DragonSpark.Application.Mobile.Maui/Navigation/Go.cs
+++ DragonSpark.Application.Mobile.Maui/Navigation/Go.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Threading.Tasks;
-using DragonSpark.Application.Mobile.Maui.Model.Navigation;
 using DragonSpark.Compose;
 using DragonSpark.Model.Operations;
-using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Controls;
 
 namespace DragonSpark.Application.Mobile.Maui.Navigation;
@@ -22,13 +19,4 @@ public class Go : IOperation
     }
 
     public ValueTask Get() => _shell.GoToAsync(_state).ToOperation();
-}
-
-// TODO
-
-public sealed class Launch : AsynchronousCommand<Uri>
-{
-    public static Launch Default { get; } = new();
-
-    Launch() : base(Launcher.OpenAsync!) {}
 }
