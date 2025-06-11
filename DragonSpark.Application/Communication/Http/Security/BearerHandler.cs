ff442733a175531baedfedece620d4cd4ae5a539
Michael DeMond <michael@dragonspark.network>
2025-06-11 12:04:12 -0400

    Expired access token upon refresh bug fix

--- DragonSpark.Application/Communication/Http/Security/BearerHandler.cs
+++ DragonSpark.Application/Communication/Http/Security/BearerHandler.cs
@@ -1,28 +1,20 @@
-using DragonSpark.Compose;
-using DragonSpark.Model.Results;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using DragonSpark.Compose;
 
 namespace DragonSpark.Application.Communication.Http.Security;
 
 public sealed class BearerHandler : DelegatingHandler
 {
 	readonly IAccessTokenProvider _provider;
-	readonly IResult<string?>     _process;
+	
+	public BearerHandler(IAccessTokenProvider provider) => _provider = provider;
 
-	public BearerHandler(IAccessTokenProvider provider) : this(provider, ProcessBearer.Default) {}
-
-	public BearerHandler(IAccessTokenProvider provider, IResult<string?> process)
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+                                                                 CancellationToken cancellationToken)
 	{
-		_provider = provider;
-		_process  = process;
-	}
-
-	protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
-	                                                             CancellationToken cancellationToken)
-	{
-		var token = _process.Get() ?? await _provider.Off(new(request, cancellationToken));
+		var token = await _provider.Off(new(request, cancellationToken));
 		if (token is not null)
 		{
 			request.Headers.Authorization = new("Bearer", token);
