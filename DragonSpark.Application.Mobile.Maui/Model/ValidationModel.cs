e9f2ad8869aecf45d7ba1b2fca2fa4d4501d91ba
Michael DeMond <michael@dragonspark.network>
2025-06-19 13:28:48 -0400

    Login form fix

---
+++ DragonSpark.Application.Mobile.Maui/Model/ValidationModel.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
+using DragonSpark.Model;
+using DragonSpark.Model.Selection.Conditions;
+
+namespace DragonSpark.Application.Mobile.Maui.Model;
+
+public partial class ValidationModel : ObservableObject, ICondition
+{
+    public ValidationModel(bool isValid = true) : this([], [], isValid) {}
+
+    public ValidationModel(Dictionary<string, string[]> local, Dictionary<string, string[]> external,
+                           bool isValid = true)
+    {
+        IsValid  = isValid;
+        Local    = local;
+        External = external;
+    }
+
+    [ObservableProperty]
+    public partial bool IsValid { get; set; }
+
+    public Dictionary<string, string[]> Local { get; }
+    public Dictionary<string, string[]> External { get; }
+
+    public bool Get(None parameter) => IsValid && Local.Count == 0;
+}
--- DragonSpark.Application.Mobile.Maui/Model/ValidationModelBase.cs
+++ DragonSpark.Application.Mobile.Maui/Model/ValidationModelBase.cs
@@ -1,39 +1,22 @@
-using System.Collections.Generic;
-using CommunityToolkit.Mvvm.ComponentModel;
-using DragonSpark.Model;
-using DragonSpark.Model.Selection.Conditions;
-
 namespace DragonSpark.Application.Mobile.Maui.Model;
 
-public partial class ValidationModelBase : ModelBase
+public class ValidationModelBase : ModelBase
 {
     protected ValidationModelBase(bool isValid = true) : this(new ValidationModel(isValid)) {}
 
+    // ReSharper disable once VirtualMemberCallInConstructor
     protected ValidationModelBase(ValidationModel validation) => Validation = validation;
 
-    [ObservableProperty]
-    public partial ValidationModel Validation { get; set; }
-}
-
-// TODO
-
-public partial class ValidationModel : ObservableObject, ICondition
-{
-    public ValidationModel(bool isValid = true) : this([], [], isValid) {}
-
-    public ValidationModel(Dictionary<string, string[]> local, Dictionary<string, string[]> external,
-                           bool isValid = true)
+    public virtual ValidationModel Validation
     {
-        IsValid  = isValid;
-        Local    = local;
-        External = external;
+        get;
+        set
+        {
+            if (!Equals(value, field))
+            {
+                field = value;
+                OnPropertyChanged();
+            }
+        }
     }
-
-    [ObservableProperty]
-    public partial bool IsValid { get; set; }
-
-    public Dictionary<string, string[]> Local { get; }
-    public Dictionary<string, string[]> External { get; }
-
-    public bool Get(None parameter) => IsValid && Local.Count == 0;
 }
