f96bec09e283ca79df5a5d5931f498284f9b81a9
Michael DeMond <michael@dragonspark.network>
2025-06-20 15:16:36 -0400

    Restructure account page

--- DragonSpark.Application/Security/Identity/Profile/CurrentProfile.cs
+++ DragonSpark.Application/Security/Identity/Profile/CurrentProfile.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Threading.Tasks;
 using DragonSpark.Compose;
 
@@ -5,21 +6,26 @@ namespace DragonSpark.Application.Security.Identity.Profile;
 
 sealed class CurrentProfile : ICurrentProfile
 {
-	readonly ICurrentPrincipal _principal;
-	readonly ICreateProfile    _profile;
+    readonly ICurrentPrincipal _principal;
+    readonly ICreateProfile    _profile;
+    readonly Profile           _default;
 
-	public CurrentProfile(ICurrentPrincipal principal, ICreateProfile profile)
-	{
-		_principal = principal;
-		_profile   = profile;
-	}
+    public CurrentProfile(ICurrentPrincipal principal, ICreateProfile profile)
+        : this(principal, profile, DefaultProfile.Default) {}
 
-	public ValueTask<Profile> Get()
-	{
-		var principal     = _principal.Get();
-		var authenticated = principal.IsAuthenticated();
-		var profile       = authenticated ? _profile.Get(principal).To<Profile>() : DefaultProfile.Default;
-		var result        = profile.ToOperation();
-		return result;
-	}
+    public CurrentProfile(ICurrentPrincipal principal, ICreateProfile profile, Profile @default)
+    {
+        _principal = principal;
+        _profile   = profile;
+        _default   = @default;
+    }
+
+    public ValueTask<Profile> Get(CancellationToken parameter)
+    {
+        var principal     = _principal.Get();
+        var authenticated = principal.IsAuthenticated();
+        var profile       = authenticated ? _profile.Get(new(principal, parameter)).To<Profile>() : _default;
+        var result        = profile.ToOperation();
+        return result;
+    }
 }
