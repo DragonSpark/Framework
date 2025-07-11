477e9f9c7466109a537916099cec31f239186ee7
Mike-E <michael@dragonspark.network>
2025-07-11 12:10:14 -0400

    Catalog/Container Refactor WIP

---
+++ DragonSpark.Presentation/Components/Content/StateViewBase.cs
@@ -0,0 +1,17 @@
+﻿using DragonSpark.Model.Results;
+using DragonSpark.Presentation.Environment.Browser;
+using DragonSpark.Runtime.Activation;
+using Microsoft.AspNetCore.Components;
+
+namespace DragonSpark.Presentation.Components.Content;
+
+public class StateViewBase<T> : Templates.ActiveContentTemplateComponentBase<T>
+{
+	[Parameter]
+	public required IClientVariable<T> Definition { get; set; }
+
+	[Parameter]
+	public IResult<T> New { get; set; } = New<T>.Default;
+}
+
+public readonly record struct SaveStateViewContext<T>(T Subject, EventCallback Save);
