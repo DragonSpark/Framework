e7e4223ed479b88a8aac2c2a252f789671ee9caa
Mike-E <michael@dragonspark.network>
2025-08-07 04:03:12 -0400

    SaleOrder: Key -> UserInput

--- DragonSpark.Application/Model/UserInput.cs
+++ DragonSpark.Application/Model/UserInput.cs
@@ -4,10 +4,12 @@ namespace DragonSpark.Application.Model;
 
 public readonly record struct UserInput<T>(ulong User, T Input) : IUserIdentity
 {
+	public static implicit operator uint(UserInput<T> instance) => instance.Get();
 	public uint Get() => (uint)User;
 }
 
 public readonly record struct UserInput(ulong User, Guid Input) : IUserIdentity
 {
+	public static implicit operator uint(UserInput instance) => instance.Get();
 	public uint Get() => (uint)User;
 }
