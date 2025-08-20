eb11cad8c4be8ecc1a3cc0b5561485a6a53e6dc7
Mike-E <michael@dragonspark.network>
2025-08-20 05:57:21 -0400

    Sale automatic transfers

--- DragonSpark.Syncfusion/Queries/DataManagerRequestFormatter.cs
+++ DragonSpark.Syncfusion/Queries/DataManagerRequestFormatter.cs
@@ -1,7 +1,10 @@
-﻿using DragonSpark.Text;
+﻿using DragonSpark.Compose;
+using DragonSpark.Model;
+using DragonSpark.Text;
 using Syncfusion.Blazor;
 using Syncfusion.Blazor.Data;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DragonSpark.SyncfusionRendering.Queries;
@@ -10,20 +13,28 @@ public sealed class DataManagerRequestFormatter : IFormatter<DataManagerRequest>
 {
 	public static DataManagerRequestFormatter Default { get; } = new();
 
-	DataManagerRequestFormatter() : this(WhereFilterFormatter.Default.Get) {}
+	DataManagerRequestFormatter() : this(WhereFilterFormatter.Default.Get, Empty.Enumerable<string>()) {}
 
 	readonly Func<WhereFilter, string> _where;
+	readonly IEnumerable<string>       _empty;
 
-	public DataManagerRequestFormatter(Func<WhereFilter, string> where) => _where = where;
+	public DataManagerRequestFormatter(Func<WhereFilter, string> where, IEnumerable<string> empty)
+	{
+		_where = where;
+		_empty = empty;
+	}
 
 	public string Get(DataManagerRequest parameter)
 	{
-		var sorted = string.Join(',', parameter.Sorted.Select(x => $"{x.Name} {x.Direction}"));
-		var select = string.Join(',', parameter.Select);
-		var where  = string.Join(',', parameter.Where.Select(_where));
+		var sorted = string.Join(',', parameter.Sorted.Account()?.Select(x => $"{x.Name} {x.Direction}") ?? _empty);
+		var select = string.Join(',', parameter.Select.Account() ?? _empty);
+		var where  = string.Join(',', parameter.Where.Account()?.Select(_where) ?? _empty);
 		var search =
 			string.Join(',',
-			            parameter.Search.Select(x => $"{string.Join(',', x.Fields)} {x.Key} {x.Operator} {x.IgnoreCase}"));
+			            parameter.Search.Account()
+			                     ?
+			                     .Select(x => $"{string.Join(',', x.Fields)} {x.Key} {x.Operator} {x.IgnoreCase}")
+			            ?? _empty);
 
 		var result = $"{select}_{where}_{search}_{sorted}_{parameter.Take}_{parameter.Skip}";
 		return result;
