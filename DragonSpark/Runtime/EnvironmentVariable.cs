dafc1ad032271953aeec1f5cb7a6075aaca63e8d
Michael DeMond <michael@dragonspark.network>
2025-10-29 14:14:07 -0400

    Environmental adjustments

--- DragonSpark/Runtime/EnvironmentVariable.cs
+++ DragonSpark/Runtime/EnvironmentVariable.cs
@@ -1,8 +1,11 @@
-﻿using DragonSpark.Model.Results;
+using DragonSpark.Compose;
+using DragonSpark.Model.Results;
 
 namespace DragonSpark.Runtime;
 
 public class EnvironmentVariable : FixedSelectedSingleton<string, string?>
 {
+    public static implicit operator string(EnvironmentVariable @this) => @this.Get().Verify();
+
 	public EnvironmentVariable(string name) : base(EnvironmentSetting.Default, name) {}
 }
