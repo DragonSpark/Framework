fd43d37d61725fe050d9bbb00e360ff4cba4cd99
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2025-11-14 11:10:26 -0500

    Fix Rename (#22)

--- DragonSpark.Application.AspNet/Security/Identity/AccessToken.cs
+++ DragonSpark.Application.AspNet/Security/Identity/AccessToken.cs
@@ -3,15 +3,15 @@ using Microsoft.AspNetCore.Authentication;
 
 namespace DragonSpark.Application.AspNet.Security.Identity;
 
-public sealed class GetClientDetails : IFormatter<AuthenticationProperties>
+public sealed class AccessToken : IFormatter<AuthenticationProperties>
 {
-	public static GetClientDetails Default { get; } = new();
+	public static AccessToken Default { get; } = new();
 
-	GetClientDetails() : this(".Token.access_token") {}
+	AccessToken() : this(".Token.access_token") {}
 
 	readonly string _value;
 
-	public GetClientDetails(string value) => _value = value;
+	public AccessToken(string value) => _value = value;
 
 	public string Get(AuthenticationProperties parameter) => parameter.GetString(_value) ?? string.Empty;
 }
