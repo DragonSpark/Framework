3105100c1a8233988396e7fe0e1d9420d4b8f870
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2025-11-30 06:11:38 -0500

    Claim Adjustments (#25)
    * Additional promote process fixes
    
    * IPromoteProfile

--- DragonSpark.Application.AspNet/Security/Identity/Model/AddClaim.cs
+++ DragonSpark.Application.AspNet/Security/Identity/Model/AddClaim.cs
@@ -1,4 +1,3 @@
-using DragonSpark.Application.AspNet.Entities.Editing;
 using DragonSpark.Compose;
 using DragonSpark.Model.Operations;
 using DragonSpark.Model.Operations.Selection.Stop;
@@ -11,27 +10,24 @@ namespace DragonSpark.Application.AspNet.Security.Identity.Model;
 
 public class AddClaim<T> : IStopAware<T, IdentityResult> where T : IdentityUser
 {
-	readonly IUsers<T>       _users;
-	readonly EditExisting<T> _edit;
-	readonly Func<T, Claim>  _claim;
+	readonly IUsers<T>      _users;
+	readonly Func<T, Claim> _claim;
 
-	protected AddClaim(IUsers<T> users, EditExisting<T> edit, string type)
-		: this(users, edit, new Claim(type, string.Empty).Accept) {}
+	protected AddClaim(IUsers<T> users, string type) : this(users, new Claim(type, string.Empty).Accept) {}
 
-	protected AddClaim(IUsers<T> users, EditExisting<T> edit, Func<T, Claim> claim)
+	protected AddClaim(IUsers<T> users, Func<T, Claim> claim)
 	{
 		_users = users;
-		_edit  = edit;
 		_claim = claim;
 	}
 
 	public async ValueTask<IdentityResult> Get(Stop<T> parameter)
 	{
-        // ReSharper disable once NotDisposedResource
-        using var edit   = await _edit.Get(parameter).On();
-        using var users  = _users.Get();
-        var       claim  = _claim(parameter);
-		var       result = await users.Subject.AddClaimAsync(parameter, claim).Off();
+		var (subject, _) = parameter;
+		using var users  = _users.Get();
+		var       claim  = _claim(parameter);
+		var       user   = await users.Subject.FindByIdAsync(subject.Id.ToString()).Off();
+		var       result = await users.Subject.AddClaimAsync(user.Verify(), claim).Off();
 		return result;
 	}
 }
