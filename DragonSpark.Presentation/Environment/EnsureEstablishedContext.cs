7513b2a509a389ec4d92315ec805a4f7259d9399
Mike-E <michael@dragonspark.network>
2025-12-14 05:31:28 -0500

    Cancel-aware fixes

---
+++ DragonSpark.Presentation/Environment/EnsureEstablishedContext.cs
@@ -0,0 +1,27 @@
+﻿using DragonSpark.Compose;
+using DragonSpark.Model.Operations;
+using DragonSpark.Model.Operations.Stop;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Presentation.Environment;
+
+public abstract class EnsureEstablishedContext<T> : IStopAware<T>
+{
+	readonly IStopAware<T>        _previous;
+	readonly ContextStore         _store;
+	readonly IHttpContextAccessor _accessor;
+
+	protected EnsureEstablishedContext(IStopAware<T> previous, ContextStore store, IHttpContextAccessor accessor)
+	{
+		_previous = previous;
+		_store    = store;
+		_accessor = accessor;
+	}
+
+	public async ValueTask Get(Stop<T> parameter)
+	{
+		_accessor.HttpContext ??= _store;
+		await _previous.Off(parameter);
+	}
+}
