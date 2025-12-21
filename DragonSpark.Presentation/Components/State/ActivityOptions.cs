41d34a5fc577208c8726f460493ec73b482f0163
Mike-E <michael@dragonspark.network>
2025-12-21 06:05:28 -0500

    TODO

--- DragonSpark.Presentation/Components/State/ActivityOptions.cs
+++ DragonSpark.Presentation/Components/State/ActivityOptions.cs
@@ -1,6 +1,5 @@
 ﻿namespace DragonSpark.Presentation.Components.State;
 
-// TODO: If target is the element, RedrawOnFinish=true.  If target is the ActivityReceiver, RedrawOnFinish=false
 public record ActivityOptions(bool RedrawOnStart = true, bool RedrawOnFinish = true)
 {
 	public static ActivityOptions Default { get; } = new();
