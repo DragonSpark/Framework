eeda0abbf5774d518c946ad137a8ae6618507727
Mike-E <michael@dragonspark.network>
2025-12-21 15:11:10 -0500

    TODOs

---
+++ DragonSpark.Presentation/Components/Content/PlaceholderInput.cs
@@ -0,0 +1,3 @@
+﻿namespace DragonSpark.Presentation.Components.Content;
+
+public readonly record struct PlaceholderInput(ushort Width = 120, ushort Height = 80, string? Text = null);
--- DragonSpark.Presentation/Components/Content/PlaceholderPath.cs
+++ DragonSpark.Presentation/Components/Content/PlaceholderPath.cs
@@ -16,19 +16,3 @@ public sealed class PlaceholderPath : IFormatter<PlaceholderInput>
 		return $"https://placehold.co/{width}x{height}.png{(text.IsNullOrEmpty() ? string.Empty : $"?text={WebUtility.UrlEncode(text)}")}";
 	}
 }
-
-public sealed class PlaceholderAlternatePath : IFormatter<PlaceholderInput>
-{
-	public static PlaceholderAlternatePath Default { get; } = new();
-
-	PlaceholderAlternatePath() {}
-
-	public string Get(PlaceholderInput parameter)
-	{
-		var (width, height, text) = parameter;
-		return
-			$"https://gifpng.com/{width}x{height}/{(text.IsNullOrEmpty() ? string.Empty : $"?text={WebUtility.UrlEncode(text)}")}";
-	}
-}
-// TODO
-public readonly record struct PlaceholderInput(ushort Width = 120, ushort Height = 80, string? Text = null);
