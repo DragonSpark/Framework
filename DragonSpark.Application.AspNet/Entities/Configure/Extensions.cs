91566ff569e442859b42188f93ad83ed841544a0
Mike-E <michael@dragonspark.network>
2025-12-29 05:05:05 -0500

    ConvertNetwork fix

---
+++ DragonSpark.Application.AspNet/Entities/Configure/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using DragonSpark.Compose;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace DragonSpark.Application.AspNet.Entities.Configure;
+
+public static class Extensions
+{
+	public static T Add<T>(this T @this, IDbContextOptionsExtension extension)
+		where T : IRelationalDbContextOptionsBuilderInfrastructure
+	{
+		@this.OptionsBuilder.To<IDbContextOptionsBuilderInfrastructure>().AddOrUpdateExtension(extension);
+		return @this;
+	}
+}
