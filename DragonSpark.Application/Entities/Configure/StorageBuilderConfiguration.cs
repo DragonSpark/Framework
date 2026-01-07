3a3d3a77f0475baa0c2f606e0bf0024e4b5ea852
Mike-E <michael@dragonspark.network>
2026-01-07 05:30:48 -0500

    DragonSpark.Application.AspNet.Entities.Migration WIP

--- DragonSpark.Application/Entities/Configure/StorageBuilderConfiguration.cs
+++ DragonSpark.Application/Entities/Configure/StorageBuilderConfiguration.cs
@@ -1,11 +1,14 @@
-﻿using DragonSpark.Model.Commands;
+﻿using DragonSpark.Compose;
+using DragonSpark.Model.Commands;
 using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace DragonSpark.Application.Entities.Configure;
 
-public class StorageBuilderConfiguration<T> : AppendedCommand<DbContextOptionsBuilder<T>> where T : DbContext
+public class StorageBuilderConfiguration<T> : Commands<DbContextOptionsBuilder<T>> where T : DbContext
 {
-	protected StorageBuilderConfiguration(Type migrations, params object[] services)
-		: base(new UseSqlServer<T>(migrations), new ConfigureApplicationServices(services)) {}
+	protected StorageBuilderConfiguration(Type migrations, Action<DbContextOptionsBuilder<T>> other,
+	                                      params object[] services)
+		: base(new UseSqlServer<T>(migrations), new ConfigureApplicationServices(services),
+		       Start.A.Command(other).Get()) {}
 }
