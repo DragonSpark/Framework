9b04ea2903f9c935508a2f0620c0153507277b3b
Mike-E <michael@dragonspark.network>
2026-01-29 06:27:02 -0500

    ResaleOrder Migrator

--- DragonSpark.Application.AspNet/Entities/Migration/Extensions.cs
+++ DragonSpark.Application.AspNet/Entities/Migration/Extensions.cs
@@ -1,8 +1,13 @@
 ﻿using DragonSpark.Application.AspNet.Entities.Migration.Migrators;
+using DragonSpark.Compose;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DragonSpark.Application.AspNet.Entities.Migration;
 
@@ -28,4 +33,12 @@ public static class Extensions
 
 	public static DbContext Context(this IInfrastructure<IServiceProvider> @this)
 		=> @this.Instance.GetRequiredService<DbContext>();
+
+	public static EntityEntry<T> Of<T>(this EntityEntry @this) where T : class => @this.To<EntityEntry<T>>();
+
+	public static Task Include<TEntity, TProperty>(this EntityEntry<TEntity> entry,
+	                                               Expression<Func<TEntity, TProperty>> path,
+	                                               CancellationToken token = default)
+		where TEntity : class
+		=> LoadMembers.Default.Allocate(new(new(path.Body, entry), token));
 }
---
+++ DragonSpark.Application.AspNet/Entities/Migration/LoadMembers.cs
@@ -0,0 +1,58 @@
+﻿using DragonSpark.Compose;
+using DragonSpark.Model.Operations;
+using DragonSpark.Model.Operations.Stop;
+using DragonSpark.Model.Sequences.Memory;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Application.AspNet.Entities.Migration;
+
+sealed class LoadMembers : IStopAware<LoadMembersInput>
+{
+	public static LoadMembers Default { get; } = new();
+
+	LoadMembers() : this(Members.Default) {}
+
+	readonly ILease<Expression, MemberInfo> _members;
+
+	public LoadMembers(ILease<Expression, MemberInfo> members) => _members = members;
+
+	public async ValueTask Get(Stop<LoadMembersInput> parameter)
+	{
+		var ((expression, entry), stop) = parameter;
+		using var members = _members.Get(expression);
+
+		var current = entry.Entity;
+		var context = entry.Context;
+
+		var span = members.AsMemory();
+
+		for (var i = 0; i < members.Length; i++)
+		{
+			var member       = span.Span[i];
+			var currentEntry = context.Entry(current);
+			var last         = i == span.Length - 1;
+			var navigation   = currentEntry.Navigation(member.Name);
+
+			var collection = navigation.Metadata.IsCollection;
+			if (!navigation.IsLoaded)
+			{
+				NavigationEntry target = last && collection
+					                         ? currentEntry.Collection(member.Name)
+					                         : currentEntry.Reference(member.Name);
+				await target.LoadAsync(stop).Off();
+			}
+
+			if (!collection)
+			{
+				current = navigation.CurrentValue;
+				if (current is null)
+				{
+					break;
+				}
+			}
+		}
+	}
+}
