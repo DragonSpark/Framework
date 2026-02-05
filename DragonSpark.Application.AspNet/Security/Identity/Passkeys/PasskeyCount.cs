ccb3a8de11242a7561c0c9820094d92dd82cdd8a
Michael DeMond <michael@dragonspark.network>
2026-02-05 14:22:31 -0500

    Passkey status fix

--- DragonSpark.Application.AspNet/Security/Identity/Passkeys/PasskeyCount.cs
+++ DragonSpark.Application.AspNet/Security/Identity/Passkeys/PasskeyCount.cs
@@ -5,17 +5,21 @@ using DragonSpark.Model.Operations.Selection.Stop;
 
 namespace DragonSpark.Application.AspNet.Security.Identity.Passkeys;
 
-public class HasPasskeys<T> : IDepending<string> where T : class
+public class PasskeyCount<T> : IStopAware<string, uint?> where T : class
 {
     readonly IUsers<T> _users;
 
-    protected HasPasskeys(IUsers<T> users) => _users = users;
+    protected PasskeyCount(IUsers<T> users) => _users = users;
 
-    public async ValueTask<bool> Get(Stop<string> parameter)
+    public async ValueTask<uint?> Get(Stop<string> parameter)
     {
         using var session = _users.Get();
         var       user    = await session.Subject.FindByEmailAsync(parameter).Off();
-        var       result  = user is not null && await session.Subject.GetPasskeysAsync(user).Off() is { Count: > 0 };
-        return result;
+        if (user is not null)
+        {
+            var list = await session.Subject.GetPasskeysAsync(user).Off();
+            return list.Count.Grade();
+        }
+        return null;
     }
 }
--- DragonSpark.Server.Mobile/Security/Devices/Validation/AttestedDeviceHandler.cs
+++ DragonSpark.Server.Mobile/Security/Devices/Validation/AttestedDeviceHandler.cs
@@ -11,21 +11,29 @@ sealed class AttestedDeviceHandler : AuthorizationHandler<AttestedDeviceRequirem
 {
     readonly IIsAttested  _attested;
     readonly ICurrentStop _stop;
+    readonly string       _claim;
 
     public AttestedDeviceHandler(IIsAttested attested, ICurrentStop stop)
+        : this(attested, stop, DeviceClaimName.Default) {}
+
+    public AttestedDeviceHandler(IIsAttested attested, ICurrentStop stop, string claim)
     {
-        _attested     = attested;
-        _stop         = stop;
+        _attested   = attested;
+        _stop       = stop;
+        _claim = claim;
     }
 
     protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context,
                                                          AttestedDeviceRequirement requirement)
     {
-        var device = context.User.Verify().FindFirstValue(DeviceClaimName.Default);
-        var valid    = device is not null && await _attested.Off(new(device, _stop.Get()));
-        if (valid)
+        var device = context.User.Verify().FindFirstValue(_claim);
+        if (device is not null && await _attested.Off(new(device, _stop.Get())))
         {
             context.Succeed(requirement);
+        }
+        else
+        {
+            context.Fail(new(this, "The device is not hardware-validated"));
         }
     }
 }
