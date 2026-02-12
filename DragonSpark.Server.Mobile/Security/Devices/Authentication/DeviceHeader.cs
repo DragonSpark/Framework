8c46ffd43b8339dcdf37bea95d76ffb66f1a707e
Michael DeMond <michael@dragonspark.network>
2026-02-12 13:24:02 -0500

    Configuration adjustments

--- DragonSpark.Server.Mobile/Security/Devices/Authentication/DeviceHeader.cs
+++
@@ -1,8 +0,0 @@
-namespace DragonSpark.Server.Mobile.Security.Devices.Authentication;
-
-/*sealed class DeviceHeader : Header
-{
-    public static DeviceHeader Default { get; } = new();
-
-    DeviceHeader() : base(SchemeName.Default) {}
-}*/
