c6dbd0ac6510d61fe4109c5c2d835724a5ae34c7
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2026-02-14 05:13:05 -0500

    Service bus telemetry reducer (#47)

---
+++ DragonSpark.Azure/Messaging/Messages/ServiceBusTelemetryReducer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace DragonSpark.Azure.Messaging.Messages;
+
+// Source - https://stackoverflow.com/a/76180024
+// Posted by Peter Bons
+// Retrieved 2026-02-14, License - CC BY-SA 4.0
+public sealed class ServiceBusTelemetryReducer : ITelemetryProcessor
+{
+	private readonly ITelemetryProcessor _next;
+
+	public ServiceBusTelemetryReducer(ITelemetryProcessor next) => _next = next;
+
+	public void Process(ITelemetry item)
+	{
+		var process = item is not DependencyTelemetry { Type: "Azure Service Bus", Name: "ServiceBusReceiver.Receive" };
+
+		if (process)
+		{
+			_next.Process(item);
+		}
+	}
+}
