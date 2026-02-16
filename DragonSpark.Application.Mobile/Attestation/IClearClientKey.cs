4a9066b92c11936a837dfcc20015ba6a82aa5648
Michael DeMond <michael@dragonspark.network>
2026-02-16 14:56:44 -0500

    Clear extra values on error

--- DragonSpark.Application.Mobile/Attestation/IClearClientKey.cs
+++ DragonSpark.Application.Mobile/Attestation/IClearClientKey.cs
@@ -1,5 +1,14 @@
-using DragonSpark.Model.Operations.Selection.Stop;
+using DragonSpark.Model;
+using DragonSpark.Model.Operations;
+using DragonSpark.Model.Operations.Selection.Conditions;
 
 namespace DragonSpark.Application.Mobile.Attestation;
 
-public interface IClearClientKey : IDepending;
+public interface IClearClientKey : DragonSpark.Model.Operations.Selection.Stop.IDepending;
+
+// TODO
+
+public sealed class Clear : DependingOnAll<Stop<None>>
+{
+    public Clear(IClearClientKey first, IClearValidationIdentity second) : base(first, second) {}
+}
