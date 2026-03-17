158fba3eb00f7e705e213e5f77b758574644e1c8
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2026-03-17 11:10:11 -0400

    Grok API iOS Fixes (#54)

--- DragonSpark.Application.Mobile.Maui/Presentation/Markup/UriMarkupExtension.cs
+++ DragonSpark.Application.Mobile.Maui/Presentation/Markup/UriMarkupExtension.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Linq;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Devices;
 
 namespace DragonSpark.Application.Mobile.Maui.Presentation.Markup;
 
@@ -7,4 +10,47 @@ public sealed class UriMarkupExtension : MarkupExtension<Uri>
     public required string Address { get; set; }
 
     public override Uri ProvideValue(IServiceProvider serviceProvider) => new(Address);
+}
+
+public enum MauiPlatform : byte
+{
+    iOS,
+    Android,
+    WinUI,
+    MacCatalyst,
+    Tizen,
+    Unknown
+}
+
+public sealed class IsPlatformExtension : IMarkupExtension<bool>
+{
+    public static IsPlatformExtension iOS { get; } = new() { Platform = MauiPlatform.iOS };
+
+    public MauiPlatform Platform { get; set; } = MauiPlatform.Unknown;
+
+    public string Platforms { get; set; } = string.Empty; // comma-separated
+
+    public bool ProvideValue(IServiceProvider serviceProvider)
+    {
+        var current = DeviceInfo.Platform.ToString() switch
+        {
+            nameof(DevicePlatform.iOS) => MauiPlatform.iOS,
+            nameof(DevicePlatform.Android) => MauiPlatform.Android,
+            nameof(DevicePlatform.WinUI) => MauiPlatform.WinUI,
+            nameof(DevicePlatform.MacCatalyst) => MauiPlatform.MacCatalyst,
+            nameof(DevicePlatform.Tizen) => MauiPlatform.Tizen,
+            _ => MauiPlatform.Unknown
+        };
+
+        return !string.IsNullOrWhiteSpace(Platforms)
+                   ? Platforms.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                              .Select(p => Enum.TryParse<MauiPlatform>(p, ignoreCase: true, out var ep)
+                                               ? ep
+                                               : MauiPlatform.Unknown)
+                              .Where(p => p != MauiPlatform.Unknown)
+                              .Contains(current)
+                   : current == Platform;
+    }
+
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider) => ProvideValue(serviceProvider);
 }
