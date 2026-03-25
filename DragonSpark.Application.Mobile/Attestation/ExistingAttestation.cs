59adb659358647eb4bc947c22190bc5047184a80
Michael DeMond <michael@dragonspark.network>
2026-03-25 13:42:04 -0400

    Clear tokens before start

--- DragonSpark.Application.Mobile/Attestation/ExistingAttestation.cs
+++ DragonSpark.Application.Mobile/Attestation/ExistingAttestation.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using DragonSpark.Application.Security.Tokens;
 using DragonSpark.Compose;
 
 namespace DragonSpark.Application.Mobile.Attestation;
@@ -9,12 +10,15 @@ sealed class ExistingAttestation : IExistingAttestation
     readonly IValidationIdentity _identity;
     readonly IChallenge          _challenge;
     readonly IAssertionToken     _assertion;
+    readonly ITokens             _tokens;
 
-    public ExistingAttestation(IValidationIdentity identity, IChallenge challenge, IAssertionToken assertion)
+    public ExistingAttestation(IValidationIdentity identity, IChallenge challenge, IAssertionToken assertion,
+                               ITokens tokens)
     {
         _identity  = identity;
         _challenge = challenge;
         _assertion = assertion;
+        _tokens    = tokens;
     }
 
     public async ValueTask<ExistingAttestationResult?> Get(CancellationToken parameter)
@@ -24,6 +28,7 @@ sealed class ExistingAttestation : IExistingAttestation
         if (identity is not null)
         {
             var (subject, hash) = identity;
+            _tokens.Execute();
             var challenge = await _challenge.Off(parameter);
             var assertion = await _assertion.Off(new(challenge.Challenge, parameter));
             return new(subject, assertion, hash, challenge);
