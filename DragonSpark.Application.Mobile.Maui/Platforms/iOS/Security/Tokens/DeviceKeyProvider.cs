0d877af26d71d243cbd55127d8a6b518fea4a984
Michael DeMond <michael@dragonspark.network>
2026-03-26 13:22:25 -0400

    Potential fix

--- DragonSpark.Application.Mobile.Maui/Platforms/iOS/Security/Tokens/DeviceKeyProvider.cs
+++ DragonSpark.Application.Mobile.Maui/Platforms/iOS/Security/Tokens/DeviceKeyProvider.cs
@@ -1,13 +1,10 @@
-using System.Threading;
-using System.Threading.Tasks;
 using DragonSpark.Application.Security.Tokens;
 using DragonSpark.Compose;
-using DragonSpark.Model.Operations.Results;
 using DragonSpark.Model.Results;
 
 namespace DragonSpark.Application.Mobile.Maui.Platforms.iOS.Security.Tokens;
 
-sealed class DeviceKeyProvider : IDeviceKeyProvider
+/*sealed class DeviceKeyProvider : IDeviceKeyProvider
 {
     public static DeviceKeyProvider Default { get; } = new();
 
@@ -20,4 +17,18 @@ sealed class DeviceKeyProvider : IDeviceKeyProvider
     public DeviceKeyProvider(IResulting<PublicJWK> previous) => _previous = previous;
 
     public ValueTask<PublicJWK> Get(CancellationToken ct) => _previous.Get();
+}*/
+sealed class DeviceKeyProvider : DragonSpark.Model.Operations.Results.Stop.Storing<PublicJWK>, IDeviceKeyProvider
+{
+    public static DeviceKeyProvider Default { get; } = new();
+
+    DeviceKeyProvider()
+        : base(DeviceKeyProcessStore.Default, CreateDeviceKey.Default.Then().Operation().Out().AsStop()) {}
+}
+// TODO
+sealed class DeviceKeyProcessStore : Variable<PublicJWK>
+{
+    public static DeviceKeyProcessStore Default { get; } = new();
+
+    DeviceKeyProcessStore() {}
 }
--- DragonSpark.Application.Mobile.Maui/Platforms/iOS/Security/Tokens/StateAwareClearDeviceKey.cs
+++ DragonSpark.Application.Mobile.Maui/Platforms/iOS/Security/Tokens/StateAwareClearDeviceKey.cs
@@ -1,24 +1,28 @@
 using System.Threading.Tasks;
+using DragonSpark.Application.Security.Tokens;
 using DragonSpark.Compose;
 using DragonSpark.Model;
 using DragonSpark.Model.Operations;
+using DragonSpark.Model.Results;
 
 namespace DragonSpark.Application.Mobile.Maui.Platforms.iOS.Security.Tokens;
 
 sealed class StateAwareClearDeviceKey : IClearDeviceKey
 {
-    readonly IClearDeviceKey  _previous;
-    readonly ClearDeviceState _clear;
+    readonly IClearDeviceKey      _previous;
+    readonly IMutable<PublicJWK?> _process;
 
-    public StateAwareClearDeviceKey(IClearDeviceKey previous, ClearDeviceState clear)
+    public StateAwareClearDeviceKey(IClearDeviceKey previous) : this(previous, DeviceKeyProcessStore.Default) {}
+
+    public StateAwareClearDeviceKey(IClearDeviceKey previous, IMutable<PublicJWK?> process)
     {
         _previous = previous;
-        _clear    = clear;
+        _process  = process;
     }
 
     public async ValueTask<bool> Get(Stop<None> parameter)
     {
-        await _clear.Off(parameter);
+        _process.Execute(null);
         return await _previous.Off(parameter);
     }
 }
