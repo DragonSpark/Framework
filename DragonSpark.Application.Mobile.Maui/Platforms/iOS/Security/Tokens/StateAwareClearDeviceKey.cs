0a885b93941bce48e8eefa3bade89b6e87c763cd
Michael DeMond <michael@dragonspark.network>
2026-03-27 14:23:25 -0400

    Clear access token on clear

--- DragonSpark.Application.Mobile.Maui/Platforms/iOS/Security/Tokens/StateAwareClearDeviceKey.cs
+++ DragonSpark.Application.Mobile.Maui/Platforms/iOS/Security/Tokens/StateAwareClearDeviceKey.cs
@@ -1,8 +1,10 @@
 using System.Threading.Tasks;
+using DragonSpark.Application.Mobile.Maui.Security.Identity;
 using DragonSpark.Application.Security.Tokens;
 using DragonSpark.Compose;
 using DragonSpark.Model;
 using DragonSpark.Model.Operations;
+using DragonSpark.Model.Operations.Selection.Stop;
 using DragonSpark.Model.Results;
 
 namespace DragonSpark.Application.Mobile.Maui.Platforms.iOS.Security.Tokens;
@@ -11,18 +13,22 @@ sealed class StateAwareClearDeviceKey : IClearDeviceKey
 {
     readonly IClearDeviceKey      _previous;
     readonly IMutable<PublicJWK?> _process;
+    readonly IDepending           _token;
 
-    public StateAwareClearDeviceKey(IClearDeviceKey previous) : this(previous, DeviceKeyProcessStore.Default) {}
+    public StateAwareClearDeviceKey(IClearDeviceKey previous)
+        : this(previous, DeviceKeyProcessStore.Default, ClearTokenState.Default) {}
 
-    public StateAwareClearDeviceKey(IClearDeviceKey previous, IMutable<PublicJWK?> process)
+    public StateAwareClearDeviceKey(IClearDeviceKey previous, IMutable<PublicJWK?> process, IDepending token)
     {
-        _previous = previous;
-        _process  = process;
+        _previous   = previous;
+        _process    = process;
+        _token = token;
     }
 
     public async ValueTask<bool> Get(Stop<None> parameter)
     {
         _process.Execute(null);
+        await _token.Off(parameter);
         return await _previous.Off(parameter);
     }
 }
