a9bf898f2e45fdbd8963eb17a85e48487d7c703b
Michael DeMond <michael@dragonspark.network>
2026-05-15 15:22:45 -0400

    Basic invite code functionality

--- DragonSpark.Application.AspNet/Security/Identity/Model/RemoveClaim.cs
+++ DragonSpark.Application.AspNet/Security/Identity/Model/RemoveClaim.cs
@@ -35,3 +35,31 @@ public class AddClaim<T> : IStopAware<T, IdentityResult> where T : IdentityUser
         return result;
     }
 }
+
+public class RemoveClaim<T> : IStopAware<T, IdentityResult> where T : IdentityUser
+{
+    readonly IAuthentications<T> _sessions;
+    readonly Func<T, Claim>      _claim;
+
+    public RemoveClaim(IAuthentications<T> sessions, string type)
+        : this(sessions, new Claim(type, string.Empty).Accept) {}
+
+    protected RemoveClaim(IAuthentications<T> sessions, Func<T, Claim> claim)
+    {
+        _sessions = sessions;
+        _claim    = claim;
+    }
+
+    public async ValueTask<IdentityResult> Get(Stop<T> parameter)
+    {
+        var (subject, _) = parameter;
+        using var session = _sessions.Get();
+        var       claim   = _claim(parameter);
+        var       users   = session.Subject.UserManager;
+        var       user    = await users.FindByIdAsync(subject.Id.ToString()).Off();
+        var       verify  = user.Verify();
+        var       result  = await users.RemoveClaimAsync(verify, claim).Off();
+        await session.Users.UpdateSecurityStampAsync(verify).Off();
+        return result;
+    }
+}
