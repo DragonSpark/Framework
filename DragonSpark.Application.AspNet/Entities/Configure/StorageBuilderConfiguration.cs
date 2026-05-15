c7e5977e4535c4c85bbc185301e2a25500530d07
Michael DeMond <michael@dragonspark.network>
2026-05-15 06:52:29 -0400

    Provide empty action constructor for storage configuration

--- DragonSpark.Application.AspNet/Entities/Configure/StorageBuilderConfiguration.cs
+++ DragonSpark.Application.AspNet/Entities/Configure/StorageBuilderConfiguration.cs
@@ -1,14 +1,17 @@
-﻿using DragonSpark.Compose;
+using System;
+using DragonSpark.Compose;
 using DragonSpark.Model.Commands;
 using Microsoft.EntityFrameworkCore;
-using System;
 
 namespace DragonSpark.Application.AspNet.Entities.Configure;
 
 public class StorageBuilderConfiguration<T> : Commands<DbContextOptionsBuilder<T>> where T : DbContext
 {
+	protected StorageBuilderConfiguration(Type migrations, params object[] services)
+		: this(migrations, _ => {}, services) {}
+
 	protected StorageBuilderConfiguration(Type migrations, Action<DbContextOptionsBuilder<T>> other,
-	                                      params object[] services)
+										  params object[] services)
 		: base(new UseSqlServer<T>(migrations), new ConfigureApplicationServices(services),
-		       Start.A.Command(other).Get()) {}
+			   Start.A.Command(other).Get()) {}
 }
