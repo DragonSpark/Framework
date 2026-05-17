1b965bc7b5ae09b593655860b56da48ac0f5f8c9
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2026-05-17 07:46:48 -0400

    Full text searching fixes (#85)

---
+++ DragonSpark.Application.AspNet/Entities/Search/DefaultCatalogName.cs
@@ -0,0 +1,8 @@
+﻿namespace DragonSpark.Application.AspNet.Entities.Search;
+
+public sealed class DefaultCatalogName : Text.Text
+{
+	public static DefaultCatalogName Default { get; } = new();
+
+	DefaultCatalogName() : base("DefaultFullTextCatalog") {}
+}
--- DragonSpark.Application.AspNet/Entities/Search/InstallFullText.cs
+++ DragonSpark.Application.AspNet/Entities/Search/InstallFullText.cs
@@ -11,7 +11,11 @@ sealed class InstallFullText : IFormatter<InstallFullTextInput>
 {
 	public static InstallFullText Default { get; } = new();
 
-	InstallFullText() {}
+	InstallFullText() : this(DefaultCatalogName.Default) {}
+
+	readonly string _name;
+
+	public InstallFullText(string name) => _name = name;
 
 	public string Get(InstallFullTextInput parameter)
 	{
@@ -21,7 +25,7 @@ sealed class InstallFullText : IFormatter<InstallFullTextInput>
 		var key        = type.FindPrimaryKey()?.GetName(identifier);
 		var names      = new List<string>();
 		foreach (var property in type.ClrType.GetProperties()
-		                             .Where(p => ExtensionMethods.Attribute<FullTextAttribute>(p) is not null))
+		                             .Where(p => p.Attribute<FullTextAttribute>() is not null))
 		{
 			var columnName = type.FindProperty(property.Name)?.GetColumnName(identifier);
 			if (columnName is not null)
@@ -34,7 +38,7 @@ sealed class InstallFullText : IFormatter<InstallFullTextInput>
 			       ? $"""
 			          IF NOT EXISTS (SELECT 1 FROM sys.fulltext_indexes fi JOIN sys.objects o ON fi.object_id = o.object_id WHERE o.name = '{table}')
 			          BEGIN
-			            CREATE FULLTEXT INDEX ON [{schema}].[{table}] ({string.Join(", ", names)}) KEY INDEX [{key}];
+			            CREATE FULLTEXT INDEX ON [{schema}].[{table}] ({string.Join(", ", names)}) KEY INDEX [{key}] ON {_name};
 			          END
 			          """
 			       : string.Empty;
