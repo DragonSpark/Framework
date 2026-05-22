16e4e30f04c65dace9e5a5a4922d91c9c31bb0e9
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2026-05-22 06:23:11 -0400

    More robust ignore exception (#91)

---
+++ DragonSpark.Application.AspNet/Diagnostics/AggregateAwareIgnoreException.cs
@@ -0,0 +1,21 @@
+﻿using DragonSpark.Model.Selection.Conditions;
+using System;
+using System.Linq;
+
+namespace DragonSpark.Application.AspNet.Diagnostics;
+
+public sealed class AggregateAwareIgnoreException : ICondition<Exception>
+{
+	public static AggregateAwareIgnoreException Default { get; } = new();
+
+	AggregateAwareIgnoreException() : this(InnerExceptionAwareIgnoreException.Default.Get) {}
+
+	readonly Func<Exception, bool> _previous;
+
+	public AggregateAwareIgnoreException(Func<Exception, bool> previous) => _previous = previous;
+
+	public bool Get(Exception parameter)
+		=> parameter is AggregateException aggregate
+			   ? aggregate.Flatten().InnerExceptions.Any(_previous)
+			   : _previous(parameter);
+}
