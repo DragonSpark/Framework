885b8bcac6a7e4b8822140b396adfc3d98f7239b
Mike-E <michael@dragonspark.network>
2026-05-28 05:39:42 -0400

    Workers

--- DragonSpark.Application.Hosting.Server/Run/Program.cs
+++ DragonSpark.Application.Hosting.Server/Run/Program.cs
@@ -1,6 +1,7 @@
 using DragonSpark.Application.AspNet.Run;
 using DragonSpark.Compose;
 using DragonSpark.Model.Commands;
+using DragonSpark.Model.Operations.Allocated;
 using Microsoft.Extensions.Hosting;
 using System;
 
@@ -9,12 +10,18 @@ namespace DragonSpark.Application.Hosting.Server.Run;
 public abstract class Program(
 	Func<IHostBuilder, IHostBuilder> select,
 	ICommand<IHostedApplicationBuilder> builder,
-	ICommand<IApplication> application)
+	ICommand<IApplication> application, IAllocated<IHost> run)
 	: AspNet.Run.Program(Start.A.Selection<string[]>()
 	                          .By.Calling(InitializeBuilder.Default.Get)
 	                          .Select(new SelectBuilder(select, builder)),
-	                     new ConfigureNewApplication(application))
+	                     new ConfigureNewApplication(application), 
+	                     run)
 {
 	protected Program(ICommand<IHostedApplicationBuilder> builder, ICommand<IApplication> application)
 		: this(x => x, builder, application) {}
+
+	public Program(Func<IHostBuilder, IHostBuilder> select,
+	               ICommand<IHostedApplicationBuilder> builder,
+	               ICommand<IApplication> application)
+		: this(select, builder, application, RunInitializedProgram.Default) {}
 }
