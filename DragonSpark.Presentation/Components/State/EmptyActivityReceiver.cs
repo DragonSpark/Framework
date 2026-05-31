744ea9c2e549d4802f7d91e8ea6b90ef8b85d932
Mike-E <michael@dragonspark.network>
2026-05-31 15:09:43 -0400

    General adjustments
    (cherry picked from commit c7885e7d340d78c25207255d5ad4baf14a50f1bb)

---
+++ DragonSpark.Presentation/Components/State/EmptyActivityReceiver.cs
@@ -0,0 +1,29 @@
+﻿using DragonSpark.Model.Commands;
+using System.Threading.Tasks;
+
+namespace DragonSpark.Presentation.Components.State;
+
+public sealed class EmptyActivityReceiver : IActivityReceiver
+{
+	public static EmptyActivityReceiver Default { get; } = new();
+
+	EmptyActivityReceiver() : this(EmptyCommand<IRenderAware>.Default) {}
+
+	public EmptyActivityReceiver(ICommand<IRenderAware> command) : this(command, command) {}
+
+	public EmptyActivityReceiver(ICommand<IRenderAware> add, ICommand<IRenderAware> remove)
+	{
+		Add    = add;
+		Remove = remove;
+	}
+
+	public ICommand<IRenderAware> Add { get; }
+
+	public ICommand<IRenderAware> Remove { get; }
+
+	public bool Active => true;
+
+	public ValueTask Get(ActivityReceiverState parameter) => ValueTask.CompletedTask;
+
+	public ValueTask<ActivityReceiverState?> Get() => ValueTask.FromResult(default(ActivityReceiverState?));
+}
