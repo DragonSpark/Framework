e1dd54c6554e435dac99b860b98f416440dfea81
Mike-E <michael@dragonspark.network>
2026-06-01 04:59:08 -0400

    ScrollMonitor component

---
+++ DragonSpark.Presentation/Environment/Browser/ComposeModulePath.cs
@@ -0,0 +1,23 @@
+﻿using DragonSpark.Compose;
+using DragonSpark.Text;
+using System;
+
+namespace DragonSpark.Presentation.Environment.Browser;
+
+sealed class ComposeModulePath : IFormatter<Type>
+{
+	public static ComposeModulePath Default { get; } = new();
+
+	ComposeModulePath() {}
+	
+	public string Get(Type parameter)
+	{
+		var assembly     = parameter.Assembly.GetName().Name.Verify();
+		var @namespace   = parameter.Namespace ?? string.Empty;
+		var internalPath = @namespace.StartsWith(assembly) ? @namespace[assembly.Length..].TrimStart('.') : @namespace;
+		var directory    = internalPath.Replace('.', '/');
+		var name         = directory.IsAssigned() ? $"{directory}/{parameter.Name}" : parameter.Name;
+		var result       = $"_content/{assembly}/{name}.razor.js";
+		return result;
+	}
+}
