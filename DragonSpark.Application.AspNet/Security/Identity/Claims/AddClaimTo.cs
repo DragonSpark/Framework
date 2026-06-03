764f8e1a754b6afe3aedb4cea13d25da89831b02
Mike-E <michael@dragonspark.network>
2026-06-03 03:48:40 -0400

    Only refresh application users

--- DragonSpark.Application.AspNet/Security/Identity/Claims/AddClaimTo.cs
+++ DragonSpark.Application.AspNet/Security/Identity/Claims/AddClaimTo.cs
@@ -1,7 +1,9 @@
 using DragonSpark.Application.AspNet.Security.Identity.Authentication;
+using DragonSpark.Application.Security.Identity;
 using DragonSpark.Compose;
 using DragonSpark.Model.Operations;
 using DragonSpark.Model.Operations.Selection.Stop;
+using DragonSpark.Model.Selection.Conditions;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Security.Claims;
@@ -11,15 +13,18 @@ namespace DragonSpark.Application.AspNet.Security.Identity.Claims;
 
 public class AddClaimTo<T> : IStopAware<T, IdentityResult> where T : IdentityUser
 {
-	readonly IAuthentications<T> _sessions;
-	readonly Func<T, Claim>      _claim;
+	readonly IAuthentications<T>         _sessions;
+	readonly Func<T, Claim>              _claim;
+	readonly ICondition<ClaimsPrincipal> _application;
 
-	public AddClaimTo(IAuthentications<T> sessions, string type) : this(sessions, new Claim(type, string.Empty).Accept) {}
+	public AddClaimTo(IAuthentications<T> sessions, string type)
+		: this(sessions, new Claim(type, string.Empty).Accept, IsApplicationPrincipal.Default) {}
 
-	protected AddClaimTo(IAuthentications<T> sessions, Func<T, Claim> claim)
+	protected AddClaimTo(IAuthentications<T> sessions, Func<T, Claim> claim, ICondition<ClaimsPrincipal> application)
 	{
-		_sessions = sessions;
-		_claim    = claim;
+		_sessions    = sessions;
+		_claim       = claim;
+		_application = application;
 	}
 
 	public async ValueTask<IdentityResult> Get(Stop<T> parameter)
@@ -35,7 +40,11 @@ public class AddClaimTo<T> : IStopAware<T, IdentityResult> where T : IdentityUse
 		if (remove.Succeeded)
 		{
 			var result = await users.AddClaimAsync(verify, claim).Off();
-			await session.Subject.RefreshSignInAsync(verify).Off(); // TODO: Sales profile
+			if (_application.Get(session.Subject.Context.User))
+			{
+				await session.Subject.RefreshSignInAsync(verify).Off();
+			}
+
 			return result;
 		}
 
