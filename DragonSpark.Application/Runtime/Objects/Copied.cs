4726e5098642a61150abcde9acb117ed90ae6e81
Mike-E <michael@dragonspark.network>
2026-06-06 10:22:48 -0400

    Class-per-file

---
+++ DragonSpark.Application/Runtime/Objects/Copied.cs
@@ -0,0 +1,24 @@
+using DragonSpark.Model.Results;
+using DragonSpark.Model.Selection.Alterations;
+
+namespace DragonSpark.Application.Runtime.Objects;
+
+public sealed class Copied<T> : Result<T?>, IMutable<T?> where T : notnull
+{
+	readonly IMutable<T?>   _previous;
+	readonly IAlteration<T> _copy;
+
+	public Copied() : this(new Variable<T>(), DefaultCopy<T>.Default) {}
+
+	public Copied(IMutable<T?> previous, IAlteration<T> copy) : base(previous)
+	{
+		_previous = previous;
+		_copy     = copy;
+	}
+
+	public void Execute(T? parameter)
+	{
+		var instance = parameter is not null ? _copy.Get(parameter) : default;
+		_previous.Execute(instance);
+	}
+}
