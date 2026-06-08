ff507b5b2e92dbdff748d54cbdab03669c3e8065
Mike-E <michael@dragonspark.network>
2026-06-08 03:47:51 -0400

    Clear user inventory on purchase

--- DragonSpark.Server/Output/ClearUserOutputs.cs
+++ DragonSpark.Server/Output/ClearUserOutputs.cs
@@ -4,15 +4,24 @@ using DragonSpark.Model.Operations;
 using DragonSpark.Model.Operations.Selection.Stop;
 using DragonSpark.Model.Operations.Stop;
 using Microsoft.AspNetCore.OutputCaching;
+using System;
 using System.Threading.Tasks;
 
 namespace DragonSpark.Server.Output;
 
-public sealed class ClearUserOutputs<T> : IStopAware<T>
+public class ClearUserOutputs : ClearUserOutputs<uint>
+{
+	public ClearUserOutputs(IOutputCacheStore output, IUserOutputKey key) : base(output, key, x => x) {}
+}
+
+public class ClearUserOutputs<T> : IStopAware<T>
 {
 	readonly IStopAware<T, uint> _user;
 	readonly IOutputCacheStore   _output;
 	readonly IUserOutputKey      _key;
+
+	public ClearUserOutputs(IOutputCacheStore output, IUserOutputKey key, Func<T, uint> user)
+		: this(user.Start().Operation().Out().AsStop(), output, key) {}
 
 	public ClearUserOutputs(IStopAware<T, uint> user, IOutputCacheStore output, IUserOutputKey key)
 	{
