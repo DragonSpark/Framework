a8eb41dc29fa20bca6635e625104c36997bfda8b
Michael DeMond <michael@dragonspark.network>
2026-06-18 10:47:49 -0400

    Improve Metadata validation surface

--- DragonSpark.Application/Components/Validation/Expressions/MetadataValueValidator.cs
+++ DragonSpark.Application/Components/Validation/Expressions/MetadataValueValidator.cs
@@ -1,9 +1,14 @@
-﻿using DragonSpark.Model.Selection.Conditions;
 using System.ComponentModel.DataAnnotations;
+using DragonSpark.Model.Selection.Conditions;
 
 namespace DragonSpark.Application.Components.Validation.Expressions;
 
-public class MetadataValueValidator : Condition<object?>, IValidateValue<object>
+public class MetadataValueValidator : MetadataValueValidator<object>
 {
-	public MetadataValueValidator(ValidationAttribute metadata) : base(metadata.IsValid) {}
+    public MetadataValueValidator(ValidationAttribute metadata) : base(metadata) {}
+}
+
+public class MetadataValueValidator<T> : Condition<T?>, IValidateValue<T>
+{
+	public MetadataValueValidator(ValidationAttribute metadata) : base(x => metadata.IsValid(x)) {}
 }
--- DragonSpark.Application/Components/Validation/Expressions/RegularExpressionValidator.cs
+++ DragonSpark.Application/Components/Validation/Expressions/RegularExpressionValidator.cs
@@ -1,10 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace DragonSpark.Application.Components.Validation.Expressions;
 
-public class RegularExpressionValidator : MetadataValueValidator
+public class RegularExpressionValidator : MetadataValueValidator<object>
 {
 	public RegularExpressionValidator(string expression) : this(new RegularExpressionAttribute(expression)) {}
 
 	public RegularExpressionValidator(RegularExpressionAttribute metadata) : base(metadata) {}
+}
+
+public class RegularExpressionValidator<T> : MetadataValueValidator<T>
+{
+    public RegularExpressionValidator(string expression) : this(new RegularExpressionAttribute(expression)) {}
+
+    public RegularExpressionValidator(RegularExpressionAttribute metadata) : base(metadata) {}
 }
