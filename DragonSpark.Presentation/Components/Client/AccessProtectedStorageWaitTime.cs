c3c0f7ff875c47432913d2e5138e319bf11782f5
Mike-E <michael@dragonspark.network>
2026-06-21 07:14:58 -0400

    Delete state on corrupted/hung local storage

---
+++ DragonSpark.Presentation/Components/Client/AccessProtectedStorageWaitTime.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DragonSpark.Presentation.Components.Client;
+
+public sealed class AccessProtectedStorageWaitTime : DragonSpark.Model.Results.Instance<TimeSpan>
+{
+	public static AccessProtectedStorageWaitTime Default { get; } = new();
+
+	AccessProtectedStorageWaitTime() : base(TimeSpan.FromSeconds(5)) {}
+}
