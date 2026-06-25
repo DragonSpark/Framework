00c8c2857d479d0a092c52edbc026199c88ddfe5
Mike-E <michael@dragonspark.network>
2026-06-25 02:51:10 -0400

    Improve form activity control

--- DragonSpark.Presentation/Components/Forms/SubmissionMonitor.cs
+++ DragonSpark.Presentation/Components/Forms/SubmissionMonitor.cs
@@ -1,4 +1,5 @@
 ﻿using DragonSpark.Model.Operations.Allocated;
+using DragonSpark.Model.Results;
 using DragonSpark.Model.Sequences.Collections;
 using DragonSpark.Presentation.Components.Forms.Validation;
 using Microsoft.AspNetCore.Components;
@@ -8,15 +9,27 @@ using System.Threading.Tasks;
 
 namespace DragonSpark.Presentation.Components.Forms;
 
-public sealed class SubmissionMonitor : Membership<EventCallback<SubmittingInput>>, IAllocated<EditContext>
+public sealed class SubmissionMonitor : Membership<EventCallback<SubmittingInput>>, IAllocated<EditContext>, ISwitch
 {
 	readonly EventCallback<EditContext> _submit;
+	readonly ISwitch?                   _allowed;
 
-	public SubmissionMonitor(EventCallback<EditContext> submit) : this(submit, []) {}
+	public SubmissionMonitor(EventCallback<EditContext> submit, ISwitch? allowed) : this(submit, allowed, []) {}
 
-	public SubmissionMonitor(EventCallback<EditContext> submit, HashSet<EventCallback<SubmittingInput>> collection)
+	public SubmissionMonitor(EventCallback<EditContext> submit, ISwitch? allowed,
+	                         HashSet<EventCallback<SubmittingInput>> collection)
 		: base(collection)
-		=> _submit = submit;
+	{
+		_submit  = submit;
+		_allowed = allowed;
+	}
 
 	public Task Get(EditContext parameter) => _submit.Invoke(parameter);
+
+	public bool Get() => _allowed?.Get() ?? true;
+
+	public void Execute(bool parameter)
+	{
+		_allowed?.Execute(parameter);
+	}
 }
