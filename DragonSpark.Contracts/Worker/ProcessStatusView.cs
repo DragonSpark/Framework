c5cf0e720a9cb3fd1127a3e9764d8586cad7cfa6
Mike-E <michael@dragonspark.network>
2026-07-17 09:47:46 -0400

    Move ProcessStatusView to Contracts

---
+++ DragonSpark.Contracts/Worker/ProcessStatusView.cs
@@ -0,0 +1,6 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DragonSpark.Contracts.Worker;
+
+[JsonDerivedType(typeof(ProcessStatusView), 0), JsonDerivedType(typeof(SuccessStatusView), 1)]
+public record ProcessStatusView(ProcessStatus Status, DateTimeOffset Time, string? Message);
