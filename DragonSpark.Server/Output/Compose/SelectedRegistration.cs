9f8f6aee84aa90a660fb79d8551eabc543555f93
Mike-E <michael@dragonspark.network>
2026-07-26 12:07:53 -0400

    Modify Fluent API to allow adding/composite

--- DragonSpark.Server/Output/Compose/SelectedRegistration.cs
+++ DragonSpark.Server/Output/Compose/SelectedRegistration.cs
@@ -8,33 +8,55 @@ namespace DragonSpark.Server.Output.Compose;
 
 public sealed class SelectedRegistration<T, TTo> where T : notnull
 {
-	readonly IOutputCacheStore _store;
-	readonly IOutputKey[]      _keys;
-	readonly Func<T, TTo>      _select;
+	readonly IOutputCacheStore   _store;
+	readonly IOutputKey[]        _keys;
+	readonly Func<T, TTo>        _select;
+	readonly List<IRegistration> _registrations;
 
 	public SelectedRegistration(IOutputCacheStore store, IOutputKey[] keys, Func<T, TTo> select)
+		: this(store, keys, select, []) {}
+
+	// ReSharper disable once TooManyDependencies
+	public SelectedRegistration(IOutputCacheStore store, IOutputKey[] keys, Func<T, TTo> select,
+	                            List<IRegistration> registrations)
 	{
-		_store  = store;
-		_keys   = keys;
-		_select = select;
+		_store         = store;
+		_keys          = keys;
+		_select        = select;
+		_registrations = registrations;
 	}
 
-	public IStopAware<T> Build()
-		=> new Evict<T>(_store, new RegistrationAwareTags(new Registration<T, TTo>(_select)), _keys);
+	public IStopAware<T> Build() => new Evict<T>(_store, new RegistrationAwareTags(_registrations), _keys);
 
-	public IStopAware<T> Many<TOut>(IStopAware<TTo, TOut> select, ISelect<TOut, IEnumerable<TTo>> keys)
+	public SelectedRegistration<T, TTo> Register()
+	{
+		_registrations.Add(new Registration<T, TTo>(_select));
+		return this;
+	}
+
+	public SelectedRegistration<T, TTo> Many<TOut>(IStopAware<TTo, TOut> select, ISelect<TOut, IEnumerable<TTo>> keys)
 		=> Many(select, keys.Get);
 
-	public IStopAware<T> Many<TOut>(IStopAware<TTo, Leasing<TOut>> select, ISelect<TOut, IEnumerable<TTo>> keys)
+	public SelectedRegistration<T, TTo> Many<TOut>(IStopAware<TTo, Leasing<TOut>> select,
+	                                               ISelect<TOut, IEnumerable<TTo>> keys)
 		=> Many(select, keys.Get);
 
-	public IStopAware<T> Many<TOut>(IStopAware<TTo, Leasing<TOut>> select, Func<TOut, IEnumerable<TTo>> keys)
-		=> new Evict<T>(_store, new RegistrationAwareTags(new ManyRegistrations<T, TOut, TTo>(_select, select, keys)),
-		                _keys);
-	public IStopAware<T> Many<TOut>(IStopAware<TTo, TOut> select, Func<TOut, IEnumerable<TTo>> keys)
-		=> new Evict<T>(_store, new RegistrationAwareTags(new Registrations<T, TOut, TTo>(_select, select, keys)),
-		                _keys);
+	public SelectedRegistration<T, TTo> Many<TOut>(IStopAware<TTo, Leasing<TOut>> select,
+	                                               Func<TOut, IEnumerable<TTo>> keys)
+	{
+		_registrations.Add(new ManyRegistrations<T, TOut, TTo>(_select, select, keys));
+		return this;
+	}
 
-	public IStopAware<T> Single(IStopAware<TTo, TTo> select)
-		=> new Evict<T>(_store, new RegistrationAwareTags(new Single<T, TTo>(_select, select)), _keys);
+	public SelectedRegistration<T, TTo> Many<TOut>(IStopAware<TTo, TOut> select, Func<TOut, IEnumerable<TTo>> keys)
+	{
+		_registrations.Add(new Registrations<T, TOut, TTo>(_select, select, keys));
+		return this;
+	}
+
+	public SelectedRegistration<T, TTo> Single(IStopAware<TTo, TTo> select)
+	{
+		_registrations.Add(new Single<T, TTo>(_select, select));
+		return this;
+	}
 }
