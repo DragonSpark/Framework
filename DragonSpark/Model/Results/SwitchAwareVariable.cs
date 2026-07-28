fe23814801428f876e3ef253c718b807c9d7d803
Michael DeMond <michael@dragonspark.network>
2026-07-28 12:47:36 -0400

    TODO

--- DragonSpark/Model/Results/SwitchAwareVariable.cs
+++ DragonSpark/Model/Results/SwitchAwareVariable.cs
@@ -1,40 +1,7 @@
 using DragonSpark.Compose;
 using DragonSpark.Model.Selection.Conditions;
-using DragonSpark.Runtime.Execution;
 
 namespace DragonSpark.Model.Results;
-
-public sealed class VisitedAwareVariable<T> : IMutationAware<T>
-{
-	readonly IMutable<T?> _mutable;
-	readonly ICounter     _counter;
-
-	public VisitedAwareVariable() : this(new Variable<T>(), new Variable<int>()) {}
-
-	public VisitedAwareVariable(IMutable<T?> mutable, IMutable<int> counts) : this(mutable, new Counter(counts)) {}
-
-	public VisitedAwareVariable(IMutable<T?> mutable, ICounter counter)
-		: this(mutable, counter, new HasCounted(counter)) {}
-
-	public VisitedAwareVariable(IMutable<T?> mutable, ICounter counter, ICondition condition)
-	{
-		_mutable  = mutable;
-		_counter  = counter;
-		Condition = condition;
-	}
-
-	public ICondition<None> Condition { get; }
-
-	public T Get() => _mutable.Get().Verify();
-
-	public void Execute(T parameter)
-	{
-		_counter.Execute();
-		_mutable.Execute(parameter);
-	}
-}
-
-// TODO
 
 public sealed class SwitchAwareVariable<T> : IMutationAware<T>
 {
---
+++ DragonSpark/Model/Results/VisitedAwareVariable.cs
@@ -0,0 +1,35 @@
+using DragonSpark.Compose;
+using DragonSpark.Model.Selection.Conditions;
+using DragonSpark.Runtime.Execution;
+
+namespace DragonSpark.Model.Results;
+
+public sealed class VisitedAwareVariable<T> : IMutationAware<T> // TODO: Replace with switch-aware
+{
+	readonly IMutable<T?> _mutable;
+	readonly ICounter     _counter;
+
+	public VisitedAwareVariable() : this(new Variable<T>(), new Variable<int>()) {}
+
+	public VisitedAwareVariable(IMutable<T?> mutable, IMutable<int> counts) : this(mutable, new Counter(counts)) {}
+
+	public VisitedAwareVariable(IMutable<T?> mutable, ICounter counter)
+		: this(mutable, counter, new HasCounted(counter)) {}
+
+	public VisitedAwareVariable(IMutable<T?> mutable, ICounter counter, ICondition condition)
+	{
+		_mutable  = mutable;
+		_counter  = counter;
+		Condition = condition;
+	}
+
+	public ICondition<None> Condition { get; }
+
+	public T Get() => _mutable.Get().Verify();
+
+	public void Execute(T parameter)
+	{
+		_counter.Execute();
+		_mutable.Execute(parameter);
+	}
+}
