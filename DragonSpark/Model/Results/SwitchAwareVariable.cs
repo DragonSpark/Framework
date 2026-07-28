784a729ce5e397d6ce836d8369b51022a83d5587
Michael DeMond <michael@dragonspark.network>
2026-07-28 12:46:42 -0400

    Full first workflow

--- DragonSpark/Model/Results/SwitchAwareVariable.cs
+++ DragonSpark/Model/Results/SwitchAwareVariable.cs
@@ -1,10 +1,10 @@
-﻿using DragonSpark.Compose;
+using DragonSpark.Compose;
 using DragonSpark.Model.Selection.Conditions;
 using DragonSpark.Runtime.Execution;
 
 namespace DragonSpark.Model.Results;
 
-public class VisitedAwareVariable<T> : IMutationAware<T>
+public sealed class VisitedAwareVariable<T> : IMutationAware<T>
 {
 	readonly IMutable<T?> _mutable;
 	readonly ICounter     _counter;
@@ -30,6 +30,35 @@ public class VisitedAwareVariable<T> : IMutationAware<T>
 	public void Execute(T parameter)
 	{
 		_counter.Execute();
+		_mutable.Execute(parameter);
+	}
+}
+
+// TODO
+
+public sealed class SwitchAwareVariable<T> : IMutationAware<T>
+{
+	readonly IMutable<T?> _mutable;
+	readonly ISwitch      _switch;
+
+	public SwitchAwareVariable() : this(new Variable<T>(), false) {}
+
+	public SwitchAwareVariable(IMutable<T?> mutable, Switch @switch) : this(mutable, @switch, @switch) {}
+
+	public SwitchAwareVariable(IMutable<T?> mutable, ISwitch @switch, ICondition condition)
+	{
+		_mutable  = mutable;
+		_switch   = @switch;
+		Condition = condition;
+	}
+
+	public ICondition<None> Condition { get; }
+
+	public T Get() => _mutable.Get().Verify();
+
+	public void Execute(T parameter)
+	{
+		_switch.Up();
 		_mutable.Execute(parameter);
 	}
 }
