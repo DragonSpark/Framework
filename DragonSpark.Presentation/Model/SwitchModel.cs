e6ee101bb958f7832dccd03a82fd28db0c614ead
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2026-08-11 12:02:26 -0400

    Switch Model (#166)

---
+++ DragonSpark.Presentation/Model/SwitchModel.cs
@@ -0,0 +1,31 @@
+﻿using DragonSpark.Model.Results;
+
+namespace DragonSpark.Presentation.Model;
+
+public class SwitchModel<TValue> : BindingModel<TValue>
+{
+	readonly Func<TValue> _initialize;
+
+	protected SwitchModel(IMutable<TValue?> store) : this(store.Execute, store.Get) {}
+
+	protected SwitchModel(Action<TValue?> set, Func<TValue?> get) : this(set, get, get!) {}
+
+	protected SwitchModel(Action<TValue?> set, Func<TValue?> get, Func<TValue> initialize) : base(set, get)
+	{
+		_initialize = initialize;
+		On          = get() is not null;
+	}
+
+	public bool On
+	{
+		get;
+		set
+		{
+			if (field != value)
+			{
+				field = value;
+				Execute(field ? _initialize() : default);
+			}
+		}
+	}
+}
