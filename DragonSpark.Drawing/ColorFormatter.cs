d3b5fc9ff730a17e56bf5f9592df49154b642bc3
Michael DeMond <michael@dragonspark.network>
2026-08-13 12:57:49 -0400

    Select background color

---
+++ DragonSpark.Drawing/ColorFormatter.cs
@@ -0,0 +1,12 @@
+﻿using DragonSpark.Text;
+using System.Drawing;
+
+namespace DragonSpark.Drawing;
+
+public sealed class ColorFormatter : Formatter<Color>
+{
+	public static ColorFormatter Default { get; } = new();
+
+	// Standard CSS RGBA order for SfColorPicker!
+	ColorFormatter() : base(x => $"#{x.R:X2}{x.G:X2}{x.B:X2}{x.A:X2}") {}
+}
--- DragonSpark.Drawing/FontDefinition.cs
+++ DragonSpark.Drawing/FontDefinition.cs
@@ -1,4 +1,4 @@
-﻿using SixLabors.Fonts;
+using SixLabors.Fonts;
 
 namespace DragonSpark.Drawing;
 
