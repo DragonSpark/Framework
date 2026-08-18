280a5a9920650e51948a06b276e3df32cbd5b47e
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2026-08-18 10:42:54 -0400

    Additional Upload Components (#177)
    * Additional Upload Components
    
    * ClearTemporaryFile

---
+++ DragonSpark.Azure/Storage/Uploads/ClearTemporaryFile.cs
@@ -0,0 +1,11 @@
+﻿using DragonSpark.Compose;
+using DragonSpark.Model.Operations.Stop;
+
+namespace DragonSpark.Azure.Storage.Uploads;
+
+public abstract class ClearTemporaryFile : StopAware<string>
+{
+	protected ClearTemporaryFile(IContainer container) : this(container.Delete()) {}
+
+	protected ClearTemporaryFile(IDelete delete) : base(delete.Then().Terminate()) {}
+}
