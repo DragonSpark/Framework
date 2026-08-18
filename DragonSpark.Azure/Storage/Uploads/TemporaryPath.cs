9c032cf6fbfe6350773be982da076046c8f9b179
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2026-08-18 10:38:25 -0400

    Additional Upload Components (#176)

---
+++ DragonSpark.Azure/Storage/Uploads/TemporaryPath.cs
@@ -0,0 +1,20 @@
+﻿using DragonSpark.Application.AspNet.Runtime;
+using DragonSpark.Application.Model;
+using DragonSpark.Compose;
+using DragonSpark.Contracts.Uploads;
+
+namespace DragonSpark.Azure.Storage.Uploads;
+
+sealed class TemporaryPath : ITemporaryPath
+{
+	readonly TemporaryUserPath _root;
+
+	public TemporaryPath(TemporaryUserPath root) => _root = root;
+
+	public string Get(UserInput<WorkspacePath> parameter)
+	{
+		var (user, (workspace, referenced)) = parameter;
+		var root = _root.Get(new(user, workspace.ToString()));
+		return !referenced.IsNullOrEmpty() ? $"{root}/{referenced}" : root;
+	}
+}
---
+++ DragonSpark.Syncfusion/Components/Uploads/Completed.cs
@@ -0,0 +1,28 @@
+﻿using DragonSpark.Application.AspNet.Runtime;
+using DragonSpark.Application.Model;
+using DragonSpark.Application.Model.Selections;
+using DragonSpark.Compose;
+using DragonSpark.Contracts.Uploads;
+using DragonSpark.Model.Operations;
+using DragonSpark.Model.Operations.Selection.Stop;
+
+namespace DragonSpark.SyncfusionRendering.Components.Uploads;
+
+public abstract class Completed<T> : IInput<WorkspacePath, T>
+{
+	readonly ITemporaryPath         _path;
+	readonly IStopAware<string, T?> _entry;
+
+	protected Completed(ITemporaryPath path, IStopAware<string, T?> entry)
+	{
+		_path  = path;
+		_entry = entry;
+	}
+
+	public async ValueTask<T> Get(Stop<UserInput<WorkspacePath>> parameter)
+	{
+		var path  = _path.Get(parameter);
+		var entry = await _entry.Off(new(path, parameter));
+		return entry.Verify();
+	}
+}
