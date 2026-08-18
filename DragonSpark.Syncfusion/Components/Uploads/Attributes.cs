5f2b5a3a3e1bdc36b2b6198871c0b9e02af412b8
Mike-E <michael@dragonspark.network>
2026-08-18 10:14:53 -0400

    Minor adjustments

---
+++ DragonSpark.Syncfusion/Components/Uploads/Attributes.cs
@@ -0,0 +1,10 @@
+﻿using DragonSpark.Model.Results;
+
+namespace DragonSpark.SyncfusionRendering.Components.Uploads;
+
+public sealed class Attributes : Instance<Dictionary<string, object>>
+{
+	public static Attributes Default { get; } = new();
+
+	Attributes() : base(new() { { "name", "files" } }) {}
+}
