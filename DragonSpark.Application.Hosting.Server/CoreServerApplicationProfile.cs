70f60006a98f1bffbd1851a2f7a0d56669440b0b
Michael DeMond <michael@dragonspark.network>
2026-08-18 13:40:43 -0400

    Basic upload support

--- DragonSpark.Application.Hosting.Server/CoreServerApplicationProfile.cs
+++ DragonSpark.Application.Hosting.Server/CoreServerApplicationProfile.cs
@@ -8,9 +8,12 @@ namespace DragonSpark.Application.Hosting.Server;
 
 public sealed class CoreServerApplicationProfile : ApplicationProfile
 {
-    public static CoreServerApplicationProfile Default { get; } = new();
+	public static CoreServerApplicationProfile Default { get; } = new();
 
-    CoreServerApplicationProfile() : this(_ => {}, CoreApplicationConfiguration.Default.Then().Terminate().Get()) {}
+	CoreServerApplicationProfile() : this(x => x) {}
+
+    public CoreServerApplicationProfile(Func<IApplicationBuilder, IApplicationBuilder> configure)
+	    : this(_ => {}, new CoreApplicationConfiguration(configure).Then().Terminate().Get()) {}
 
     public CoreServerApplicationProfile(Action<MvcOptions> configure, ICommand<IApplicationBuilder> application)
         : base(new DefaultServiceConfiguration(configure), application) {}
