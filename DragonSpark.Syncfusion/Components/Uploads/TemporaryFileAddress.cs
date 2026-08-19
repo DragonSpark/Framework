9ee8028c0beaf1a9a53f548febfbd5cbac511c91
Mike-E <michael@dragonspark.network>
2026-08-19 10:28:53 -0400

    Temprary file classes

---
+++ DragonSpark.Syncfusion/Components/Uploads/TemporaryFileAddress.cs
@@ -0,0 +1,19 @@
+﻿using DragonSpark.Compose;
+using Flurl;
+
+namespace DragonSpark.SyncfusionRendering.Components.Uploads;
+
+public class TemporaryFileAddress : IUploadFileAddress
+{
+	readonly Uri _address;
+
+	protected TemporaryFileAddress(Uri address) => _address = address;
+
+	public string Get(string parameter)
+	{
+		var directory = new FileInfo(parameter).Directory.Verify();
+		return _address.AppendPathSegment(directory.Parent.Verify().Name)
+		               .AppendPathSegment(directory.Name)
+		               .AppendPathSegment(Path.GetFileName(parameter));
+	}
+}
