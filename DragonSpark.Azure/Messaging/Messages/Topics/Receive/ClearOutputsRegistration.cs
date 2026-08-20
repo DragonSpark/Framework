8d81bf16f2f8c0848e506b60c3cb6aa2b4d5d6ce
Mike-E <michael@dragonspark.network>
2026-08-20 05:07:11 -0400

    Tag outputs based on definitions

--- DragonSpark.Azure/Messaging/Messages/Topics/Receive/ClearOutputsRegistration.cs
+++ DragonSpark.Azure/Messaging/Messages/Topics/Receive/ClearOutputsRegistration.cs
@@ -1,16 +1,19 @@
 ﻿using DragonSpark.Server.Output;
+using DragonSpark.Text;
 using Microsoft.AspNetCore.OutputCaching;
 
 namespace DragonSpark.Azure.Messaging.Messages.Topics.Receive;
 
 public class ClearOutputsRegistration<T> : EventRegistration<T> where T : Message
 {
-	protected ClearOutputsRegistration(IOutputCacheStore output, IOutputKey key)
+	protected ClearOutputsRegistration(IOutputCacheStore output, IFormatter<T> key)
 		: base(new ClearOutputs<T>(output, key)) {}
+	protected ClearOutputsRegistration(IOutputCacheStore output, IOutputKey key)
+		: base(new ClearOutputAdapter<T>(output, key)) {}
 }
 
 public class ClearOutputsRegistration<TIn, T> : EventRegistration<T, TIn> where T : Message<TIn>
 {
-	protected ClearOutputsRegistration(IOutputCacheStore output, IOutputKey key)
+	protected ClearOutputsRegistration(IOutputCacheStore output, IFormatter<TIn> key)
 		: base(new ClearOutputs<TIn>(output, key)) {}
 }
---
+++ DragonSpark.Server/Output/TagDefinition.cs
@@ -0,0 +1,37 @@
+﻿using DragonSpark.Compose;
+using DragonSpark.Model.Operations;
+using DragonSpark.Model.Operations.Selection.Stop;
+using DragonSpark.Model.Operations.Stop;
+using DragonSpark.Model.Selection;
+using DragonSpark.Text;
+
+namespace DragonSpark.Server.Output;
+
+public class TagDefinition<TIn, TOut, T> : IStopAware<TagDefinitionInput<TIn, TOut>>
+{
+	readonly ICurrentTags            _tags;
+	readonly ISelect<TOut, string>   _output;
+	readonly IStopAware<TIn, string> _input;
+
+	// ReSharper disable once TooManyDependencies
+	protected TagDefinition(ICurrentTags tags, IFormatter<T> key, Func<TOut, T> select, IStopAware<TIn, T> input)
+		: this(tags, Start.A.Selection(select).Then().Select(key).Get(), input.Then().Select(key).Out()) {}
+
+	protected TagDefinition(ICurrentTags tags, ISelect<TOut, string> output, IStopAware<TIn, string> input)
+	{
+		_tags   = tags;
+		_output = output;
+		_input  = input;
+	}
+
+	public async ValueTask Get(Stop<TagDefinitionInput<TIn, TOut>> parameter)
+	{
+		var tags = _tags.Get();
+		if (tags is not null)
+		{
+			var ((input, output), stop) = parameter;
+			var item = output is not null ? _output.Get(output) : await _input.Off(new(input, stop));
+			tags?.Add(item);
+		}
+	}
+}
