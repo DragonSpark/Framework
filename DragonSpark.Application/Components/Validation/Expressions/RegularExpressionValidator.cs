c9c6da77b15b6e3d8f189624ba61c58e1621228c
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2026-08-25 05:22:45 -0400

    Restore necessary files (#190)

--- DragonSpark.Application/Components/Validation/Expressions/RegularExpressionValidator.cs
+++ DragonSpark.Application/Components/Validation/Expressions/RegularExpressionValidator.cs
@@ -8,3 +8,10 @@ public class RegularExpressionValidator : MetadataValueValidator<object>
 
 	public RegularExpressionValidator(RegularExpressionAttribute metadata) : base(metadata) {}
 }
+
+public class RegularExpressionValidator<T> : MetadataValueValidator<T>
+{
+    public RegularExpressionValidator(string expression) : this(new RegularExpressionAttribute(expression)) {}
+
+    public RegularExpressionValidator(RegularExpressionAttribute metadata) : base(metadata) {}
+}
