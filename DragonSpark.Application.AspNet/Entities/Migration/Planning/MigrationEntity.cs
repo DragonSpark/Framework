5cd7614c33f283795faa411856d113b3ef2c6999
Mike-E <michael@dragonspark.network>
2026-09-12 06:18:10 -0400

    Static expansions

---
+++ DragonSpark.Application.AspNet/Entities/Migration/Planning/MigrationEntity.cs
@@ -0,0 +1,12 @@
+﻿using DragonSpark.Model.Selection.Conditions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DragonSpark.Application.AspNet.Entities.Migration.Planning;
+
+sealed class MigrationEntity : Condition<IEntityType>
+{
+	public static MigrationEntity Default { get; } = new();
+
+	MigrationEntity() : base(x => x.IsOwned() && x.GetViewName() is null && x.FindPrimaryKey() != null) {}
+}
---
+++ DragonSpark.Application.AspNet/Entities/Queries/Composition/Expansion.cs
@@ -0,0 +1,20 @@
+﻿using DragonSpark.Model.Selection;
+using System.Linq.Expressions;
+
+namespace DragonSpark.Application.AspNet.Entities.Queries.Composition;
+
+public abstract class Expansion<TIn, TOut> : Projection<TIn, TOut>, ISelect<TIn, TOut>
+{
+	public static implicit operator Func<TIn, TOut>(Expansion<TIn, TOut> instance) => instance._select;
+
+	public static implicit operator Expression<Func<TIn, TOut>>(Expansion<TIn, TOut> @this) => @this.Get();
+
+	readonly Func<TIn, TOut> _select;
+
+	protected Expansion(Expression<Func<TIn, TOut>> projection) : this(projection, projection.Compile()) {}
+
+	protected Expansion(Expression<Func<TIn, TOut>> projection, Func<TIn, TOut> select) : base(projection)
+		=> _select = select;
+
+	public TOut Get(TIn parameter) => _select(parameter);
+}
