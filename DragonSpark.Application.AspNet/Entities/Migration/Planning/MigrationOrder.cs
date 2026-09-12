5c297e089a48bd382089a86ce07ba1ccb5e0dc58
Mike-E <michael@dragonspark.network>
2026-09-12 06:18:10 -0400

    Static expansions

--- DragonSpark.Application.AspNet/Entities/Migration/Planning/MigrationOrder.cs
+++ DragonSpark.Application.AspNet/Entities/Migration/Planning/MigrationOrder.cs
@@ -1,5 +1,4 @@
 ﻿using DragonSpark.Model.Sequences;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using NetFabric.Hyperlinq;
 using System.Buffers;
@@ -12,52 +11,24 @@ public sealed class MigrationOrder : IArray<IModel, IEntityType>
 
 	MigrationOrder() : this(TopologicalSort.Default) {}
 
-	readonly ITopologicalSort _sort;
+	readonly ITopologicalSort        _sort;
+	readonly Func<IEntityType, bool> _entity;
 
-	public MigrationOrder(ITopologicalSort sort) => _sort = sort;
+	public MigrationOrder(ITopologicalSort sort) : this(sort, MigrationEntity.Default.Get) {}
+
+	public MigrationOrder(ITopologicalSort sort, Func<IEntityType, bool> entity)
+	{
+		_sort        = sort;
+		_entity = entity;
+	}
 
 	public Array<IEntityType> Get(IModel parameter)
 	{
 		using var entities = parameter.GetEntityTypes()
-									  .Where(t => t.IsOwned() && t.GetViewName() is null
-												  && t.FindPrimaryKey() != null)
+									  .Where(_entity)
 									  .AsValueEnumerable()
 									  .ToArray(ArrayPool<IEntityType>.Shared);
 		var result = _sort.Get(entities);
 		return result;
 	}
-
-	// TODO:
-	/*public Array<IEntityType> Get(IModel parameter)
-	{
-		using var entities = parameter.GetEntityTypes()
-		                              .Where(t => t.Name.EndsWith(".IssuanceProcess")
-		                                          && !t.IsOwned() && t.GetViewName() is null
-		                                          && t.FindPrimaryKey() != null)
-		                              .AsValueEnumerable()
-		                              .ToArray(ArrayPool<IEntityType>.Shared);
-		var result = _sort.Get(entities);
-		return result;
-	}*/
-
-	/*public Array<IEntityType> Get(IModel parameter)
-	{
-		using var entities = parameter.GetEntityTypes()
-		                              .Where(t => (t.Name.EndsWith(".IssuanceProcess")
-		                                           || t.Name.EndsWith(".StandardUserAccount")
-		                                           || t.Name.EndsWith(".ProductDefinition")
-		                                           || t.Name.EndsWith(".MarketplaceProfile")
-		                                           || t.Name.EndsWith(".ImageFileDetails")
-		                                           || t.Name.EndsWith(".DocumentFileDetails")
-		                                           || t.Name.EndsWith(".AudioFileDetails")
-		                                           || t.Name.EndsWith(".VideoFileDetails")
-		                                           || t.Name.EndsWith(".TextFileDetails")
-		                                           || t.Name.EndsWith(".Identity.User"))
-		                                          && !t.IsOwned() && t.GetViewName() is null
-		                                          && t.FindPrimaryKey() != null)
-		                              .AsValueEnumerable()
-		                              .ToArray(ArrayPool<IEntityType>.Shared);
-		var result = entities.ToArray();
-		return result;
-	}*/
 }
