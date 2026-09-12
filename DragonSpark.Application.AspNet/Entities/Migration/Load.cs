a80fbd327a5fc64488d2a83de80564dd949e0e51
Mike-E <michael@dragonspark.network>
2026-09-12 05:38:09 -0400

    File Transfer adjustments

--- DragonSpark.Application.AspNet/Entities/Migration/Load.cs
+++ DragonSpark.Application.AspNet/Entities/Migration/Load.cs
@@ -1,4 +1,3 @@
-using DragonSpark.Compose;
 using DragonSpark.Model.Operations;
 using DragonSpark.Model.Operations.Allocated.Stop;
 using Microsoft.EntityFrameworkCore;
@@ -14,7 +13,11 @@ sealed class Load<T> : IAllocating<LoadInput<T>, T> where T : class
 	public Task<T> Get(Stop<LoadInput<T>> parameter)
 	{
 		var ((subject, include), stop) = parameter;
-		var key = subject.Entity.To<T>();
-		return include(subject.Context.Set<T>()).SingleAsync(x => x == key, stop);
+
+		var property = subject.Metadata.FindPrimaryKey()!.Properties.Single();
+		var key      = subject.Property(property.Name).CurrentValue;
+
+		return include(subject.Context.Set<T>())
+			.SingleAsync(x => EF.Property<object>(x, property.Name).Equals(key), stop);
 	}
 }
